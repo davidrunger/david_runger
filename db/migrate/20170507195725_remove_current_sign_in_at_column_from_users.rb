4233d7624ebdb8bed7ff6d63ca8ec21d6643a20b
David Runger <david.runger@hired.com>
2017-05-07 13:00:26 -0700

    Replace current_sign_in_at with last_activity_at
    We will also be adding logging of all HTTP requests (in a Postgres
    table) shortly.

---
+++ db/migrate/20170507195725_remove_current_sign_in_at_column_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveCurrentSignInAtColumnFromUsers < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :users, :current_sign_in_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506133752) do
+ActiveRecord::Schema.define(version: 20170507195725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20170506133752) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "current_sign_in_at"
+    t.datetime "last_activity_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
