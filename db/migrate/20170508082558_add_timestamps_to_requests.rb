2fda75ee79b0081c039c0d9fb85967f95aac1151
David Runger <david.runger@hired.com>
2017-05-08 01:27:58 -0700

    Add timestamps to requests table

---
+++ db/migrate/20170508082558_add_timestamps_to_requests.rb
@@ -0,0 +1,12 @@
+class AddTimestampsToRequests < ActiveRecord::Migration[5.1]
+  def up
+    change_table :requests do |t|
+      t.timestamps
+    end
+  end
+
+  def down
+    remove_column :requests, :created_at
+    remove_column :requests, :updated_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508061507) do
+ActiveRecord::Schema.define(version: 20170508082558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20170508061507) do
     t.integer "status"
     t.integer "view"
     t.integer "db"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
