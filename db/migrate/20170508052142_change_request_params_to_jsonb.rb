c6d487b37284d269b6eb2d85d0c6c9f5d4f54664
David Runger <david.runger@hired.com>
2017-05-07 22:23:35 -0700

    Change `requests.params` column to jsonb

---
+++ db/migrate/20170508052142_change_request_params_to_jsonb.rb
@@ -0,0 +1,6 @@
+class ChangeRequestParamsToJsonb < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :requests, :params, :string
+    add_column :requests, :params, :jsonb
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507201107) do
+ActiveRecord::Schema.define(version: 20170508052142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(version: 20170507201107) do
     t.bigint "user_id"
     t.string "url"
     t.string "handler"
-    t.string "params"
     t.string "referer"
+    t.jsonb "params"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
