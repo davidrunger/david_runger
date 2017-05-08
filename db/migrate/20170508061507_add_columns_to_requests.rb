10307140ab76ecd2ffa740b6366722eb273cdab6
David Runger <david.runger@hired.com>
2017-05-07 23:16:47 -0700

    Add more fields to `requests` (format, status, view, db)

---
+++ db/migrate/20170508061507_add_columns_to_requests.rb
@@ -0,0 +1,8 @@
+class AddColumnsToRequests < ActiveRecord::Migration[5.1]
+  def change
+    add_column :requests, :format, :string, null: false
+    add_column :requests, :status, :integer
+    add_column :requests, :view, :integer
+    add_column :requests, :db, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508053215) do
+ActiveRecord::Schema.define(version: 20170508061507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20170508053215) do
     t.string "referer"
     t.jsonb "params"
     t.string "method", null: false
+    t.string "format", null: false
+    t.integer "status"
+    t.integer "view"
+    t.integer "db"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
