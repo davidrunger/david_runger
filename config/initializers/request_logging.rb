be8172204ed731103fd0a24b3c5f43301609beb2
David Runger <david.runger@hired.com>
2017-05-10 06:35:42 -0700

    Don't crate request logs for NewRelicPinger

--- config/initializers/request_logging.rb
+++ config/initializers/request_logging.rb
@@ -1,9 +1,17 @@
+module RequestLogging
+  IGNORED_USER_AGENTS = %w[
+    NewRelicPinger
+  ]
+  IGNORED_USER_AGENTS_REGEX = Regexp.new(IGNORED_USER_AGENTS.join('|'))
+end
+
 ActiveSupport::Notifications.subscribe('process_action.action_controller') do |_name, _started, _finished, _unique_id, payload|
   params = payload[:params]
   request_uuid = params['request_uuid']
   stashed_json = request_uuid && $redis.get(request_uuid)
   stashed_data = stashed_json.present? ? JSON.parse(stashed_json) : {}
-  Request.create!(
+  user_agent = stashed_data['user_agent']
+  request_attributes = {
     user_id: stashed_data['user_id'],
     url: stashed_data['url'],
     format: payload[:format],
@@ -15,7 +23,16 @@ ActiveSupport::Notifications.subscribe('process_action.action_controller') do |_
     view: payload[:view_runtime],
     db: payload[:db_runtime],
     ip: stashed_data['ip'],
-    user_agent: stashed_data['user_agent'],
+    user_agent: user_agent,
     requested_at: stashed_data['requested_at'],
-  )
+  }
+
+  if user_agent =~ RequestLogging::IGNORED_USER_AGENTS_REGEX
+    Rails.logger.info(<<-LOG.squish)
+      Skipping creation of a Request because user_agent #{user_agent} is in IGNORED_USER_AGENTS.
+      Request attributes were #{request_attributes}
+    LOG
+  else
+    Request.create!(request_attributes)
+  end
 end
