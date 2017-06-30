0802e99d44c9715e109c908796f5f4fb3037857d
David Runger <david.runger@hired.com>
2017-06-29 23:13:02 -0700

    Organize routes a little more (mostly add newlines)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
-  get 'groceries', to: 'grocery_lists#show'
   get 'login', to: 'sessions#new'
+
+  get 'groceries', to: 'grocery_lists#show'
+
   resources :templates, only: [:index, :new, :create, :show]
+
   namespace :api, defaults: {format: :json} do
     resources :stores, only: [:create, :destroy] do
       resources :items, only: [:index, :create]
