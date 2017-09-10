dd57cd765c2532d5cb1ce4fa9fbc2b3a98257381
David Runger <david.runger@hired.com>
2017-09-10 12:16:26 -0700

    Serve JavaScript via prebuilt pack when not running dev server

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,6 +1,6 @@
 module LayoutHelper
   def webpack_dev_server_present?
-    Rails.env.development? && Net::HTTP.get(URI("http://localhost:8080")) ? true : false
+    Rails.env.development? && !!Net::HTTP.get(URI("http://localhost:8080"))
   rescue Errno::ECONNREFUSED
     return false
   end
