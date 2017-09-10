6e4473b9165fd473b9e4d669860445eb11d5f65b
David Runger <david.runger@hired.com>
2017-09-10 13:30:35 -0700

    Always use javascript_pack_tag in application.html.erb
    This `webpack_dev_server_present?` conditional logic wasn't working too
    well when trying to deploy to production. Let's see if it works better
    if we trust Rails's magic and just always use `javascript_pack_tag`.

--- app/helpers/layout_helper.rb
+++
@@ -1,7 +0,0 @@
-module LayoutHelper
-  def webpack_dev_server_present?
-    Rails.env.development? && !!Net::HTTP.get(URI("http://localhost:8080"))
-  rescue Errno::ECONNREFUSED
-    return false
-  end
-end
