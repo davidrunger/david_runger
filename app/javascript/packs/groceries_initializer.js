c14e8144b3c603ddf5840c85199f010769f17071
David Runger <david.runger@hired.com>
2017-10-08 11:30:14 -0700

    Add Vuex to Groceries app (with demo counter store)
    (Obviously we'll be replacing the counter store with a store for our
    groceries app.)

--- app/javascript/packs/groceries_initializer.js
+++ app/javascript/packs/groceries_initializer.js
@@ -1,11 +1,25 @@
+import Vuex from 'vuex';
 import Vue from '../vendor/vue';
 import Groceries from '../groceries/groceries.vue';
 
+Vue.use(Vuex);
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count += 1;
+    },
+  },
+});
+
 document.addEventListener('DOMContentLoaded', () => {
   document.body.appendChild(document.createElement('replacedcontainer'));
-  new Vue({ // eslint-disable-line no-new
-    el: 'replacedcontainer',
-    template: '<Groceries/>',
+  new Vue({
     components: { Groceries },
+    el: 'replacedcontainer',
+    store,
+    template: '<Groceries/>',
   });
 });
