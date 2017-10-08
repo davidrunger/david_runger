58a875f10f1d8db0f15359dcf3cb44d62253d931
David Runger <david.runger@hired.com>
2017-10-08 13:01:13 -0700

    Conform production.js webpack config to eslint

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,10 +2,10 @@
 
 /* eslint global-require: 0 */
 
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const CompressionPlugin = require('compression-webpack-plugin')
-const sharedConfig = require('./shared.js')
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const CompressionPlugin = require('compression-webpack-plugin');
+const sharedConfig = require('./shared.js');
 
 module.exports = merge(sharedConfig, {
   output: { filename: '[name]-[chunkhash].js' },
@@ -19,18 +19,18 @@ module.exports = merge(sharedConfig, {
       sourceMap: true,
 
       compress: {
-        warnings: false
+        warnings: false,
       },
 
       output: {
-        comments: false
-      }
+        comments: false,
+      },
     }),
 
     new CompressionPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
-      test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/
-    })
-  ]
-})
+      test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/,
+    }),
+  ],
+});
