71852f672d918f26658dcea2756c04494a5c591e
David Runger <david.runger@hired.com>
2017-10-08 09:01:55 -0700

    Change title from "Grocery List" to "Groceries"
    I prefer "Groceries" because it's simpler and because it matches the
    `/groceries` URL.

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,6 +1,6 @@
 class GroceryListsController < ApplicationController
 	def show
-		@title = 'Grocery List'
+		@title = 'Groceries'
     bootstrap(
       current_user: current_user&.as_json,
       stores: ActiveModel::Serializer::CollectionSerializer.new(current_user.stores.includes(:items)),
