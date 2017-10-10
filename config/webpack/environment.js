68dfeb5395105d7ed5af75915dd0c6ac4ea5a20d
David Runger <david.runger@hired.com>
2017-10-10 01:33:55 -0700

    Override webpacker vue loader with my previous config
    I don't like how I have to know that the loader is named `'vue'` to
    override it. Whatcha gonna do, though. :-/

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker');
 
+environment.loaders.set('vue', {
+  test: /.vue$/,
+  loader: 'vue-loader',
+  options: {
+    loaders: {
+      js: 'babel-loader',
+      file: 'file-loader',
+      scss: 'vue-style-loader!css-loader!postcss-loader!sass-loader',
+      sass: 'vue-style-loader!css-loader!postcss-loader!sass-loader?indentedSyntax',
+    },
+  },
+});
+
 module.exports = environment;
