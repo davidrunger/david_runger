d8293b63cf2260a78dc9455dfb6bfde068d06ec2
David Runger <david.runger@hired.com>
2017-10-10 00:04:40 -0700

    Add production.js webpacker 3.0.2 config
    ... plus `NoEmitOnErrorsPlugin`.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,36 +1,9 @@
-// Note: You must restart bin/webpack-dev-server for changes to take effect
-
-/* eslint global-require: 0 */
-
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const CompressionPlugin = require('compression-webpack-plugin');
-const sharedConfig = require('./shared.js');
+const environment = require('./environment');
 
-module.exports = merge(sharedConfig, {
-  output: { filename: '[name]-[chunkhash].js' },
-  devtool: 'source-map',
-  stats: 'normal',
-
+module.exports = merge(environment.toWebpackConfig(), {
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      sourceMap: true,
-
-      compress: {
-        warnings: false,
-      },
-
-      output: {
-        comments: false,
-      },
-    }),
-
-    new CompressionPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/,
-    }),
   ],
 });
