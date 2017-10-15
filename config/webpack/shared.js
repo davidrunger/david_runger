0842fe89ba5580248f3b3e41e91d50127ca72225
David Runger <david.runger@hired.com>
2017-10-14 18:15:02 -0700

    Remove redundant/unnecessary webpack settings from shared.js
    The change from webpacker 2 => webpacker 3 brought a lot of webpack
    configuration from the app into the webpacker gem / node package
    themselves. As a result, a lot of the webpack config settings that I had
    in shared.js from the webpacker 2 days was no longer necessary. This
    commit removes the stuff that is now managed by webpacker.
    
    (To figure out what's redundant, I just did
    `console.log(developmentConfig)` in `config/webpack/development.js` and
    looked for repeated entries in the printed object.)

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -3,13 +3,10 @@
 /* eslint global-require: 0 */
 /* eslint import/no-dynamic-require: 0 */
 
-const webpack = require('webpack');
 const { basename, dirname, join, relative, resolve } = require('path');
 const { sync } = require('glob');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
 const extname = require('path-complete-extname');
-const { env, settings, output, loadersDir } = require('./configuration.js');
+const { settings } = require('./configuration.js');
 
 const extensionGlob = `**/*{${settings.extensions.join(',')}}*`;
 const entryPath = join(settings.source_path, settings.source_entry_path);
@@ -26,14 +23,8 @@ module.exports = {
     {} // eslint-disable-line comma-dangle
   ),
 
-  output: {
-    filename: '[name].js',
-    path: output.path,
-    publicPath: output.publicPath,
-  },
-
   module: {
-    rules: sync(join(loadersDir, '*.js')).map(loader => require(loader)).concat([
+    rules: [
       {
         test: /\.js$|\.vue$/,
         enforce: 'pre',
@@ -41,32 +32,21 @@ module.exports = {
         loader: 'eslint-loader',
         options: {},
       },
-    ]),
+    ],
   },
 
-  plugins: [
-    new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
-    new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-[hash].css' : '[name].css'),
-    new ManifestPlugin({
-      publicPath: output.publicPath,
-      writeToFileEmit: true,
-    }),
-  ],
+  plugins: [],
 
   resolve: {
     alias: {
       img: resolve(__dirname, '../../app/assets/images'),
       css: resolve(__dirname, '../../app/assets/stylesheets'),
-      vendor: resolve(__dirname, '../../app/javascript/vendor'),
     },
     extensions: settings.extensions,
-    modules: [
-      resolve(settings.source_path),
-      'node_modules',
-    ],
+    modules: [],
   },
 
   resolveLoader: {
-    modules: ['node_modules'],
+    modules: [],
   },
 };
