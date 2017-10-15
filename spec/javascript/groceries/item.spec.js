3382c67f266104760b9e99596aac289c36365851
David Runger <david.runger@hired.com>
2017-10-15 14:39:27 -0700

    Write a few more basic tests for the groceries `Item`

--- spec/javascript/groceries/item.spec.js
+++ spec/javascript/groceries/item.spec.js
@@ -6,9 +6,23 @@ import Item from 'groceries/item.vue';
 
 describe('Item', () => {
   const item = { id: 48, name: 'bananas', needed: 0 };
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(Item, { propsData: { item } });
+  });
 
   it('is a Vue instance', () => {
-    const wrapper = mount(Item, { propsData: { item } });
     expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  it('renders item.name in a span', () => {
+    expect(
+      _.some(wrapper.findAll('span').wrappers, span => span.text() === 'bananas'),
+    ).toBeTruthy();
+  });
+
+  it('doesnt initially contain a text input', () => {
+    expect(wrapper.contains('input[type="text"]')).toBeFalsy();
   });
 });
