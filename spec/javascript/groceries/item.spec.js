e305f10530c1ff0d19853862b4de7e13143cd662
David Runger <david.runger@hired.com>
2017-10-15 12:47:01 -0700

    Pass `item` `propsData` in grocery `Item` spec
    Now the spec is passing! :tada: :smile:

--- spec/javascript/groceries/item.spec.js
+++ spec/javascript/groceries/item.spec.js
@@ -5,8 +5,10 @@ import { mount } from 'vue-test-utils';
 import Item from 'groceries/item.vue';
 
 describe('Item', () => {
+  const item = { id: 48, name: 'bananas', needed: 0 };
+
   it('is a Vue instance', () => {
-    const wrapper = mount(Item);
+    const wrapper = mount(Item, { propsData: { item } });
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 });
