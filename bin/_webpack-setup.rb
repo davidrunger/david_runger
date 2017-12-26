d92168295e977ae3bf655e5afa678d538003f028
David Runger <davidjrunger@gmail.com>
2017-12-26 03:13:45 -0800

    Fix JS route compilation on production
    I'm not quite sure where JS route compilation went wrong ... but at some
    point it did. Hopefully this fixes it!

--- bin/_webpack-setup.rb
+++ bin/_webpack-setup.rb
@@ -1,3 +1,7 @@
 puts 'Ensuring that compiled routes are up-to-date ...'
-system('bin/rails app/javascript/rails_assets/routes.js')
+if File.exist?('app/javascript/rails_assets/routes.js')
+  system('bin/rails app/javascript/rails_assets/routes.js')
+else
+  system('bin/rails build_js_routes')
+end
 puts 'Done checking/compiling routes.'
