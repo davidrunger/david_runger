12a9b1f97928102e125fff54cfc0e2ab0fba4822
David Runger <davidjrunger@gmail.com>
2017-12-31 00:38:07 -0800

    Track status code classes (200, 300, etc) via StatsD
    I need more stuff for the Grafana dashboard!

--- statsd-config.js
+++ statsd-config.js
@@ -1,6 +1,15 @@
 // Example w/ explanations of these and other available settings:
 // https://github.com/etsy/statsd/blob/master/exampleConfig.js
 
+//
+// Local Testing Config
+//
+// {
+//   debug: true,
+//   dumpMessages: true,
+//   backends: ['./backends/console'],
+// }
+
 {
   graphite: {
     globalPrefix: process.env.HOSTEDGRAPHITE_APIKEY,
