7631b7689423e633c52636b08df261ca2f06b37b
David Runger <davidjrunger@gmail.com>
2018-05-13 16:09:28 -0700

    Convert @weight_log ivar to local variable in controller action
    Minor refactor. In general, we should probably only be using instance
    variables when a local variable does not suffice.

--- app/controllers/api/weight_logs_controller.rb
+++ app/controllers/api/weight_logs_controller.rb
@@ -1,7 +1,7 @@
 class Api::WeightLogsController < ApplicationController
   def create
-    @weight_log = current_user.weight_logs.create!(weight_log_params)
-    render json: @weight_log, status: :created
+    weight_log = current_user.weight_logs.create!(weight_log_params)
+    render json: weight_log, status: :created
   end
 
   private
