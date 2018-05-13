46b6fb3043a6d625e357058659627d644483c1b9
David Runger <davidjrunger@gmail.com>
2018-05-13 05:19:16 -0700

    Add user's weight logs to bootstrap data

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,7 +2,10 @@ class LogsController < ApplicationController
   def index
     @title = 'Log'
     @description = 'Log your weight and exercise history'
-    bootstrap(current_user: current_user.as_json)
+    bootstrap(
+      current_user: current_user.as_json,
+      weight_logs: ActiveModel::Serializer::CollectionSerializer.new(current_user.weight_logs),
+    )
     render :index
   end
 end
