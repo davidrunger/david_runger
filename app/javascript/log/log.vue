7ca226b00f5754fe0cea7e1258936d29162a6eaa
David Runger <davidjrunger@gmail.com>
2018-05-13 05:13:41 -0700

    Add basic form to post a new WeightLog

--- app/javascript/log/log.vue
+++ app/javascript/log/log.vue
@@ -1,9 +1,46 @@
 <template lang='pug'>
-div {{bootstrap.current_user.email}}
+div
+  div {{bootstrap.current_user.email}}
+  vue-form(@submit.prevent='postWeightLog' :state='formstate')
+    validate
+      el-input(
+        placeholder='Weight'
+        type='number'
+        v-model='newWeightLogWeight'
+        name='newWeightLogWeight'
+        required
+      )
+    el-input(
+      value='Add'
+      type='submit'
+      :disabled='formstate.$invalid'
+    )
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      formstate: {},
+      newWeightLogWeight: '',
+    };
+  },
+
+  methods: {
+    postWeightLog() {
+      if (this.formstate.$invalid) return;
+
+      const payload = {
+        weight_log: {
+          weight: this.newWeightLogWeight,
+        },
+      };
+      this.$http.post(this.$routes.api_weight_logs_path(), payload).then(() => {
+        this.newWeightLogWeight = '';
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
