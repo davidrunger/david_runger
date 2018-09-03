fc8ae7d784d812aa22c1bb461a6f71cc474715e7
David Runger <davidjrunger@gmail.com>
2018-09-03 15:29:17 -0700

    Link to Spiceworks IP geolocation page in Request index & show views
    (in admin)

--- app/fields/ip_address_field.rb
+++ app/fields/ip_address_field.rb
@@ -4,4 +4,8 @@ class IpAddressField < Administrate::Field::Base
   def to_s
     data
   end
+
+  def spiceworks_ip_url
+    "https://community.spiceworks.com/tools/ip-lookup/results?hostname=#{data}"
+  end
 end
