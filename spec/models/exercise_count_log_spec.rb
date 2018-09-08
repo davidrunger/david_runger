726ae4789b54d58b6fb6202f426517bbee0d0009
David Runger <davidjrunger@gmail.com>
2018-09-08 11:29:25 -0700

    Delete all .rb files related to exercise logging

--- spec/models/exercise_count_log_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe ExerciseCountLog, type: :model do
-  subject(:exercise_count_log) { exercise_count_logs(:chin_ups_count_log) }
-
-  it { is_expected.to belong_to(:exercise) }
-
-  it { is_expected.to validate_presence_of(:exercise_id) }
-end
