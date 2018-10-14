514fe36badf9fb6edbf35b173bdf1ede51435873
David Runger <davidjrunger@gmail.com>
2018-10-14 12:13:52 -0700

    Remove meta description tag from groceries app

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -1,10 +1,6 @@
 class GroceriesController < ApplicationController
   def show
     @title = 'Groceries'
-    @description = <<~DESCRIPION.squish
-      A free and convenient online app for keeping track of groceries and other items that you want
-      or need
-    DESCRIPION
     bootstrap(
       current_user: UserSerializer.new(current_user),
       stores:
