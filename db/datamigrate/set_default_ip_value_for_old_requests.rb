7b79924f2c601cecee626bdd7883f86cf06faf1a
David Runger <davidjrunger@gmail.com>
2019-05-09 20:38:35 -0700

    Ignore rubocop Rails/SkipsModelValidations offense

--- db/datamigrate/set_default_ip_value_for_old_requests.rb
+++ db/datamigrate/set_default_ip_value_for_old_requests.rb
@@ -1,3 +1,5 @@
 def set_default_ip_value_for_old_requests
+  # rubocop:disable Rails/SkipsModelValidations
   Request.update_all(ip: 'NOT_RECORDED')
+  # rubocop:enable Rails/SkipsModelValidations
 end
