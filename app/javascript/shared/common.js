43b12128c0a9eec4654c55f85d910c696b047237
David Runger <davidjrunger@gmail.com>
2019-05-18 10:31:27 -0700

    Undefine `window._` that is defined via `import _ from 'lodash'`
    See https://github.com/lodash/lodash/issues/2550

--- app/javascript/shared/common.js
+++ app/javascript/shared/common.js
@@ -1,1 +1,8 @@
+import _ from 'lodash';
+
 import 'shared/routes';
+
+// undefine global lodash (except for tests); see https://github.com/lodash/lodash/issues/2550
+if (window.davidrunger && window.davidrunger.env !== 'test') {
+  _.noConflict();
+}
