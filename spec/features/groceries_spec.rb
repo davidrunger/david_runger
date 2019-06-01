d34624e18b0c1b45ddcf9df5246fbb65405d1f4b
David Runger <davidjrunger@gmail.com>
2019-06-01 12:10:25 -0700

    Render correct template (index, not show) for groceries#index action
    This fixes a 500 error that is occurring when trying to view the
    groceries app.

---
+++ spec/features/groceries_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Groceries app' do
+  let(:user) { users(:user) }
+
+  context 'when user is signed in' do
+    before { sign_in(user) }
+
+    it 'renders expected content' do
+      visit groceries_path
+
+      expect(page).to have_text(user.email)
+
+      store = user.stores.reorder(viewed_at: :desc).first!
+      expect(page).to have_text(store.name)
+      expect(page).to have_button('Check in shopping trip')
+      expect(page).to have_button('Text items to phone')
+      expect(page).to have_button('Copy to clipboard')
+
+      item = store.items.first!
+      expect(page).to have_text("#{item.name} (#{item.needed})")
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -76,6 +76,7 @@ RSpec.configure do |config|
 
   config.include(FactoryBot::Syntax::Methods)
   config.include(Devise::Test::ControllerHelpers, type: :controller)
+  config.include(Devise::Test::IntegrationHelpers, type: :feature)
 
   config.after(:each, type: :controller) do
     Devise.sign_out_all_scopes
