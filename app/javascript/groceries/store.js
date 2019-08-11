d170d1ed36a961e140845d9d32551527ad888716
David Runger <davidjrunger@gmail.com>
2019-08-11 10:02:15 -0700

    Avoid error if `state.stores` does not (yet) exist in groceries

--- app/javascript/groceries/store.js
+++ app/javascript/groceries/store.js
@@ -76,6 +76,8 @@ const getters = {
   ...ModalVuex.getters,
 
   currentStore(state) {
+    if (!state.stores) return null;
+
     return (
       _(state.stores).filter('viewed_at').sortBy(['viewed_at']).last() ||
       state.stores[0]
