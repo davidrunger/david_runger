9075365b59c7043c6ea7a6687ab8fc78f4f520d8
David Runger <davidjrunger@gmail.com>
2019-08-11 10:05:16 -0700

    Return function to remove event listener from event bus #on function

--- app/javascript/lib/event_bus.js
+++ app/javascript/lib/event_bus.js
@@ -2,8 +2,10 @@
 // This can be expanded in the future to include a payload, too.
 // This would require using CustomEvent rather than Event, and possibly a polyfill for IE.
 
+// returns a function that unsubscribes from the event
 export function on(eventName, callback) {
   window.addEventListener(eventName, callback);
+  return () => { window.removeEventListener(eventName, callback); };
 }
 
 export function emit(eventName) {
