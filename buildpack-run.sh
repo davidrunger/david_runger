2b67861c5d473e72197a28068f5ed42c278aff65
David Runger <davidjrunger@gmail.com>
2019-08-31 14:27:45 -0700

    Remove foreman from Gemfile
    Instead, we will install foreman separately from the bundled gems using
    the [heroku-buildpack-run][1] buildpack, which I have added to the
    Heroku app via
    `heroku buildpacks:add https://github.com/weibeld/heroku-buildpack-run`.
    That buildpack will execute `buildpack-run.sh`, which is also added in
    this commit.
    
    [1]: https://elements.heroku.com/buildpacks/weibeld/heroku-buildpack-run

---
+++ buildpack-run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+gem_parent_dir="vendor/heroku/ruby/2.6.0"
+
+mkdir -p $gem_parent_dir
+mkdir -p vendor/heroku/bin
+export GEM_PATH="$GEM_PATH:$gem_parent_dir"
+
+gem install foreman --install-dir "$gem_parent_dir" --bindir vendor/heroku/bin
