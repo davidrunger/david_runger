fbb236b03ae9ac16ae8427ade9a12a93cb9460fe
David Runger <davidjrunger@gmail.com>
2019-10-25 19:59:46 -0700

    Reduce the number of threads, processes, & Redis connections used
    This sets Sidekiq concurrency to 2, which is a significant decrease from
    the default (which I think is 10 and used to be 25). That's fine; we
    aren't really in a big rush to process our Sidekiq jobs. They can wait
    for each other. :)
    
    This changes the fallback `WEB_CONCURRENCY` to 2, reflecting a change I
    have already made to the Heroku environment variable setting.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,14 +2,24 @@
 
 require 'sidekiq-scheduler' if Sidekiq.server?
 
-sidekiq_redis_options = {
-  db: 1,
-}
+# We'll give Sidekiq db 1. The app uses db 0 for its direct uses.
+build_sidekiq_redis_connection = proc { Redis.new(db: 1) }
 
 Sidekiq.configure_client do |config|
-  config.redis = sidekiq_redis_options
+  # Sidekiq Client Connection Pool size:
+  # We have 20 connections total from Heroku Redis Hobby Dev plan
+  # ... minus 7 connections used for Sidekiq below ...
+  # ... minus 5 connections for `rails console` and general padding ...
+  # ... leaves us with 8 connections total for the Rails server processes.
+  # We are running 2 processes, so each process gets 4 connections.
+  # We'll use 2 connections for the Sidekiq client pool (the line below) and 2 for the application.
+  config.redis = ConnectionPool.new(size: 2, &build_sidekiq_redis_connection)
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = sidekiq_redis_options
+  # Sidekiq Server Connection Pool size:
+  # This is `(max_)concurrency + 5`.
+  # For concurrency (2), see config/sidekiq.yml.
+  # For why we are adding 5, see https://github.com/mperham/sidekiq/wiki/Using-Redis#complete-control
+  config.redis = ConnectionPool.new(size: 7, &build_sidekiq_redis_connection)
 end
