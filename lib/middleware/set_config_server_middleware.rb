58609dc1ee6370cbf94c1d1aecb49e0b939d4d37
David Runger <davidjrunger@gmail.com>
2019-10-26 17:49:15 -0700

    Add Sidekiq middleware in development with more detailed logging

--- lib/middleware/set_config_server_middleware.rb
+++ lib/middleware/set_config_server_middleware.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# I'm not sure why it's necessary to explicitly define this module; I thought zeitwerk is supposed
+# to do it for us. Maybe it's because it gets loaded so early.
+module Middleware; end
+
 class Middleware::SetConfigServerMiddleware
   def initialize(app)
     @app = app
