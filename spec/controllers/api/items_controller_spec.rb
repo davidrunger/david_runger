b310b86779ea1929a5f45bafc0662235914d4ec0
David Runger <davidjrunger@gmail.com>
2020-01-05 17:38:59 -0800

    Add more specs for Api::ItemsController

--- spec/controllers/api/items_controller_spec.rb
+++ spec/controllers/api/items_controller_spec.rb
@@ -23,4 +23,87 @@ RSpec.describe Api::ItemsController do
       end
     end
   end
+
+  describe '#update' do
+    subject(:patch_update) { patch(:update, params: params) }
+
+    let(:item) { items(:item) }
+    let(:base_params) { {id: item.id} }
+
+    context 'when attempting to update the item of another user' do
+      let(:owning_user) { item.store.user }
+      let(:user) { User.where.not(id: owning_user).first! }
+      let(:params) { base_params.merge(item: {name: item.name + ' Changed'}) }
+
+      it 'does not update the item' do
+        expect { patch_update }.not_to change { item.reload.attributes }
+      end
+
+      it 'returns a 404 status code' do
+        patch_update
+        expect(response.status).to eq(404)
+      end
+    end
+
+    context 'when the item is being updated with invalid params' do
+      let(:invalid_params) { {item: {name: ''}} }
+      let(:params) { base_params.merge(invalid_params) }
+
+      it 'does not update the item' do
+        expect { patch_update }.not_to change { item.reload.attributes }
+      end
+
+      it 'returns a 422 status code' do
+        patch_update
+        expect(response.status).to eq(422)
+      end
+    end
+
+    context 'when the item is being updated with valid params' do
+      let(:valid_params) { {item: {name: item.name + ' Changed'}} }
+      let(:params) { base_params.merge(valid_params) }
+
+      it 'updates the item' do
+        expect { patch_update }.to change { item.reload.name }
+      end
+
+      it 'returns a 200 status code' do
+        patch_update
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+
+  describe '#destroy' do
+    subject(:delete_destroy) { delete(:destroy, params: {id: item.id}) }
+
+    let(:item) { items(:item) }
+
+    context 'when attempting to destroy the item of another user' do
+      let(:owning_user) { item.store.user }
+      let(:user) { User.where.not(id: owning_user).first! }
+
+      it 'does not destroy the item' do
+        expect { delete_destroy }.not_to change { item.reload.persisted? }
+      end
+
+      it 'returns a 404 status code' do
+        delete_destroy
+        expect(response.status).to eq(404)
+      end
+    end
+
+    context "when attempting to destroy one's own item" do
+      let(:user) { item.store.user }
+
+      it 'destroys the item' do
+        expect { delete_destroy }.to change { Item.find_by(id: item.id) }.from(Item).to(nil)
+      end
+
+      it 'returns a 204 status code' do
+        delete_destroy
+        expect(response.status).to eq(204)
+      end
+    end
+  end
 end
