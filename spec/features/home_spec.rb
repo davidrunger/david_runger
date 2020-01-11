6ccddf26017d8dfd1b71c2bcd411aeca95e16455
David Runger <davidjrunger@gmail.com>
2020-01-10 20:48:21 -0800

    Switch to :rack_test as default Capybara driver
    Unlike our `:chrome_headless` driver, `:rack_test` allows setting custom
    request headers (which we need for a test that we are adding in this
    PR). Plus, I think `:rack_test` is faster.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Home page' do
-  it 'says "David Runger / Full stack web developer"' do
+  it 'says "David Runger / Full stack web developer"', :js do
     visit root_path
     expect(page).to have_text(<<~HEADLINE)
       David Runger
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,8 @@ Capybara.register_driver(:chrome_headless) do |app|
   options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
-Capybara.default_driver = :chrome_headless
+Capybara.default_driver = :rack_test
+Capybara.javascript_driver = :chrome_headless
 # allow loading JS & CSS assets via `save_and_open_page` when running `rails s`
 Capybara.asset_host = 'http://localhost:3000'
 Capybara.server = :puma, {Silent: true}
