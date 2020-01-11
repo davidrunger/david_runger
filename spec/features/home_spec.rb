785c01ccddfca5837e88011de4f34ac1d4044e01
David Runger <davidjrunger@gmail.com>
2020-01-10 20:51:23 -0800

    Add test for redirecting requests made from an unsupported browser

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -8,4 +8,28 @@ RSpec.describe 'Home page' do
       Full stack web developer
     HEADLINE
   end
+
+  context 'when using an unsupported browser' do
+    let(:ie_11_user_agent) { 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko' }
+
+    before do
+      page.driver.header('User-Agent', ie_11_user_agent)
+
+      browser = Browser.new(ie_11_user_agent)
+      expect(browser.name).to eq('Internet Explorer')
+      expect(browser.version).to eq('11')
+      expect(BrowserSupportChecker.new(browser)).not_to be_supported
+    end
+
+    context 'when an Accept header of */* is provided' do
+      before { page.driver.header('Accept', '*/*') }
+
+      it 'redirects to upgrade_browser_path' do
+        visit root_path
+
+        expect(page).to have_current_path(upgrade_browser_path)
+        expect(page).to have_text("We don't support Internet Explorer version 11.")
+      end
+    end
+  end
 end
