addbf17301ee376aeb3f5a10e258e3f2db471cfc
David Runger <davidjrunger@gmail.com>
2020-02-02 13:24:43 -0800

    Add test for TruncateTables#perform (and move code from rake task)

--- app/workers/truncate_tables.rb
+++ app/workers/truncate_tables.rb
@@ -3,7 +3,67 @@
 class TruncateTables
   include Sidekiq::Worker
 
+  ROW_COUNT_SQL = <<~SQL
+    SELECT relname, n_live_tup
+    FROM pg_stat_user_tables
+    ORDER BY n_live_tup DESC;
+  SQL
+
+  def self.max_allowed_rows
+    Integer(ENV.fetch('MAX_TABLE_ROWS') { 1_500 })
+  end
+
+  def self.print_row_counts
+    ApplicationRecord.connection.execute('ANALYZE')
+    ApplicationRecord.connection.execute(ROW_COUNT_SQL).to_a.each do |row|
+      Rails.logger.info("#{row['n_live_tup']} rows - #{row['relname']}")
+    end
+  end
+
+  def self.truncate(table:, timestamp:)
+    num_rows =
+      ApplicationRecord.connection.execute("SELECT count(*) FROM #{table}").to_a.first['count']
+    Rails.logger.info("Rows in `#{table}` prior to truncation: #{num_rows}")
+
+    min_surviving_timestamp_sql = <<~SQL
+      SELECT #{timestamp}
+      FROM #{table}
+      ORDER BY #{timestamp} DESC
+      LIMIT #{max_allowed_rows}
+    SQL
+
+    min_surviving_timestamp =
+      ApplicationRecord.connection.execute(min_surviving_timestamp_sql).to_a.last&.dig(timestamp)
+    return if min_surviving_timestamp.nil?
+
+    delete_old_rows_sql = <<~SQL.squish
+      DELETE
+      FROM #{table}
+      WHERE #{timestamp} < '#{min_surviving_timestamp}'
+    SQL
+    ApplicationRecord.connection.execute(delete_old_rows_sql)
+    Rails.logger.info(
+      "Deleted rows older than #{min_surviving_timestamp} (database time, probably UTC)",
+    )
+    num_rows =
+      ApplicationRecord.connection.execute("SELECT count(*) FROM #{table}").to_a.first['count']
+    Rails.logger.info("Rows in `#{table}` after truncation: #{num_rows}")
+  end
+
   def perform
-    Rake::Task['db:truncate_tables'].invoke
+    Rails.logger.info(<<~LOG.squish)
+      About to truncate database tables;
+      max allowed rows is #{self.class.max_allowed_rows}
+    LOG
+
+    Rails.logger.info('Approximate row counts prior to deletion:')
+    self.class.print_row_counts
+    Rails.logger.info
+
+    self.class.truncate(table: 'requests', timestamp: 'requested_at')
+    self.class.truncate(table: 'pghero_query_stats', timestamp: 'captured_at')
+    self.class.truncate(table: 'pghero_space_stats', timestamp: 'captured_at')
+
+    Rails.logger.info('Done truncating tables')
   end
 end
---
+++ spec/workers/truncate_tables_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe TruncateTables do
+  subject(:worker) { TruncateTables.new }
+
+  describe '#perform' do
+    subject(:perform) { worker.perform }
+
+    context 'when there is at least one row in the `requests` table' do
+      before { expect(Request.count).to be > 0 }
+
+      it 'issues a DELETE command against the `requests` table' do
+        expect(ApplicationRecord.connection).to receive(:execute).
+          with(/DELETE FROM requests/).
+          and_call_original
+
+        # pass other calls through
+        allow(ApplicationRecord.connection).to receive(:execute).and_call_original
+
+        perform
+      end
+    end
+  end
+end
