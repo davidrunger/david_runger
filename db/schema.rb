0cb734b53a42c22f00584b38c869b96c3aa8a60f
David Runger <davidjrunger@gmail.com>
2020-04-19 14:48:46 -0700

    Create `log_shares` table (to authorize others to view a log)

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_173959) do
+ActiveRecord::Schema.define(version: 2020_04_19_213042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_02_16_173959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["store_id"], name: "index_items_on_store_id"
+  end
+
+  create_table "log_shares", force: :cascade do |t|
+    t.bigint "log_id", null: false
+    t.text "email", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["log_id", "email"], name: "index_log_shares_on_log_id_and_email", unique: true
   end
 
   create_table "logs", force: :cascade do |t|
@@ -128,6 +136,7 @@ ActiveRecord::Schema.define(version: 2020_02_16_173959) do
   end
 
   add_foreign_key "items", "stores"
+  add_foreign_key "log_shares", "logs"
   add_foreign_key "logs", "users"
   add_foreign_key "number_log_entries", "logs"
   add_foreign_key "requests", "users"
