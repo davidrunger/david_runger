9d027988f13e663be19f94fe007f34a7f6d1eeab
David Runger <davidjrunger@gmail.com>
2020-05-26 15:37:55 -0700

    Fix line length violation caused by adding spaces to hash literals

--- spec/controllers/api/workouts_controller_spec.rb
+++ spec/controllers/api/workouts_controller_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe Api::WorkoutsController do
     subject(:post_create) { post(:create, params: params) }
 
     context 'when the item params are valid' do
-      let(:valid_params) { { workout: { time_in_seconds: 600, rep_totals: { chinups: 2, pushups: 9 } } } }
+      let(:valid_params) do
+        { workout: { time_in_seconds: 600, rep_totals: { chinups: 2, pushups: 9 } } }
+      end
       let(:params) { valid_params }
 
       it 'returns a 201 status code' do
