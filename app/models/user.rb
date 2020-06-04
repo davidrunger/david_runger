7ac2c79b7cafb6a01e4632f10e04fba91c7382c3
David Runger <davidjrunger@gmail.com>
2020-06-04 10:40:53 -0700

    Move username anonymization logic to User model
    (from `WorkoutSerializer`)

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,4 +45,15 @@ class User < ApplicationRecord
   def sms_usage
     sms_records.sum(:cost)
   end
+
+  def partially_anonymized_username
+    email_username, email_domain = email.split('@')
+
+    if email_username.length >= 8
+      partially_anonymized_email_username = "#{email_username[0..2]}...#{email_username[-3..-1]}"
+      [partially_anonymized_email_username, email_domain].join('@')
+    else
+      "User #{id}"
+    end
+  end
 end
