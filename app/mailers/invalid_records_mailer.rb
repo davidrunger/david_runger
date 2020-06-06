5c262010753fe546ae1469f35ed8e0af234f5e43
David Runger <davidjrunger@gmail.com>
2020-06-06 05:29:03 -0700

    Only mention classes w/ 1+ invalid records in #invalid_records mail

--- app/mailers/invalid_records_mailer.rb
+++ app/mailers/invalid_records_mailer.rb
@@ -2,7 +2,7 @@
 
 class InvalidRecordsMailer < ApplicationMailer
   def invalid_records(invalid_records_count_hash)
-    @invalid_records_count_hash = invalid_records_count_hash
+    @nonzero_invalid_record_counts = invalid_records_count_hash.reject { |_key, value| value == 0 }
     mail(subject: 'There is at least one invalid record. :(')
   end
 end
