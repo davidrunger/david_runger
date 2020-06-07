948c3c8315e691f3ef815ed16ea08800bf1ae6d6
David Runger <davidjrunger@gmail.com>
2020-06-06 21:53:53 -0700

    Strip extraneous email body content from incoming emails; add Flipper
    We're adding Flipper (a feature-flipper / rollout library) so that we
    can turn on/off the printing of extra debug info about the email body of
    incoming mails in the LogEntriesMailbox. (Unfortunately, it's
    more-or-less necessary to debug this in production because we cannot
    really simulate all of the Mailgun processing / email protocols / DNS
    configuration etc that culminates in this flow in production.)

--- app/mailboxes/log_entries_mailbox.rb
+++ app/mailboxes/log_entries_mailbox.rb
@@ -1,11 +1,42 @@
 # frozen_string_literal: true
 
 class LogEntriesMailbox < ApplicationMailbox
+  extend Memoist
+
   def process
+    if debug?
+      puts('--- BEGIN mail.text_part.presence.to_s')
+      p(mail.text_part.presence.to_s)
+      puts('--- END mail.text_part.presence.to_s')
+
+      puts('--- BEGIN mail.body.to_s')
+      p(mail.body.to_s)
+      puts('--- END mail.body.to_s')
+    end
+
     log_id = mail.to.first.presence!.match(ApplicationMailbox::LOG_ENTRIES_ROUTING_REGEX)[:log_id]
     user_email = mail.from.first.presence!
     user = User.find_by!(email: user_email)
     log = user.logs.find(log_id)
-    log.log_entries.create!(data: mail.body.to_s.rstrip)
+    email_content =
+      # call #dup because #trim modifies the string (via at least one `#gsub!` call)
+      EmailReplyTrimmer.trim((mail.text_part.presence || mail.body).to_s.dup).
+        sub(/\AContent-Type:.+\n+/, '').
+        rstrip
+
+    if debug?
+      puts('--- BEGIN email_content')
+      p(email_content)
+      puts('--- END email_content')
+    end
+
+    log.log_entries.create!(data: email_content)
+  end
+
+  private
+
+  memoize \
+  def debug?
+    Flipper.enabled?(:debug_log_entries_mailbox_processing)
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,10 +9,13 @@ Sidekiq.configure_client do |config|
   # Sidekiq Client Connection Pool size:
   # We have 20 connections total from Heroku Redis Hobby Dev plan
   # ... minus 7 connections used for Sidekiq below ...
-  # ... minus 5 connections for `rails console` and general padding ...
-  # ... leaves us with 8 connections total for the Rails server processes.
-  # We are running 2 processes, so each process gets 4 connections.
-  # We'll use 2 connections for the Sidekiq client pool (the line below) and 2 for the application.
+  # ... minus 3 connections for `rails console` and general padding ...
+  # ... leaves us with 10 connections total for the Rails server processes.
+  # We are running 2 processes, so each process gets 5 connections.
+  # We'll distribute those Redis connections used by each Rails server process like so:
+  # * 2 connections for the Sidekiq client pool (the line below) (per process)
+  # * 2 for the application (per process)
+  # * 1 for Flipper (per process)
   config.redis = ConnectionPool.new(size: 2, &build_sidekiq_redis_connection)
 end
 
