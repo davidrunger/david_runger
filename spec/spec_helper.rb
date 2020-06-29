c47f35d547c0751af9884d8dd18360b57d37d519
David Runger <davidjrunger@gmail.com>
2020-06-29 15:15:02 -0700

    Put the RunHeat worker behind a disable_run_heat_worker flipper flag

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -124,6 +124,11 @@ RSpec.configure do |config|
     metadata[:request_format] = :json
   end
 
+  config.before(:each) do
+    # flush Flipper state
+    system('redis-cli -n 3 FLUSHDB', exception: true, out: File::NULL)
+  end
+
   config.before(:each, request_format: :json) do
     request.accept = 'application/json'
   end
--- spec/workers/run_heat_spec.rb
+++ spec/workers/run_heat_spec.rb
@@ -6,28 +6,40 @@ RSpec.describe RunHeat do
   describe '#perform' do
     subject(:perform) { worker.perform }
 
-    before do
-      # https://github.com/aws/aws-sdk-ruby/issues/1371#issuecomment-269742397
-      client_stub = Aws::S3::Client.new(stub_responses: true)
-      expect(Aws::S3::Resource).
-        to receive(:new).
-        and_return(Aws::S3::Resource.new(client: client_stub))
-    end
+    context 'when the `disable_run_heat_worker` feature flag is disabled' do
+      before do
+        # https://github.com/aws/aws-sdk-ruby/issues/1371#issuecomment-269742397
+        client_stub = Aws::S3::Client.new(stub_responses: true)
+        expect(Aws::S3::Resource).
+          to receive(:new).
+          and_return(Aws::S3::Resource.new(client: client_stub))
+      end
 
-    around do |spec|
-      ClimateControl.modify(S3_BUCKET: 'david-runger-uploads') do
-        spec.run
+      around do |spec|
+        ClimateControl.modify(S3_BUCKET: 'david-runger-uploads') do
+          spec.run
+        end
+      end
+
+      it 'downloads, zips, and uploads' do
+        expect(worker).to receive(:system).with('bin/heat')
+        expect(worker).to receive(:system).with(%r{\Acd tmp/heat/images/ && zip -r \w+})
+        # rubocop:disable RSpec/AnyInstance
+        expect_any_instance_of(Aws::S3::Object).to receive(:upload_file)
+        # rubocop:enable RSpec/AnyInstance
+
+        perform
       end
     end
 
-    it 'downloads, zips, and uploads' do
-      expect(worker).to receive(:system).with('bin/heat')
-      expect(worker).to receive(:system).with(%r{\Acd tmp/heat/images/ && zip -r \w+})
-      # rubocop:disable RSpec/AnyInstance
-      expect_any_instance_of(Aws::S3::Object).to receive(:upload_file)
-      # rubocop:enable RSpec/AnyInstance
+    context 'when the `disable_run_heat_worker` feature flag is enabled' do
+      before { Flipper.enable(:disable_run_heat_worker) }
 
-      perform
+      it 'does not execute any system commands' do
+        expect(worker).not_to receive(:system)
+
+        perform
+      end
     end
   end
 end
