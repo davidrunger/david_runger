c9411be03f192722e437117931a8e5e4c87ecab9
David Runger <davidjrunger@gmail.com>
2020-07-04 10:33:16 -0700

    Fix comment about number of connections for $redis_pool (4, not 2)
    (The number of connections (per server process) was changed from 2 to 4
    in f5e3794 but we failed to also update the comment at that time.)

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-# See comments in config/initializers/sidekiq.rb for rationale re: pool size of 2.
+# See comments in config/initializers/sidekiq.rb for rationale re: pool size of 4.
 $redis_pool = ConnectionPool.new(size: 4, timeout: 1) { Redis.new }
