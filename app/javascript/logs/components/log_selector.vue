davidrunger/david_runger

commit 4c9d198a8c0dc76c0aaf8d83443bf9b9f92b28cb

Close logs quick-selector when route changes; stop managing manually
This change fixes a bug wherein command-clicking (Mac) a log link from
within the logs quick-selector would close the quick-selector. The quick
selector should remain open so that multiple logs can be opened from the
quick selector via command-clicking.

Since command-clicking doesn't trigger a route change within the current
tab, this change (close the logs quick-selector when the route changes,
and stop managing the appearance of the quick-selector manually) results
in the quick selector remaining open when command-clicking links, as
desired.

At first I was going to move the quick-selector state
(`highlightedLogIndex`, `searchString`) into the Vuex store, so that the
router could dispatch Vuex actions to reset the quick selector state
when the route changes.

However, I ended up instead just having the router emit a
`'groceries:route-changed'` event that can be listened for by the quick
selector, and then the quick selector can manage resetting its own
state. This was a simpler/smaller change, and I think it largely "feels
right" in terms of the router shouldn't necessarily "know" about Vuex
actions to update the state of the logs quick-selector.

app/javascript/logs/components/log_selector.vue
@@ -19,13 +19,14 @@ Modal(
         router-link.log-link(
           :to='{ name: "log", params: { slug: log.slug }}'
           :class='{bold: (index === highlightedLogIndex)}'
-          @click.native='resetQuickSelector'
         ) {{log.name}}
 </template>
 
 <script>
 import { mapGetters, mapState } from 'vuex';
 import FuzzySet from 'fuzzyset.js';
+
+import { on } from 'lib/event_bus';
 
 export default {
   computed: {
@@ -62,6 +63,11 @@ export default {
     this.logNames.forEach(logName => {
       this.fuzzySet.add(logName);
     });
+    this.unsubscribeFromRouteChanges = on('groceries:route-changed', this.resetQuickSelector);
+  },
+
+  destroyed() {
+    this.unsubscribeFromRouteChanges();
   },
 
   data() {
@@ -103,7 +109,6 @@ export default {
 
     selectLog(log) {
       this.$router.push({ name: 'log', params: { slug: log.slug }});
-      this.resetQuickSelector();
     },
   },
 

app/javascript/logs/router.js
@@ -2,6 +2,7 @@ import VueRouter from 'vue-router';
 
 import Log from 'logs/components/log.vue';
 import LogsIndex from 'logs/components/logs_index.vue';
+import { emit } from 'lib/event_bus';
 
 const routes = [
   { path: '/logs',  name: 'logs-index', component: LogsIndex },
@@ -11,6 +12,11 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  emit('groceries:route-changed');
+  next();
 });
 
 export default router;

commit ba282fc0f306b250efbe3613ad8add8a0103dcaa

Fix event name (groceries:route-changed => logs:route-changed)
This happens in the logs app; it appears that this is a typo that has
existed since this code was introduced in 4c9d198 .

app/javascript/logs/components/log_selector.vue
@@ -61,7 +61,7 @@ export default {
     this.logNames.forEach(logName => {
       this.fuzzySet.add(logName);
     });
-    this.unsubscribeFromRouteChanges = on('groceries:route-changed', this.resetQuickSelector);
+    this.unsubscribeFromRouteChanges = on('logs:route-changed', this.resetQuickSelector);
   },
 
   destroyed() {

app/javascript/logs/router.js
@@ -16,7 +16,7 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  emit('groceries:route-changed');
+  emit('logs:route-changed');
   next();
 });
 