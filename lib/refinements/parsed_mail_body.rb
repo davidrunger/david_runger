f6328098d21a17cd9685f5e0b1a04d5a09c0650c
David Runger <davidjrunger@gmail.com>
2020-07-10 14:55:01 -0700

    Improve email parsing
    Sorry that I'm not providing any test cases to demonstrate the
    improvement :( but it's hard to generate such test cases without
    revealing personal info.

--- lib/refinements/parsed_mail_body.rb
+++ lib/refinements/parsed_mail_body.rb
@@ -5,10 +5,12 @@ module ParsedMailBody
     def parsed_body
       # call #dup because ::trim modifies the string (via at least one `#gsub!` call)
       unparsed_body = (text_part.presence || body).to_s.dup
+      # trim header content from the beginning of the body
+      unparsed_body.sub!(/\A[\s\S]*^Content-Transfer-Encoding:.+\n+/, '')
+      # decode quoted-printable encoding
+      unparsed_body = unparsed_body.unpack1('M').force_encoding('utf-8')
       # trim content from the end of the body ("On [date/time] [person/email] wrote:[...]")
       EmailReplyTrimmer.trim(unparsed_body).rstrip.
-        # trim header content from the beginning of the body
-        sub(/\A[\s\S]*^Content-Transfer-Encoding:.+\n+/, '').
         # remove newlines that were added just to break up long lines
         gsub(/\S+\n\S+/) do |match|
           word_before_newline = match.split("\n").first
@@ -25,7 +27,7 @@ module ParsedMailBody
     private
 
     # this method tries to determine words that the user _actually_ put before a newline, based on
-    # the idea that such words will be followed by a `<` character (from a `<br>` or a `</div>`)
+    # the idea that such words will be followed by a `<br` or a `</div`) in the `html_part`
     def words_truly_before_newlines
       @words_truly_before_newlines ||=
         begin
@@ -34,11 +36,12 @@ module ParsedMailBody
               to_s.
               sub(/\A[\s\S]*^Content-Transfer-Encoding:.+[\r\n]+/, '').
               unpack1('M').
+              force_encoding('utf-8').
               delete("\r").
               rstrip
           nokogiri_doc = Nokogiri.parse(decoded_html)
           nokogiri_doc.css('.gmail_quote').remove
-          Set.new(nokogiri_doc.to_s.scan(/[^<>\s]+(?=<)/))
+          Set.new(nokogiri_doc.to_s.scan(/[^<>\s]+(?=<br|<\/div)/))
         end
     end
   end
