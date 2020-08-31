b94a11f12dc4da8ccaff2f4a8e1f264963f27dd5
David Runger <davidjrunger@gmail.com>
2020-08-31 14:49:54 -0700

    Loosen IP validation format of `StoreRequestBlockInRedis` for IPv6
    fixes https://rollbar.com/davidjrunger/davidrunger/items/317/

--- app/actions/ip_blocks/store_request_block_in_redis.rb
+++ app/actions/ip_blocks/store_request_block_in_redis.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class IpBlocks::StoreRequestBlockInRedis < ApplicationAction
-  requires :ip, String, format: { with: /[.0-9]{7,15}/ }
+  requires :ip, String, format: { with: /[.:0-9a-z]{7,39}/ }
   requires :path, String, format: { with: %r{\A/} }
 
   def execute
