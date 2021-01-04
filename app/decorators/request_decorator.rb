d80c2e54c7d53c857684b353c11dac15f062e089
David Runger <davidjrunger@gmail.com>
2021-01-03 21:18:53 -0800

    Add `draper` gem (& use to decorate user agent of Requests in admin)

---
+++ app/decorators/request_decorator.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class RequestDecorator < ApplicationDecorator
+  extend Memoist
+
+  delegate_all
+
+  def pretty_user_agent
+    if good_browser_data?
+      "#{browser_name} #{browser_version} on #{browser_platform}"
+    else
+      user_agent
+    end
+  end
+
+  private
+
+  def good_browser_data?
+    [browser_name, browser_version, browser_platform].all?(&:present?) &&
+      browser_name != 'Unknown Browser'
+  end
+
+  memoize \
+  def browser
+    Browser.new(user_agent)
+  end
+
+  def browser_name
+    browser.name
+  end
+
+  def browser_version
+    browser.version
+  end
+
+  def browser_platform
+    browser.platform.name
+  end
+end
