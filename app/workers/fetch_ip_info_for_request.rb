e366f95743ece64abaf75871dd991b63bfcfea39
David Runger <davidjrunger@gmail.com>
2021-01-28 02:34:46 -0800

    Use cache #fetch (not #read and #write) in FetchIpInfoForRequest

--- app/workers/fetch_ip_info_for_request.rb
+++ app/workers/fetch_ip_info_for_request.rb
@@ -4,8 +4,6 @@ class FetchIpInfoForRequest
   extend Memoist
   prepend ApplicationWorker
 
-  class BlankIpInfo < StandardError ; end
-
   LOCAL_IPS = %w[::1 127.0.0.1].map(&:freeze).freeze
 
   def perform(request_id)
@@ -19,9 +17,7 @@ class FetchIpInfoForRequest
     ip = request.ip
     return if ip.blank? || (Rails.env.development? && ip.in?(LOCAL_IPS))
 
-    ip_info = Rails.cache.read(cache_key(ip)) || ip_info_from_api(ip)
-    fail(BlankIpInfo, "Blank IP info for IP '#{ip}'") if ip_info.blank? # trigger Sidekiq retry
-
+    ip_info = Rails.cache.fetch(cache_key(ip), expires_in: 4.weeks) { ip_info_from_api(ip) }
     request.update!(ip_info)
   end
 
@@ -35,13 +31,9 @@ class FetchIpInfoForRequest
     raw_ip_info_from_api = Faraday.json_connection.get("http://ip-api.com/json/#{ip}").body
     isp, city, state, country = raw_ip_info_from_api.values_at(*%w[isp city region countryCode])
 
-    parsed_ip_info = {
+    {
       location: [city, state, country].reject(&:blank?).join(', '),
       isp: isp,
     }
-
-    Rails.cache.write(cache_key(ip), parsed_ip_info, expires_in: 4.weeks)
-
-    parsed_ip_info
   end
 end
