c2d034641cad8b2e59b4c66b82de4914331458da
David Runger <davidjrunger@gmail.com>
2021-01-29 09:15:29 -0800

    Use Heroku app name as hashid salt for review apps
    This eliminates the need to set a HASHID_SALT ENV var for review apps.

--- config/initializers/hashid.rb
+++ config/initializers/hashid.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Hashid::Rails.configure do |config|
-  config.salt = ENV.fetch('HASHID_SALT')
+  config.salt = ENV['HEROKU_APP_NAME'].presence || ENV.fetch('HASHID_SALT')
   # we'll only use lowercase letters (default setting is to use capitals, as well)
   config.alphabet = (('a'..'z').to_a + ('0'..'9').to_a).join('').freeze
 end
