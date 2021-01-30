b0dc0b62cd53d80efd135f85249171dcbc52fe88
David Runger <davidjrunger@gmail.com>
2021-01-29 16:47:24 -0800

    Don't load `element-plus` unnecessarily for the Home app
    We'll load specifically just `ElCard` and its CSS, since this is the
    only `element-plus` element that the Home app actually uses.

--- app/javascript/packs/logs_app.js
+++ app/javascript/packs/logs_app.js
@@ -1,9 +1,11 @@
 import { renderApp } from 'shared/customized_vue';
+import { useElementPlus } from 'shared/element_plus';
 import LogApp from 'logs/logs.vue';
 import storeDefinition from 'logs/store';
 import router from 'logs/router';
 
-renderApp(LogApp, {
+const app = renderApp(LogApp, {
   router,
   storeDefinition,
 });
+useElementPlus(app);
