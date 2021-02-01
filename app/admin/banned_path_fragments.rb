a2fe4130b6ca5f3b6da69c07956e21a9489c86b2
David Runger <davidjrunger@gmail.com>
2021-02-01 02:15:03 -0800

    Respect newlines in admin for `BannedPathFragment#notes`

--- app/admin/banned_path_fragments.rb
+++ app/admin/banned_path_fragments.rb
@@ -2,4 +2,23 @@
 
 ActiveAdmin.register(BannedPathFragment) do
   permit_params :value, :notes
+
+  index do
+    id_column
+    column :value
+    column(:notes, class: 'pre-wrap')
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :value
+      row(:notes, class: 'pre-wrap')
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
 end
