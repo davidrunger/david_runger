0cf7f2857a4a7a8d89e100c4166d3b460b02f09e
David Runger <davidjrunger@gmail.com>
2021-02-06 17:59:09 -0800

    Use `RequestStore` to memoize `RedisConfig` for only one request
    Memoizing indefinitely in a simple Hash was somewhat problematic in
    that, when a setting was modified in the admin web app, that change
    wouldn't be picked up by Sidekiq until after a restart (thereby clearing
    the RedisConfig memory store in Sidekiq and forcing loading from Redis).

--- lib/redis_config/redis_backed_map.rb
+++ lib/redis_config/redis_backed_map.rb
@@ -8,27 +8,27 @@ class RedisConfig::RedisBackedMap
   end
 
   def []=(key, value)
-    $redis_pool.with { |conn| conn.hset(redis_hash_key, key, value) }
-    @memory_store[key] = value
+    $redis_pool.with { |conn| conn.hset(namespace, key, value) }
+    memory_store[key] = value
   end
 
   def delete(key)
-    $redis_pool.with { |conn| conn.hdel(redis_hash_key, key) }
+    $redis_pool.with { |conn| conn.hdel(namespace, key) }
     memory_store.delete(key)
   end
 
   def clear!
-    $redis_pool.with { |conn| conn.del(redis_hash_key) }
-    @memory_store = nil
+    $redis_pool.with { |conn| conn.del(namespace) }
+    RequestStore.store[namespace] = nil
   end
 
   private
 
   def memory_store
-    @memory_store ||= $redis_pool.with { |conn| conn.hgetall(redis_hash_key) }
+    RequestStore.store[namespace] ||= $redis_pool.with { |conn| conn.hgetall(namespace) }
   end
 
-  def redis_hash_key
+  def namespace
     "#{RedisConfig::REDIS_NAMESPACE}:#{@name}-map"
   end
 end
