554d86d7fe9c7cca0c891042bb0d9f2169a2d337
David Runger <davidjrunger@gmail.com>
2021-02-17 15:18:36 -0800

    Render Rollbar UUID in 500 error page

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -9,7 +9,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
   if Rails.env.in?(%w[development test])
-    config.enabled = false
+    config.enabled = ENV['ROLLBAR_ACCESS_TOKEN'].present?
   end
 
   # By default, Rollbar will try to call the `current_user` controller method
---
+++ spec/controllers/errors_controller_spec.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+RSpec.describe ErrorsController do
+  describe '#not_found' do
+    subject(:get_not_found) { get(:not_found) }
+
+    it %(says "The page you were looking for doesn't exist.") do
+      get_not_found
+      expect(response.body).to have_text("The page you were looking for doesn't exist.")
+    end
+  end
+
+  describe '#unacceptable' do
+    subject(:get_unacceptable) { get(:unacceptable) }
+
+    it 'says "The change you wanted was rejected."' do
+      get_unacceptable
+      expect(response.body).to have_text('The change you wanted was rejected.')
+    end
+  end
+
+  describe '#internal_error' do
+    subject(:get_internal_error) { get(:internal_error) }
+
+    it 'says "Sorry, something went wrong."' do
+      get_internal_error
+      expect(response.body).to have_text('Sorry, something went wrong.')
+    end
+
+    context 'when `rollbar.exception_uuid` has been set in `request.env`' do
+      before { request.env['rollbar.exception_uuid'] = rollbar_uuid }
+
+      let(:rollbar_uuid) { SecureRandom.uuid }
+
+      it 'mentions the Rollbar UUID' do
+        get_internal_error
+        expect(response.body).to have_text("Please provide this error ID: #{rollbar_uuid}")
+      end
+    end
+  end
+
+  context 'when the request format is json', request_format: :json do
+    describe '#not_found' do
+      subject(:get_not_found) { get(:not_found) }
+
+      it %(says "The page you were looking for doesn't exist.") do
+        get_not_found
+        expect(json_response).to eq({ 'error' => 'Resource not found' })
+      end
+    end
+
+    describe '#unacceptable' do
+      subject(:get_unacceptable) { get(:unacceptable) }
+
+      it 'says "The change you wanted was rejected."' do
+        get_unacceptable
+        expect(json_response).to eq({ 'error' => 'Params unacceptable' })
+      end
+    end
+
+    describe '#internal_error' do
+      subject(:get_internal_error) { get(:internal_error) }
+
+      it 'says "Sorry, something went wrong."' do
+        get_internal_error
+        expect(json_response).to eq({ 'error' => 'Internal server error' })
+      end
+    end
+  end
+end
