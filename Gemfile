f1da74f4884e2bad07592885a8d5395df141f1e8
David Runger <davidjrunger@gmail.com>
2021-04-08 07:27:41 -0700

    Bump Ruby from 3.0.0 to 3.0.1
    Also, bump yarn from 1.22.5 to 1.22.10 . (GitHub Actions now seems to
    have this newer yarn version for some reason?)

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 source 'https://rubygems.org'
-ruby '3.0.0'
+ruby '3.0.1'
 
 gem 'active_actions', github: 'davidrunger/active_actions'
 gem 'activeadmin'
--- bin/test/tasks/check_versions.rb
+++ bin/test/tasks/check_versions.rb
@@ -5,13 +5,13 @@ class Test::Tasks::CheckVersions < Pallets::Task
 
   def run
     execute_system_command(<<~COMMAND)
-      ruby --version && [ "$(ruby --version | cut -c1-11)" = 'ruby 3.0.0p' ]
+      ruby --version && [ "$(ruby --version | cut -c1-11)" = 'ruby 3.0.1p' ]
     COMMAND
     execute_system_command(<<~COMMAND)
       node --version && [ "$(node --version)" = 'v14.15.0' ]
     COMMAND
     execute_system_command(<<~COMMAND)
-      yarn --version && [ "$(yarn --version)" = '1.22.5' ]
+      yarn --version && [ "$(yarn --version)" = '1.22.10' ]
     COMMAND
   end
 end
