f463282db48795f289ff95dd34d09ccd6269ee31
David Runger <davidjrunger@gmail.com>
2022-05-28 21:20:55 -0500

    Store asset size timeseries in Postgres rather than Redis
    Storing them in Redis was using too much memory when reading the values
    because the timestamps and values were stored as strings. By storing
    them in a Postgres JSON array, we can leave them as integers and floats,
    saving memory when reading the values. I think inserts will be a bit
    slower and use more memory, but that's okay.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_231420) do
+ActiveRecord::Schema.define(version: 2022_05_29_020803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -235,6 +235,13 @@ ActiveRecord::Schema.define(version: 2022_04_22_231420) do
     t.datetime "updated_at", null: false
     t.string "note"
     t.index ["log_id"], name: "index_text_log_entries_on_log_id"
+  end
+
+  create_table "timeseries", force: :cascade do |t|
+    t.text "name", null: false
+    t.json "measurements", default: [], null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
