c8baa3f0f6c79b4c15b119cbb1779d4029614c9f
David Runger <davidjrunger@gmail.com>
2022-06-13 07:12:03 -0500

    Add back postcss as a development dependency
    This fixes the stylelint check, which has been broken.

--- bin/test/tasks/run_stylelint.rb
+++ bin/test/tasks/run_stylelint.rb
@@ -4,6 +4,8 @@ class Test::Tasks::RunStylelint < Pallets::Task
   include Test::TaskHelpers
 
   def run
-    execute_system_command('./node_modules/.bin/stylelint app/**/*.{css,scss,vue} --max-warnings 0')
+    execute_system_command(
+      './node_modules/.bin/stylelint "app/**/*.{css,scss,vue}" --max-warnings 0',
+    )
   end
 end
