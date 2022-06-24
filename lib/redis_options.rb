ab55eba080536ef34c3441ef4986172a5ddfba1d
David Runger <davidjrunger@gmail.com>
2022-06-24 07:35:08 -0500

    Use ENV vars (not encrypted credentials) for Redis and Postgres
    Heroku changes the values of these ENV vars sometimes. We need our app
    to be able to pick up those changes immediately.

--- lib/redis_options.rb
+++ lib/redis_options.rb
@@ -5,15 +5,8 @@ module RedisOptions
     url_base =
       case Rails.env
       when 'development', 'test' then 'redis://localhost:6379'
-      else production_redis_url
+      else ENV.fetch('REDIS_TLS_URL')
       end
     { url: "#{url_base}/#{db}", ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-  end
-
-  def self.production_redis_url
-    ENV.fetch('EMERGENCY_REDIS_TLS_URL', nil) ||
-      Rails.application.credentials.redis&.fetch(:tls_url) ||
-      # check the ENV var for Heroku review app deployments (since they cannot decode credentials)
-      ENV.fetch('REDIS_TLS_URL', nil)
   end
 end
