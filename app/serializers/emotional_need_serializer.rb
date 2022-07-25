d67169899fb3ff3246d420ea766c286fef068d46
David Runger <davidjrunger@gmail.com>
2022-07-25 11:35:21 -0500

    Run annotate when files have been added in app/serializers etc

--- app/serializers/emotional_need_serializer.rb
+++ app/serializers/emotional_need_serializer.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: emotional_needs
+#
+#  created_at  :datetime         not null
+#  description :text
+#  id          :bigint           not null, primary key
+#  marriage_id :bigint           not null
+#  name        :string
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_emotional_needs_on_marriage_id  (marriage_id)
+#
 class EmotionalNeedSerializer < ActiveModel::Serializer
   attributes :description, :id, :name
 end
