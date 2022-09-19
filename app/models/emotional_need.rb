a12cb14ee56eba9faad180e89174cfb32c75bfd4
David Runger <davidjrunger@gmail.com>
2022-09-19 10:56:48 -0500

    Add validations for EmotionalNeed name

--- app/models/emotional_need.rb
+++ app/models/emotional_need.rb
@@ -8,14 +8,16 @@
 #  description :text
 #  id          :bigint           not null, primary key
 #  marriage_id :bigint           not null
-#  name        :string
+#  name        :string           not null
 #  updated_at  :datetime         not null
 #
 # Indexes
 #
-#  index_emotional_needs_on_marriage_id  (marriage_id)
+#  index_emotional_needs_on_marriage_id_and_name  (marriage_id,name) UNIQUE
 #
 class EmotionalNeed < ApplicationRecord
   belongs_to :marriage
   has_many :need_satisfaction_ratings, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { scope: :marriage }
 end
