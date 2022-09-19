davidrunger/david_runger

commit 2398cb688129ff99cd8db432ac97d6d6459d9436

Show year in text log timestamp

app/javascript/logs/components/data_renderers/text_log.vue
@@ -3,7 +3,7 @@
   .container
     table.text-log-table
       EditableTextLogRow(
-        v-for='logEntry in formattedLogEntries'
+        v-for='logEntry in sortedLogEntries'
         :key='logEntry.id'
         :logEntry='logEntry'
       )
@@ -12,14 +12,8 @@
 </template>
 
 <script>
-import createDOMPurify from 'dompurify';
-import { marked } from 'marked';
-import strftime from 'strftime';
-
 import EditableTextLogRow from '@/logs/components/editable_text_log_row.vue';
 import NewLogEntryForm from '@/logs/components/new_log_entry_form.vue';
-
-const DOMPurify = createDOMPurify(window);
 
 export default {
   components: {
@@ -28,7 +22,7 @@ export default {
   },
 
   computed: {
-    formattedLogEntries() {
+    sortedLogEntries() {
       let logEntriesToShow;
       if (this.showAllEntries || (this.log_entries.length <= 3)) {
         logEntriesToShow = this.log_entries;
@@ -36,15 +30,7 @@ export default {
         logEntriesToShow = this.log_entries.slice(this.log_entries.length - 3);
       }
 
-      const sortedAndFormattedEntries =
-        logEntriesToShow.map(logEntry => ({
-          id: logEntry.id,
-          createdAt: strftime('%b %-d %-l:%M%P', new Date(logEntry.created_at)),
-          plaintext: logEntry.data,
-          html: DOMPurify.sanitize(marked(logEntry.data)),
-        })).reverse();
-
-      return sortedAndFormattedEntries;
+      return logEntriesToShow.reverse();
     },
   },
 
@@ -124,7 +110,7 @@ table.text-log-table {
   }
 
   td {
-    padding: 10px 0;
+    padding: 5px 10px;
     word-break: initial;
     white-space: pre-wrap;
     line-height: 1.13rem;

app/javascript/logs/components/editable_text_log_row.vue
@@ -1,10 +1,10 @@
 <template lang='pug'>
 tr
-  td {{logEntry.createdAt}}
+  td(v-html='formattedCreatedAt')
 
   td(v-if='editing')
     el-input(v-model='newPlaintext' type='textarea' ref='textInput')
-  td.left-align(v-else v-html='logEntry.html')
+  td.left-align(v-else v-html='html')
 
   td(v-if='editing')
     el-button(@click='updateLogEntry' size='small') Save
@@ -14,17 +14,33 @@ tr
 </template>
 
 <script>
+import createDOMPurify from 'dompurify';
+import { marked } from 'marked';
+import strftime from 'strftime';
+
+const DOMPurify = createDOMPurify(window);
+
 export default {
+  computed: {
+    formattedCreatedAt() {
+      return strftime('%b %-d, %Y at&nbsp;%-l:%M%P', new Date(this.logEntry.created_at));
+    },
+
+    html() {
+      return DOMPurify.sanitize(marked(this.logEntry.data));
+    },
+  },
+
   data() {
     return {
       editing: false,
-      newPlaintext: this.logEntry.plaintext.slice(),
+      newPlaintext: this.logEntry.data.slice(),
     };
   },
 
   methods: {
     cancelEditing() {
-      this.newPlaintext = this.logEntry.plaintext.slice(); // undo any changes made
+      this.newPlaintext = this.logEntry.data.slice(); // undo any changes made
       this.editing = false;
     },
 

commit 1d33534209cb6f56245c76398cdb38954819c029

Don't reverse ordering of logs after editing one
This fixes a bug introduced in 2398cb6 (#1690).

app/javascript/logs/components/data_renderers/text_log.vue
@@ -30,7 +30,7 @@ export default {
         logEntriesToShow = this.log_entries.slice(this.log_entries.length - 3);
       }
 
-      return logEntriesToShow.reverse();
+      return logEntriesToShow.slice().reverse();
     },
   },
 

spec/features/logs_spec.rb
@@ -34,12 +34,13 @@ RSpec.describe 'Logs app' do
       context 'when there are no entries yet for the text log' do
         before { log.log_entries.find_each(&:destroy!) }
 
-        it 'allows the user to submit their first and second entry for the log' do
+        it 'allows the user to submit log entries and edit them' do
           visit(log_path(slug: log.slug))
 
+          # Add first log entry
           first_log_entry_text = 'Some great text log entry content!'
           expect {
-            first(:css, '.new-log-input textarea').native.send_keys(first_log_entry_text)
+            first('.new-log-input textarea').native.send_keys(first_log_entry_text)
             click_button('Add')
             expect(page).to have_text(first_log_entry_text) # wait for AJAX request to complete
           }.to change {
@@ -49,9 +50,10 @@ RSpec.describe 'Logs app' do
           last_log_entry = log.log_entries.reorder(:created_at).last!
           expect(last_log_entry.data).to eq(first_log_entry_text)
 
+          # Add second log entry
           second_log_entry_text = 'Even more great content!'
           expect {
-            first(:css, '.new-log-input textarea').native.send_keys(second_log_entry_text)
+            first('.new-log-input textarea').native.send_keys(second_log_entry_text)
             click_button('Add')
             expect(page).to have_text(second_log_entry_text) # wait for AJAX request to complete
             expect(page).to have_text(first_log_entry_text) # confirm first log entry's still there
@@ -61,6 +63,15 @@ RSpec.describe 'Logs app' do
 
           last_log_entry = log.log_entries.reorder(:created_at).last!
           expect(last_log_entry.data).to eq(second_log_entry_text)
+
+          # Edit second log entry (and verify that correct order is preserved)
+          first('a', text: 'Edit').click
+          added_edit_text = ' Added text!'
+          first('.text-log-table textarea').send_keys(added_edit_text)
+          first('button', text: 'Save').click
+          expect(page).to have_text(
+            /#{second_log_entry_text}#{added_edit_text}.*#{first_log_entry_text}/,
+          )
         end
       end
 