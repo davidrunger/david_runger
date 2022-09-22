45a0faa2f88c2954a450a942e354fbce70c88cb2
David Runger <davidjrunger@gmail.com>
2022-09-22 11:01:09 -0500

    Remove detectify site verification route

--- spec/features/site_ownership_verification_spec.rb
+++ spec/features/site_ownership_verification_spec.rb
@@ -8,12 +8,4 @@ RSpec.describe 'site ownership verification', :rack_test_driver do
       expect(page.body).to eq('google-site-verification: google83c07e1014ea4a70.html')
     end
   end
-
-  describe 'detectify' do
-    it 'responds with the detectify site verification code' do
-      visit('7ee411f3bc50160c876708bdda166787.txt')
-
-      expect(page.body).to eq('detectify')
-    end
-  end
 end
