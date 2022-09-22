aa8a1d14ed45034667ffafda0f3916b19c4fe5f9
David Runger <davidjrunger@gmail.com>
2022-09-21 22:09:44 -0500

    Add detectify route verification

--- spec/features/google_verification_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe 'Google site verification', :rack_test_driver do
-  it 'responds with the Google site verification code' do
-    visit('google83c07e1014ea4a70')
-
-    expect(page.body).to eq('google-site-verification: google83c07e1014ea4a70.html')
-  end
-end
