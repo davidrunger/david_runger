ee320fbbdb8245534e885c0ba95de0cf38c56ceb
David Runger <davidjrunger@gmail.com>
2022-09-22 13:58:34 -0500

    Enable CSP by setting a "Content-Securit-Policy" header

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,22 +6,25 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap and inline scripts
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    extra_sources = (Rails.env.development? && !ENV.key?('PRODUCTION_ASSET_CONFIG')) ? %i[ws] : []
+
+    policy.default_src(:self, :https, *extra_sources)
+    policy.font_src(:self, :https, :data, *extra_sources)
+    policy.img_src(:self, :https, :data, *extra_sources)
+    policy.object_src(:none)
+    policy.script_src(:self, :https, *extra_sources)
+    policy.style_src(:self, :https, :unsafe_inline, *extra_sources)
+    # Specify URI for violation reports
+    # policy.report_uri "/csp-violation-report-endpoint"
+  end
+
+  # Generate session nonces for permitted importmap and inline scripts
+  config.content_security_policy_nonce_generator =
+    ->(request) { request.session.id.to_s.presence || SecureRandom.base64(16) }
+  config.content_security_policy_nonce_directives = %w[script-src]
+
+  # Report violations without enforcing the policy.
+  # config.content_security_policy_report_only = true
+end
