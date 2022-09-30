08f91b1f16d8dae8101b0d3ac11903e51d436c4d
David Runger <davidjrunger@gmail.com>
2022-09-30 03:41:16 -0500

    Send email attachments to Mailgun

--- spec/lib/email/mailgun_via_http_spec.rb
+++ spec/lib/email/mailgun_via_http_spec.rb
@@ -1,50 +1,33 @@
 # frozen_string_literal: true
 
 RSpec.describe Email::MailgunViaHttp do
+  include MailSpecHelpers
+
   subject(:mailgun_via_http) { Email::MailgunViaHttp.new({}) }
 
   describe '#deliver!' do
     subject(:deliver!) { mailgun_via_http.deliver!(mail) }
 
-    let(:mail) do
-      instance_double(
-        ::Mail::Message,
-        subject: email_subject,
-        body: email_body,
-        to: [ugly_to_email],
-        from: [ugly_from_email],
-        reply_to: [ugly_reply_to_email],
-      )
-    end
-    let(:email_subject) { "There's a new davidrunger.com user! :) Email: davidjrunger@gmail.com." }
-    let(:email_body) { 'A new user has been created with email davidjrunger@gmail.com!' }
-    let(:ugly_from_email) { 'reply@davidrunger.com' }
-    let(:pretty_from_email) { "DavidRunger.com <#{ugly_from_email}>" }
-    let(:ugly_to_email) { 'davidjrunger@gmail.com' }
-    let(:pretty_to_email) { "David Runger <#{ugly_to_email}>" }
-    let(:ugly_reply_to_email) { 'reply@mg.davidrunger.com' }
-    let(:pretty_reply_to_email) { "DavidRunger.com <#{ugly_reply_to_email}>" }
+    let(:mail) { mail_from_raw_email_fixture('body_and_attachment') }
     let(:stubbed_mailgun_api_key) { '2a4d89d1-1984-4453-8ea5-2468d1769a6c' }
     let!(:mailgun_http_request) do
       stub_request(
         :post,
         'https://api.mailgun.net/v3/mg.davidrunger.com/messages',
       ).with(
-        body: Faraday::NestedParamsEncoder.encode(
-          to: pretty_to_email,
-          subject: email_subject,
-          from: pretty_from_email,
-          'h:Reply-To' => pretty_reply_to_email,
-          html: email_body,
-        ),
         headers: {
           'Accept' => '*/*',
           'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
           'Authorization' => 'Basic YXBpOjJhNGQ4OWQxLTE5ODQtNDQ1My04ZWE1LTI0NjhkMTc2OWE2Yw==',
-          'Content-Type' => 'application/x-www-form-urlencoded',
+          'Content-Type' =>
+            %r{multipart/form-data; boundary=-----------RubyMultipartPost-[0-9a-f]{32}},
           'User-Agent' => /Faraday v\d+\.\d+\.\d+/,
         },
-      ).to_return(
+      ) do |request|
+        # https://github.com/bblimke/webmock/issues/ 623#issuecomment-536603971
+        request.body.force_encoding('BINARY')
+        request.body.include?('attached a zip file')
+      end.to_return(
         status: 200,
         body: '',
         headers: {},
@@ -52,17 +35,6 @@ RSpec.describe Email::MailgunViaHttp do
     end
 
     before do
-      expect(mail).to receive(:[]).at_least(:once) do |key|
-        case key
-        when 'From' then pretty_from_email
-        when 'To' then pretty_to_email
-        when 'Subject' then email_subject
-        when 'Reply-To' then pretty_reply_to_email
-        else
-          raise('Unexpected key accessed on mail object')
-        end
-      end
-
       expect(Rails.application.credentials).to receive(:mailgun!).
         and_return(api_key: stubbed_mailgun_api_key)
     end
