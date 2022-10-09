02031e6c7a30fae754fb9bd4090f8f83a20d8898
David Runger <davidjrunger@gmail.com>
2022-10-09 11:36:35 -0500

    Use skedjewel (Crystal binary) rather than schedjewel (Ruby gem)
    This should save some memorry. `schedjewel` uses something like 22MB. I
    think that `skedjewel` will use less than 2 MB.

--- spec/config/scheduled_sidekiq_jobs_spec.rb
+++ spec/config/scheduled_sidekiq_jobs_spec.rb
@@ -3,9 +3,9 @@
 # rubocop:disable RSpec/DescribeClass
 RSpec.describe 'Sidekiq scheduled jobs' do
   # rubocop:enable RSpec/DescribeClass
-  subject(:schedule) { YAML.load(ERB.new(File.read('config/schedjewel.yml')).result) }
+  subject(:schedule) { YAML.load_file('config/skedjewel.yml') }
 
   specify 'all scheduled Sidekiq job classes are indeed defined' do
-    expect { schedule['jobs'].map { |job_hash| job_hash['job'].constantize } }.not_to raise_error
+    expect { schedule['jobs'].keys.map(&:constantize) }.not_to raise_error
   end
 end
