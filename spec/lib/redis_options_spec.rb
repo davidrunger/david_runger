2801e438c5b536719f700ed4eaa85bd109cb2959
David Runger <davidjrunger@gmail.com>
2022-10-14 01:25:04 -0500

    Increase branch coverage to 100% for RedisOptions

--- spec/lib/redis_options_spec.rb
+++ spec/lib/redis_options_spec.rb
@@ -50,4 +50,52 @@ RSpec.describe RedisOptions do
       end
     end
   end
+
+  describe '#default_db_number' do
+    subject(:default_db_number) { redis_options.send(:default_db_number) }
+
+    context 'when not providing a database number' do
+      subject(:redis_options) { RedisOptions.new }
+
+      context 'when DB_SUFFIX is _unit' do
+        around { |spec| ClimateControl.modify(DB_SUFFIX: '_unit') { spec.run } }
+
+        it 'returns 4' do
+          expect(default_db_number).to eq(4)
+        end
+      end
+
+      context 'when DB_SUFFIX is _api' do
+        around { |spec| ClimateControl.modify(DB_SUFFIX: '_api') { spec.run } }
+
+        it 'returns 5' do
+          expect(default_db_number).to eq(5)
+        end
+      end
+
+      context 'when DB_SUFFIX is _html' do
+        around { |spec| ClimateControl.modify(DB_SUFFIX: '_html') { spec.run } }
+
+        it 'returns 6' do
+          expect(default_db_number).to eq(6)
+        end
+      end
+
+      context 'when DB_SUFFIX is _feature' do
+        around { |spec| ClimateControl.modify(DB_SUFFIX: '_feature') { spec.run } }
+
+        it 'returns 7' do
+          expect(default_db_number).to eq(7)
+        end
+      end
+
+      context 'when DB_SUFFIX is _unexpected' do
+        around { |spec| ClimateControl.modify(DB_SUFFIX: '_unexpected') { spec.run } }
+
+        it 'raises an error' do
+          expect { default_db_number }.to raise_error('Unexpected DB_SUFFIX!')
+        end
+      end
+    end
+  end
 end
