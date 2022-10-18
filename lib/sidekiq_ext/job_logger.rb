7e3aa6a3d0bb79aea909447f3f2a11848b516eaa
David Runger <davidjrunger@gmail.com>
2022-10-17 21:45:53 -0500

    Log Sidekiq job arguments and queue

--- lib/sidekiq_ext/job_logger.rb
+++ lib/sidekiq_ext/job_logger.rb
@@ -5,16 +5,25 @@ require 'sidekiq/job_logger'
 module SidekiqExt ; end
 
 class SidekiqExt::JobLogger < ::Sidekiq::JobLogger
-  def job_hash_context(job_hash)
-    {
-      bid: job_hash['bid'],
-      jid: job_hash['jid'],
-      tags: job_hash['tags'],
-      queue: job_hash['queue'],
-      # If we're using a wrapper class, like ActiveJob, use the "wrapped"
-      # attribute to expose the underlying thing.
-      class: job_hash['wrapped'] || job_hash['class'],
-      args: job_hash['args'].to_s,
-    }.compact
+  # This is basically copy-pasted from the Sidekiq source code, but we are adding
+  # `:queue` and `:args` to `Sidekiq::Context` so that they'll be logged.
+  def call(item, queue)
+    start = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
+    Sidekiq::Context.add(:queue, queue)
+    Sidekiq::Context.add(:args, JSON(item['args']))
+    @logger.info('start')
+
+    yield
+
+    Sidekiq::Context.add(:elapsed, elapsed(start))
+    @logger.info('done')
+    # rubocop:disable Lint/RescueException
+    # This is what the Sidekiq source code does, so we'll do it here, too.
+  rescue Exception
+    # rubocop:enable Lint/RescueException
+    Sidekiq::Context.add(:elapsed, elapsed(start))
+    @logger.info('fail')
+
+    raise
   end
 end
