d5b70f0ff0d7d41d3d0e62530ac406a37dc8ae28
David Runger <davidjrunger@gmail.com>
2022-10-17 23:54:48 -0500

    Remove InvalidRecordsCheck workers and mailer
    We now have enough records that running these jobs is starting to take a
    somewhat noticeable amount of time and use a noticeable amount of
    memory. This could be further optimized, but the time/effort to do that
    doesn't seem worth it, since this worker pretty much never finds any
    invalid records, anyway.

--- app/mailers/invalid_records_mailer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class InvalidRecordsMailer < ApplicationMailer
-  def invalid_records(klass_name, number_of_invalid_records)
-    @klass_name = klass_name
-    @number_of_invalid_records = number_of_invalid_records
-    mail(subject: "There are #{@number_of_invalid_records} invalid #{@klass_name}s. :(")
-  end
-end
