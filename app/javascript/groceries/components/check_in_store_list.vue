95f534c278e8f2c5fd2b41bb32047d2bacb1adb1
David Runger <davidjrunger@gmail.com>
2022-10-25 13:34:33 -0500

    DRY up store.vue by extracting check_in_store_list.vue component

---
+++ app/javascript/groceries/components/check_in_store_list.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+ul
+  li.block.mb1(
+    v-for='(store, index) in stores'
+    :key='store.id'
+  )
+    input(
+      type='checkbox'
+      v-model='groceriesStore.checkInStores'
+      :value='store'
+      :id='`checkin-stores-${store.id}`'
+    )
+    label.ml1(:for='`checkin-stores-${store.id}`') {{store.name}}
+</template>
+
+<script>
+import { useGroceriesStore } from '@/groceries/store';
+
+export default {
+  data() {
+    return {
+      groceriesStore: useGroceriesStore(),
+    };
+  },
+
+  props: {
+    stores: {
+      required: true,
+      type: Array,
+    },
+  },
+};
+</script>
