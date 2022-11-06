c80ce7838fb55b24b1becc49e3bb3694a9f68477
David Runger <davidjrunger@gmail.com>
2022-11-06 01:16:30 -0600

    Add worker to check for item availability

---
+++ app/workers/check_website.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class CheckWebsite
+  extend Memoist
+  prepend ApplicationWorker
+
+  def perform
+    available_for_cash_carry = nil
+    available_for_click_collect = nil
+    quantity = nil
+
+    availabilities.find do |availability|
+      case availability.deep_symbolize_keys
+      in {
+        availableForCashCarry: available_for_cash_carry,
+        availableForClickCollect: available_for_click_collect,
+        buyingOption: { cashCarry: { availability: { quantity: } } },
+        classUnitKey: { classUnitCode: '410' },
+      } then true
+      else false
+      end
+    end
+
+    Rails.logger.info(<<~LOG.squish)
+      [#{self.class.name}]
+      available_for_cash_carry=#{available_for_cash_carry}
+      available_for_click_collect=#{available_for_click_collect}
+      quantity=#{quantity}
+    LOG
+
+    if available_for_cash_carry || available_for_click_collect || (quantity > 0)
+      CheckWebsiteMailer.item_available(
+        available_for_cash_carry,
+        available_for_click_collect,
+        quantity,
+      ).deliver_later
+    end
+  end
+
+  private
+
+  memoize \
+  def availabilities
+    Faraday. # rubocop:disable Style/SingleArgumentDig
+      json_connection.
+      get(
+        ENV.fetch('CHECK_WEBSITE_URL'),
+        nil,
+        {
+          'Accept' => 'application/json;version=2',
+          'Accept-Language' => 'en-US,en;q=0.5',
+          'X-Client-ID' => ENV.fetch('X_CLIENT_ID'),
+        },
+      ).
+      body.
+      dig('availabilities')
+  end
+end
