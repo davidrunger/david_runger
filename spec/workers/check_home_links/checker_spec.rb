acde63ca569b3702eec3fba2c260e63568108be2
David Runger <davidjrunger@gmail.com>
2022-11-28 13:48:01 -0600

    Only send email about bad home link if it fails twice in 48 hours

--- spec/workers/check_home_links/checker_spec.rb
+++ spec/workers/check_home_links/checker_spec.rb
@@ -9,41 +9,64 @@ RSpec.describe CheckHomeLinks::Checker do
     subject(:perform) { worker.perform(url) }
 
     let(:status) { 200 }
+    let(:redis_failure_key) { worker.send(:redis_failure_key, url) }
 
     before do
       stub_request(:get, 'https://github.com/davidrunger').
         to_return(status:, body: '', headers: {})
     end
 
-    context 'when the link returns the expected status' do
-      it 'does not send an email', queue_adapter: :test do
-        expect { perform }.not_to enqueue_mail
+    context 'when a previous failure is marked in Redis' do
+      before { $redis_pool.with { _1.call('set', redis_failure_key, '1') } }
+
+      context 'when the link returns the expected status' do
+        it 'does not send an email', queue_adapter: :test do
+          expect { perform }.not_to enqueue_mail
+        end
+      end
+
+      context 'when the link returns an unexpected status' do
+        let(:status) { 404 }
+
+        it 'sends an AdminMailer#bad_home_link email', queue_adapter: :test do
+          expect { perform }.
+            to enqueue_mail(AdminMailer, :bad_home_link).
+            with(url, status, [200])
+        end
+      end
+
+      context 'when fetching the URL raises an error' do
+        before do
+          # rubocop:disable RSpec/AnyInstance
+          allow_any_instance_of(Faraday::Connection).
+            to receive(:get).
+            and_raise(Faraday::ConnectionFailed, 'Operation timed out - user specified timeout')
+          # rubocop:enable RSpec/AnyInstance
+        end
+
+        it 'sends an AdminMailer#bad_home_link email', queue_adapter: :test do
+          expect { perform }.
+            to enqueue_mail(AdminMailer, :bad_home_link).
+            with(url, nil, [200])
+        end
       end
     end
 
-    context 'when the link returns an unexpected status' do
-      let(:status) { 404 }
+    context 'when a previous failure did not occur within the past 48 hours' do
+      before { expect($redis_pool.with { _1.call('get', redis_failure_key) }).to eq(nil) }
 
-      it 'sends an AdminMailer#bad_home_link email', queue_adapter: :test do
-        expect { perform }.
-          to enqueue_mail(AdminMailer, :bad_home_link).
-          with(url, status, [200])
-      end
-    end
+      context 'when the link returns an unexpected status' do
+        let(:status) { 404 }
 
-    context 'when fetching the URL raises an error' do
-      before do
-        # rubocop:disable RSpec/AnyInstance
-        allow_any_instance_of(Faraday::Connection).
-          to receive(:get).
-          and_raise(Faraday::ConnectionFailed, 'Operation timed out - user specified timeout')
-        # rubocop:enable RSpec/AnyInstance
-      end
+        it 'does not send an AdminMailer#bad_home_link email', queue_adapter: :test do
+          expect { perform }.not_to enqueue_mail(AdminMailer, :bad_home_link)
+        end
 
-      it 'sends an AdminMailer#bad_home_link email', queue_adapter: :test do
-        expect { perform }.
-          to enqueue_mail(AdminMailer, :bad_home_link).
-          with(url, nil, [200])
+        it 'marks the failure in Redis' do
+          expect { perform }.to change {
+            $redis_pool.with { _1.call('get', redis_failure_key) }
+          }.from(nil).to('1')
+        end
       end
     end
   end
