b825237f714dcac914bcd557b7302d0fdb962ee8
David Runger <davidjrunger@gmail.com>
2023-01-17 10:55:42 -0600

    Move mailer views to a separate directory

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  # https://boringrails.com/articles/writing-better-action-mailers/
+  prepend_view_path 'app/views/mailers'
+
+  layout 'mailer'
+
   default(
     to: '"David Runger" <davidjrunger@gmail.com>',
     from: '"DavidRunger.com" <reply@davidrunger.com>',
     reply_to: '"DavidRunger.com" <reply@mg.davidrunger.com>',
   )
-  layout 'mailer'
 end
