d87fd230137178b5abaa1e0e2d7ff50a4a20b7bf
David Runger <davidjrunger@gmail.com>
2023-01-19 13:28:08 -0600

    [specs] Add a unit test for `Items::BulkUpdate::Create`
    I wanted this test to test the `isolator` gem that I'm going to add.

---
+++ spec/actions/items/bulk_update/create_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe(Items::BulkUpdate::Create) do
+  subject(:action) { Items::BulkUpdate::Create.new!(items:, attributes_change:) }
+
+  let(:items) { [item] }
+  let(:item) { Item.first!.tap { _1.update!(needed: old_needed) } }
+  let(:attributes_change) { { 'needed' => new_needed } }
+  let(:old_needed) { 55 }
+  let(:new_needed) { 0 }
+
+  describe '#run!' do
+    subject(:run!) { action.run! }
+
+    it 'changes all of the items as specified by attributes_change' do
+      expect { run! }.to change { item.reload.needed }.from(old_needed).to(new_needed)
+    end
+  end
+end
