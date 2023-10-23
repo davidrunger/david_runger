9e63a875141c353e0dddef50ca5b0f0974b8d218
David Runger <davidjrunger@gmail.com>
2023-10-23 18:49:26 -0500

    Fix blog specs
    These were somehow broken (albeit with that breakage forcing an
    improvement) by the bump from Rails 7.0.8 to 7.1.1.

--- spec/controllers/blog_controller_spec.rb
+++ spec/controllers/blog_controller_spec.rb
@@ -4,46 +4,82 @@ RSpec.describe(BlogController) do
   context 'when a BLOG_ROOT_URL env var is set' do
     let(:blog_host_url) { 'https://my-blog.static-host.com' }
 
+    let(:blog_path) { 'blog' }
+
     around do |spec|
       ClimateControl.modify(BLOG_ROOT_URL: blog_host_url) do
         spec.run
       end
     end
 
-    context 'when the blog server responds with content' do
-      before do
-        stub_request(:get, blog_host_url).
-          with(
-            headers: {
-              'Accept' => '*/*',
-              'Accept-Encoding' => 'gzip',
-              'Connection' => 'keep-alive',
-              'User-Agent' => 'Ruby',
-            },
-          ).
-          to_return(status: 200, body: 'great blog content', headers: {})
-      end
+    describe '#index' do
+      subject(:get_index) { get(:index) }
 
-      describe '#index' do
-        subject(:get_index) { get(:index) }
+      context 'when the blog server responds with blog index content' do
+        before do
+          stub_request(:get, "#{blog_host_url}/#{blog_path}").
+            with(
+              headers: {
+                'Accept' => '*/*',
+                'Accept-Encoding' => 'gzip',
+                'Connection' => 'keep-alive',
+                'User-Agent' => 'Ruby',
+              },
+            ).
+            to_return(status: 200, body: 'great blog content', headers: {})
+        end
 
         it 'responds with 200' do
           get_index
           expect(response).to have_http_status(200)
         end
       end
+    end
 
-      describe '#show' do
-        subject(:get_show) { get(:show, params: { slug: 'why-i-love-ruby' }) }
+    describe '#show' do
+      subject(:get_show) { get(:show, params: { slug: }) }
+
+      let(:slug) { 'why-i-love-ruby' }
+
+      context 'when the blog server responds with blog show content' do
+        before do
+          stub_request(:get, "#{blog_host_url}/#{blog_path}/#{slug}").
+            with(
+              headers: {
+                'Accept' => '*/*',
+                'Accept-Encoding' => 'gzip',
+                'Connection' => 'keep-alive',
+                'User-Agent' => 'Ruby',
+              },
+            ).
+            to_return(status: 200, body: 'great blog content', headers: {})
+        end
 
         it 'responds with 200' do
           get_show
           expect(response).to have_http_status(200)
         end
       end
+    end
 
-      describe '#assets' do
-        subject(:get_assets) { get(:assets, params: { path: '_bridgetown/static/index.QL2A.css' }) }
+    describe '#assets' do
+      subject(:get_assets) { get(:assets, params: { path: asset_path }) }
+
+      let(:asset_path) { '_bridgetown/static/index.QL2A.css' }
+
+      context 'when the blog server responds with blog show content' do
+        before do
+          stub_request(:get, "#{blog_host_url}/#{blog_path}/#{asset_path}").
+            with(
+              headers: {
+                'Accept' => '*/*',
+                'Accept-Encoding' => 'gzip',
+                'Connection' => 'keep-alive',
+                'User-Agent' => 'Ruby',
+              },
+            ).
+            to_return(status: 200, body: 'great blog content', headers: {})
+        end
 
         it 'responds with 200' do
           get_assets
