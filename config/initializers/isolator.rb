e114f75fa1a850e87d1e0b1ab0f9f6503c358668
David Runger <davidjrunger@gmail.com>
2023-12-30 10:02:55 -0600

    Bump Ruby from 3.2.2 to 3.3.0 (#3702)
    * Bump Ruby from 3.2.2 to 3.3.0
    
    * Remove isolator
    
    It's not compatible with Ruby 3.3.0 because its dependency anyway_config
    is not. https://github.com/palkan/anyway_config/issues/ 144
    
    * Bump simple_cov-formatter-terminal from 0.3.1 to 1.0.0
    
    We need the runger_config dependency bump from 4.0.0 to 5.0.0 for Ruby
    3.3.0 compatibility.

--- config/initializers/isolator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# `Isolator` is not available in production (per the Gemfile)
-if Rails.env.in?(%w[development test])
-  Isolator.configure do |config|
-    config.raise_exceptions = true
-  end
-end
