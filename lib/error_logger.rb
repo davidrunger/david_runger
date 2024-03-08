60ba72bcc5b30b03c68d359ded9cc6b81a7ceb47
David Runger <davidjrunger@gmail.com>
2024-03-08 01:33:55 -0600

    Add missing space in ErrorLogger
    Specs FTW.

--- lib/error_logger.rb
+++ lib/error_logger.rb
@@ -5,7 +5,7 @@ module ErrorLogger
     data_log_line = data.map { |key, value| "#{key}=#{value.inspect}" }.join(' ')
     Rails.logger.warn(
       "#{error_klass.name}: #{message}" \
-      "#{"; #{data_log_line}" if data_log_line.present?}",
+      "#{" ; #{data_log_line}" if data_log_line.present?}",
     )
 
     Rollbar.warn(*[Error.new(error_klass, message), data].compact_blank)
