c0037b8f9061b99f0ebd33496e487540f7aecdcc
David Runger <davidjrunger@gmail.com>
2024-03-08 00:54:03 -0600

    Work around prism bug for heredocs that start with interpolation
    RuboCop complains that the indentation is incorrect.

--- lib/error_logger.rb
+++ lib/error_logger.rb
@@ -3,10 +3,10 @@
 module ErrorLogger
   def self.warn(message:, data: {}, error_klass: StandardError)
     data_log_line = data.map { |key, value| "#{key}=#{value.inspect}" }.join(' ')
-    Rails.logger.warn(<<~LOG.squish)
-      #{error_klass.name}: #{message}
-      #{"; #{data_log_line}" if data_log_line.present?}
-    LOG
+    Rails.logger.warn(
+      "#{error_klass.name}: #{message}" \
+      "#{"; #{data_log_line}" if data_log_line.present?}",
+    )
 
     Rollbar.warn(*[Error.new(error_klass, message), data].compact_blank)
   end
