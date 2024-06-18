b59aa659bb403cf7f5bf41bf4e287b82bab5b0f6
David Runger <davidjrunger@gmail.com>
2024-06-18 10:09:03 -0500

    [groceries] Extract CheckInItemsList component (#4409)
    This will be useful because I am going to reuse this component as we
    build out additional sections in the check-in modal for "In cart" and
    "Skipped".

---
+++ app/javascript/groceries/components/check_in_items_list.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+ul.check-in-items-list.mb-2
+  li.flex.items-center.mb-2(
+    v-for='(item, index) in items'
+    :key='item.id'
+  )
+    input(
+      type='checkbox'
+      v-model='itemsToZero'
+      :value='item'
+      :id='`trip-checkin-item-${item.id}`'
+    )
+    label.ml-2(:for='`trip-checkin-item-${item.id}`')
+      span {{item.name}}
+      span(v-if='item.needed > 1') {{' '}} ({{item.needed}})
+      span.pr-1
+      el-button(
+        link
+        type='primary'
+        @click='skip(item)'
+      ) Skip
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Item } from '@/groceries/types';
+
+export default defineComponent({
+  props: {
+    items: {
+      type: Array as PropType<Array<Item>>,
+      required: true,
+    },
+  },
+});
+</script>
