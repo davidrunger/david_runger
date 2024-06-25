061c51025025e5ce2faa111c32ef02049587d0d9
David Runger <davidjrunger@gmail.com>
2024-06-25 14:17:47 -0500

    Add an initializer that verifies the githooks have been set up

---
+++ config/initializers/0g_githooks_check.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# The name of this file uses lexicographical ordering. See:
+# https://stackoverflow.com/a/38927158/4009384 . To determine a new filename for
+# insertion, see: https://runkit.com/davidrunger/667b2a5707d6c60008d1fe66 .
+
+if (
+  ENV['SKIP_GITHOOKS_CHECK'].blank? &&
+    `git config core.hooksPath`.strip != 'tools/githooks'
+)
+  $stderr.puts(AmazingPrint::Colors.red(<<~ERROR))
+    You have not configured the git hooks for this repo! To do so, run:
+
+      #{AmazingPrint::Colors.blue('git config core.hooksPath tools/githooks')}
+  ERROR
+
+  $stderr.puts(AmazingPrint::Colors.red(<<~ERROR))
+    Or, if you must, you can set `SKIP_GITHOOKS_CHECK=1` in `.env.development.local`.
+  ERROR
+
+  exit(1) # rubocop:disable Rails/Exit
+end
---
+++ config/initializers/0s_requires.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# The name of this file uses lexicographical ordering. See:
+# https://stackoverflow.com/a/38927158/4009384 . To determine a new filename for
+# insertion, see: https://runkit.com/davidrunger/667b2a5707d6c60008d1fe66 .
+
+# This avoids "DEPRECATION WARNING: Initialization autoloaded the constant RedisOptions."
+require Rails.root.join('app/poros/redis_options.rb')
