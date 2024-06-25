28ac92f6783a7cce12f53c2db5a803600ad57eb0
David Runger <davidjrunger@gmail.com>
2024-06-25 13:41:38 -0500

    Remove lefthook
    We added lefthook in #1693 / 8004fc89 .
    
    Instead, I am going to move instead to an approach of manually managing
    git hooks as executable scripts that I define (probably in my dotfiles
    repo, so that I can also add the setup to that repo's install script).

--- app/javascript/home/components/projects.vue
+++ app/javascript/home/components/projects.vue
@@ -158,7 +158,6 @@ HomeSection(section='projects' title='Projects')
             #[a(href='https://github.com/paper-trail-gem/paper_trail') PaperTrail] model change tracking,
             #[a(href='https://github.com/rack/rack-attack') Rack::Attack] IP blocking and request throttling,
             #[a(href='https://github.com/flyerhzm/bullet') Bullet] eager-loading enforcement,
-            #[a(href='https://github.com/evilmartians/lefthook') Lefthook] git hook management,
             #[a(href='https://github.com/gitleaks/gitleaks') Gitleaks] secret protection,
             #[a(href='https://aws.amazon.com/s3/') AWS S3] blob storage,
             #[a(href='https://rollbar.com/') Rollbar] error tracking,
---
+++ docs/setup.md
@@ -0,0 +1,9 @@
+# Setup
+
+## Git hooks
+
+Opt in to our git hooks by executing in this repository:
+
+```sh
+git config core.hooksPath tools/githooks
+```
