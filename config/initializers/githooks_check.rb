178bc0842acc29bc7ed9c00d96ee6e8753e9b2d1
David Runger <davidjrunger@gmail.com>
2024-07-02 10:40:58 -0500

    [githooks_check] Remove outdated comment about file name ordering

--- config/initializers/githooks_check.rb
+++ config/initializers/githooks_check.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# The name of this file uses lexicographical ordering. See:
-# https://stackoverflow.com/a/38927158/4009384 . To determine a new filename for
-# insertion, see: https://runkit.com/davidrunger/667b2a5707d6c60008d1fe66 .
-
 # :nocov:
 if (
   Rails.env.local? &&
