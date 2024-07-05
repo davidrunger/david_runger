e6a43aad483a838bfafe41fa870d6f14b01d8d48
David Runger <davidjrunger@gmail.com>
2024-07-05 09:06:28 -0500

    Fix pnpm-lock.yaml (#4557)
    When running `pnpm install --frozen-lockfile`, I was getting this error:
    
    ```
    ERR_PNPM_BROKEN_LOCKFILE The lockfile at
    "/home/david/code/david_runger/pnpm-lock.yaml"
    is broken: duplicated mapping key (909:3)
    
    906 |   '@volar/source-map@2.4.0-alpha.15':
    907 |     resolution: {integrity: sha51 ...
    908 |
    909 |   '@volar/source-map@2.4.0-alpha.15':
    ---------^
    910 |     resolution: {integrity: sha51 ...
    911 |
    ```
    
    This seems to have been caused by a bad git resolution of PR #4555 and
    PR #4556 .
    
    Fix:
    
    ```
    rm pnpm-lock.yaml
    pnpm install
    ```
    
    Also, remove `--silent` flag from pnpm commands. Motivation: it wasn't
    clear what the problem was from looking at the build logs ( example:
    https://github.com/davidrunger/david_runger/actions/runs/9809328382/job/27087113972
    ), because the error output was being silenced.

--- lib/test/tasks/pnpm_install.rb
+++ lib/test/tasks/pnpm_install.rb
@@ -4,6 +4,6 @@ class Test::Tasks::PnpmInstall < Pallets::Task
   include Test::TaskHelpers
 
   def run
-    execute_system_command('pnpm install --frozen-lockfile --silent')
+    execute_system_command('pnpm install --frozen-lockfile')
   end
 end
