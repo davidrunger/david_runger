a630436aa13c4da2e492ce296fd955dbb0e0f2de
David Runger <davidjrunger@gmail.com>
2024-07-21 17:18:29 -0500

    Provide a copy-to-clipboard button for the quiz link [QUIZ-1]

--- app/javascript/lib/toast.ts
+++ app/javascript/lib/toast.ts
@@ -1,9 +1,16 @@
 import Toastify from 'toastify-js';
 
-export function toast(message: string) {
+type ToastType = 'error';
+
+const TYPE_TO_CLASS_MAP = {
+  error: 'error',
+};
+
+export function toast(message: string, options?: { type: ToastType }) {
   Toastify({
     text: message,
     position: 'center',
     duration: 3000,
+    className: options?.type && TYPE_TO_CLASS_MAP[options?.type],
   }).showToast();
 }
