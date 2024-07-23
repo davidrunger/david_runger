cb346eb5fece91eedaef091dd03c02b98dca816e
David Runger <davidjrunger@gmail.com>
2024-07-23 13:31:41 -0500

    Add a dedicated proposal page
    GROC~3 calls for us to link to a place where users can invite their
    partner to the app as their partner. The only place that we currently
    have to do that has a URL of `/check_ins`. We want to add a dedicated
    place with a more appropriate URL as a generic place to make a proposal.

--- app/controllers/marriages_controller.rb
+++ app/controllers/marriages_controller.rb
@@ -1,8 +1,24 @@
 class MarriagesController < ApplicationController
   self.container_classes = CheckInsController.container_classes
 
+  before_action :ensure_no_spouse, only: %i[new]
+
   def show
     @marriage = current_user.marriage.decorate
     authorize(@marriage, :show?)
+  end
+
+  def new
+    authorize(Marriage)
+    render :new
+  end
+
+  private
+
+  def ensure_no_spouse
+    if current_user.spouse
+      flash[:alert] = "You are already married to #{current_user.spouse.email}."
+      redirect_to(check_ins_path)
+    end
   end
 end
