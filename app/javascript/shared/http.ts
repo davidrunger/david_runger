b0cd895152357908e49551382526ed15cf787ed8
David Runger <davidjrunger@gmail.com>
2024-07-24 05:55:38 -0500

    Remove unused import

--- app/javascript/shared/http.ts
+++ app/javascript/shared/http.ts
@@ -1,7 +1,6 @@
 import xior from 'xior';
 
 import { toast } from '@/lib/toast';
-import { assert } from '@/shared/helpers';
 
 const csrfMetaTag = document.querySelector('meta[name="csrf-token"]');
 const csrfToken = csrfMetaTag?.getAttribute('content');
