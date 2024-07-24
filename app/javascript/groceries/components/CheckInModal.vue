a4eb2a2340bbdd1d55ce4916b50edacf4b22ffb2
David Runger <davidjrunger@gmail.com>
2024-07-24 02:09:26 -0500

    Extract CheckInModal component from Store [GROC-15] (#4699)

---
+++ app/javascript/groceries/components/CheckInModal.vue
@@ -0,0 +1,75 @@
+<template lang="pug">
+Modal(name='check-in-shopping-trip' width='85%' maxWidth='400px')
+  slot
+    .flex.flex-col.max-h-full
+      .shrink-0.flex.items-center.mb-3
+        span Stores: {{checkInStoreNames}}
+        el-button.choose-stores.ml-2(
+          link
+          type='primary'
+          @click='manageCheckInStores'
+        ) Choose stores
+
+      .flex-1.overflow-y-auto
+        CheckInItemsList(
+          title="Needed"
+          :items="neededUnskippedCheckInItemsNotInCart"
+        )
+
+        CheckInItemsList(
+          title="In Cart"
+          :items="neededUnskippedCheckInItemsInCart"
+        )
+
+        CheckInItemsList(
+          title="Skipped"
+          :items="neededSkippedCheckInItems"
+        )
+
+      .shrink-0.flex.justify-around.mt-4
+        el-button(
+          @click="modalStore.hideModal({ modalName: 'check-in-shopping-trip' })"
+          type='primary'
+          link
+        ) Cancel
+        el-button(
+          @click='handleTripCheckinModalSubmit'
+          type='primary'
+          plain
+        ) Check in items in cart
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+
+import { useGroceriesStore } from '@/groceries/store';
+import type { Store } from '@/groceries/types';
+import { useModalStore } from '@/shared/modal/store';
+
+import CheckInItemsList from './CheckInItemsList.vue';
+
+const groceriesStore = useGroceriesStore();
+const modalStore = useModalStore();
+
+const {
+  neededSkippedCheckInItems,
+  neededUnskippedCheckInItemsInCart,
+  neededUnskippedCheckInItemsNotInCart,
+} = storeToRefs(groceriesStore);
+
+const checkInStoreNames = computed((): string => {
+  return groceriesStore.checkInStores
+    .map((store: Store) => store.name)
+    .join(', ');
+});
+
+function handleTripCheckinModalSubmit() {
+  groceriesStore.zeroItemsInCart();
+  modalStore.hideModal({ modalName: 'check-in-shopping-trip' });
+}
+
+function manageCheckInStores() {
+  modalStore.showModal({ modalName: 'manage-check-in-stores' });
+}
+</script>
