a6df3a93a245e89838f932fcaad3537e9f731841
David Runger <davidjrunger@gmail.com>
2024-07-24 02:10:35 -0500

    Extract ManageCheckInStoresModal component from Store [GROC-16]

---
+++ app/javascript/groceries/components/ManageCheckInStoresModal.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+Modal(name='manage-check-in-stores' width='80%' maxWidth='370px')
+  slot
+    h4.font-bold.mt-2.mb-4.
+      Which stores would you like to check in?
+    CheckInStoreList(
+      :stores='groceriesStore.sortedStores'
+    )
+    h4.font-bold.mb-4.
+      Spouse's stores
+    CheckInStoreList(
+      :stores='groceriesStore.sortedSpouseStores'
+    )
+    div.flex.justify-around.mt-4
+      el-button(
+        @click="modalStore.hideModal({ modalName: 'manage-check-in-stores' })"
+        type='primary'
+      ) Done
+</template>
+
+<script setup lang="ts">
+import { useModalStore } from '@/shared/modal/store';
+import { useGroceriesStore } from '../store';
+import CheckInStoreList from './CheckInStoreList.vue';
+
+const groceriesStore = useGroceriesStore();
+const modalStore = useModalStore();
+</script>
+
+<style scoped lang="scss">
+</style>
--- app/javascript/groceries/components/Store.vue
+++ app/javascript/groceries/components/Store.vue
@@ -65,23 +65,7 @@
 
   CheckInModal
 
-  Modal(name='manage-check-in-stores' width='80%' maxWidth='370px')
-    slot
-      h4.font-bold.mt-2.mb-4.
-        Which stores would you like to check in?
-      CheckInStoreList(
-        :stores='groceriesStore.sortedStores'
-      )
-      h4.font-bold.mb-4.
-        Spouse's stores
-      CheckInStoreList(
-        :stores='groceriesStore.sortedSpouseStores'
-      )
-      div.flex.justify-around.mt-4
-        el-button(
-          @click="modalStore.hideModal({ modalName: 'manage-check-in-stores' })"
-          type='primary'
-        ) Done
+  ManageCheckInStoresModal
 </template>
 
 <script setup lang="ts">
@@ -97,8 +81,8 @@ import type { Item as ItemType, Store } from '@/groceries/types';
 import { useModalStore } from '@/shared/modal/store';
 
 import CheckInModal from './CheckInModal.vue';
-import CheckInStoreList from './CheckInStoreList.vue';
 import Item from './Item.vue';
+import ManageCheckInStoresModal from './ManageCheckInStoresModal.vue';
 
 const props = defineProps({
   store: {
