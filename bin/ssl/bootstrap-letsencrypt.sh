129c460ba7bf3edc58aed1036bd39d2c1b4b8973
David Runger <davidjrunger@gmail.com>
2024-07-31 18:06:35 -0500

    [bootstrap-letsencrypt] Update comment to reflect staging=1 default (#4764)

--- bin/ssl/bootstrap-letsencrypt.sh
+++ bin/ssl/bootstrap-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(davidrunger.com www.davidrunger.com)
 rsa_key_size=4096
 data_path="./ssl-data/certbot"
 email="davidjrunger@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Default to staging to avoid rate limits. Set to 0 (false) once ready to go for real.
 
 if [ -d "$data_path" ]; then
   read -r -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
