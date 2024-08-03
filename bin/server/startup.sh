8893afec5ce4c6259ff8bce11dddb21e9de83787
David Runger <davidjrunger@gmail.com>
2024-08-03 11:07:59 -0500

    [bin/server] Add install script and start services on boot [DEV-74]

---
+++ bin/server/startup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+# This script (startup.sh in the david_runger repo) is not intended to be
+# executed directly, but rather to be symlinked to `/etc/rc.local`, so that it's
+# contents will be executed at system startup.
+
+docker_startup_log=/tmp/docker-compose.log
+
+# Log the script execution start time
+echo "Startup script executed at: $(date -u +"%Y-%m-%dT%H:%M:%SZ")" >> "$docker_startup_log"
+
+# Log current directory and user
+echo "Current directory: $(pwd)" >> "$docker_startup_log"
+echo "Current user: $(whoami)" >> "$docker_startup_log"
+
+# Wait for Docker to be active
+while ! systemctl is-active --quiet docker; do
+  echo "Waiting for Docker to start at: $(date -u +"%Y-%m-%dT%H:%M:%SZ")" >> "$docker_startup_log"
+  sleep 1
+done
+
+# Change to the directory containing the docker-compose.yml file
+cd "/$(whoami)/david_runger" || {
+  echo "Failed to change directory to /$(whoami)/david_runger" >> "$docker_startup_log"
+  exit 1
+}
+
+# Start the Docker services
+bin/server/boot-services.sh >> "$docker_startup_log" 2>&1 &
+
+exit 0
