282ba1f78368d1897c4f52ff2cc70fb8ec4eae47
David Runger <davidjrunger@gmail.com>
2024-08-03 15:01:25 -0500

    Schedule nightly database backups and add script to download [DEV-68]

---
+++ bin/server/back-up-db-to-s3.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail # exit on any error, don't allow undefined variables, pipes don't swallow errors
+
+# Clean up from previous runs
+rm -rf tmp/backup
+
+# Create backup
+mkdir -p ./tmp/backup
+docker compose run \
+  --rm --volume "$(realpath tmp/backup):/tmp/backup" \
+  postgres \
+  sh -c "
+    mkdir -p /tmp/backup && \
+    pg_dump -U david_runger -h postgres -F t -w david_runger_production \
+    > '/tmp/backup/export'
+  "
+
+# Send backup to S3
+docker compose run \
+  --rm --volume "$(realpath tmp/backup):/backup" \
+  s3_db_backup
+
+# Clean up
+rm -rf tmp/backup
