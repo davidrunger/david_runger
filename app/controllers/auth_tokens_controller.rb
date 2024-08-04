afa08a0d7adbfa3042514e5e510969919c148255
David Runger <davidjrunger@gmail.com>
2024-08-04 02:10:10 -0500

    Move UsersController to MyAccountController and stop taking id param (#4811)
    Motivations:
    1. Make the controller more secure by eliminating the possibility of
    IDOR by now simply referencing `current_user` rather than a User by id.
    2. Allow providing a universal path (`/my_account`) for account
    management, rather than each user having a different path for account
    management. This is useful, for example, to put in a privacy policy.
    
    Also, add a MyAccountController#show action (which takes some of the
    view code that was previously in #edit). Motivation: this makes the URL
    cleaner (just `/my_account`, rather than `/my_account/edit`), and also I
    think it just makes sense to have a #show page where the user can see
    their own account info and on which we can place the button to destroy
    the user's account.

--- app/controllers/auth_tokens_controller.rb
+++ app/controllers/auth_tokens_controller.rb
@@ -4,20 +4,20 @@ class AuthTokensController < ApplicationController
   def create
     authorize(AuthToken)
     AuthTokens::Create.run!(user: current_user)
-    redirect_to(edit_user_path(current_user))
+    redirect_to(edit_my_account_path)
   end
 
   def destroy
     authorize(@auth_token)
     @auth_token.destroy!
-    redirect_to(edit_user_path(current_user))
+    redirect_to(edit_my_account_path)
   end
 
   def update
     authorize(@auth_token)
     @auth_token.update!(auth_token_params)
     flash[:notice] = 'Updated auth token successfully!'
-    redirect_to(edit_user_path(current_user))
+    redirect_to(edit_my_account_path)
   end
 
   private
