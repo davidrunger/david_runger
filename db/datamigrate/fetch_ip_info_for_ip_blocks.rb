ad310ebf48387f5f840568d128fa9357e6c48420
David Runger <davidjrunger@gmail.com>
2024-08-05 21:37:06 -0500

    Print relative (rather than absolute) datamigration file name in logs

--- db/datamigrate/fetch_ip_info_for_ip_blocks.rb
+++ db/datamigrate/fetch_ip_info_for_ip_blocks.rb
@@ -1,4 +1,4 @@
-Rails.logger.info("Starting datamigration #{__FILE__}.")
+Rails.logger.info("Starting datamigration #{__FILE__.delete_prefix("#{Rails.root}/")} .")
 
 ip_blocks_with_missing_info =
   IpBlock.where(isp: nil).or(
@@ -11,4 +11,4 @@ ip_blocks_with_missing_info.find_each.with_index do |ip_block, index|
   FetchIpInfoForRecord.perform_in(index * 5, ip_block.class.name, ip_block.id)
 end
 
-Rails.logger.info("Datamigration #{__FILE__} complete.")
+Rails.logger.info("Datamigration #{__FILE__.delete_prefix("#{Rails.root}/")} complete.")
