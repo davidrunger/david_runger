edecfce34c740d422add7fe77d13840dab275a7c
David Runger <davidjrunger@gmail.com>
2024-08-05 21:42:10 -0500

    Revert bad RuboCop autocorrection and disable the cop

--- db/datamigrate/fetch_ip_info_for_ip_blocks.rb
+++ db/datamigrate/fetch_ip_info_for_ip_blocks.rb
@@ -1,4 +1,6 @@
-Rails.logger.info("Starting datamigration #{__FILE__.delete_prefix(Rails.root.join('').to_s)} .")
+# rubocop:disable Rails/FilePath
+Rails.logger.info("Starting datamigration #{__FILE__.delete_prefix("#{Rails.root}/")} .")
+# rubocop:enable Rails/FilePath
 
 ip_blocks_with_missing_info =
   IpBlock.where(isp: nil).or(
@@ -11,4 +13,6 @@ ip_blocks_with_missing_info.find_each.with_index do |ip_block, index|
   FetchIpInfoForRecord.perform_in(index * 5, ip_block.class.name, ip_block.id)
 end
 
-Rails.logger.info("Datamigration #{__FILE__.delete_prefix(Rails.root.join('').to_s)} complete.")
+# rubocop:disable Rails/FilePath
+Rails.logger.info("Datamigration #{__FILE__.delete_prefix("#{Rails.root}/")} complete.")
+# rubocop:enable Rails/FilePath
