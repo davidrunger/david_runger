c700ab7ec343f85daf7721488b78446d6673f947
David Runger <davidjrunger@gmail.com>
2024-08-05 21:30:13 -0500

    Add log statements to datamigration

--- db/datamigrate/fetch_ip_info_for_ip_blocks.rb
+++ db/datamigrate/fetch_ip_info_for_ip_blocks.rb
@@ -1,8 +1,14 @@
+Rails.logger.info("Starting datamigration #{__FILE__}.")
+
 ip_blocks_with_missing_info =
   IpBlock.where(isp: nil).or(
     IpBlock.where(location: nil),
   )
 
+Rails.logger.info("Enqueueing IP info fetches for #{ip_blocks_with_missing_info.size} IpBlocks.")
+
 ip_blocks_with_missing_info.find_each.with_index do |ip_block, index|
   FetchIpInfoForRecord.perform_in(index * 5, ip_block.class.name, ip_block.id)
 end
+
+Rails.logger.info("Datamigration #{__FILE__} complete.")
