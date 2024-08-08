0e74dfbe33be18e3cf8381f8a2e74da672c8d2d1
David Runger <davidjrunger@gmail.com>
2024-08-08 12:36:27 -0500

    Add connectedToLogEntriesChannel to davidrunger in Window type

--- app/javascript/shared/index.d.ts
+++ app/javascript/shared/index.d.ts
@@ -4,6 +4,7 @@ declare global {
   interface Window {
     davidrunger: {
       bootstrap: object;
+      connectedToLogEntriesChannel?: boolean;
       env: 'development' | 'test' | 'production';
       modalKeydownListenerRegistered: boolean;
     };
