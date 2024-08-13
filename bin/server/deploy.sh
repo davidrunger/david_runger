081c2d24eb328095952df9524367b8178a116098
David Runger <davidjrunger@gmail.com>
2024-08-13 13:52:38 -0500

    Build Docker image on GitHub (not on server) [DEV-71] (#4903)

--- bin/server/deploy.sh
+++ bin/server/deploy.sh
@@ -21,8 +21,10 @@ fi
 # Run the install script.
 bin/server/install.sh
 
-# Rebuild the app.
-bin/build-docker production
+# Pull the Docker image for the currently checked out Git SHA, and tag it as 'latest'.
+image_tag_for_sha="davidrunger/david_runger:sha-$(git rev-parse HEAD)"
+docker pull "$image_tag_for_sha"
+docker tag "$image_tag_for_sha" davidrunger/david_runger:latest
 
 # Run release tasks.
 docker compose run --rm web bin/server/release-tasks
