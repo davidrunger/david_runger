a14d6f1abdc2793cd7f81be463201d54294c72e2
David Runger <davidjrunger@gmail.com>
2024-08-13 17:21:37 -0500

    Enable YJIT only for web (not for worker) [DEV-91]
    This is intended to save memory on the server (for the Sidekiq processes
    not using YJIT), at the cost of processing speed.

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,9 @@ class DavidRunger::Application < Rails::Application
   # Initialize configuration defaults for originally generated Rails version.
   config.load_defaults(7.2)
 
+  # We enable YJIT for the web server only in config/initializers/enable_yjit.rb.
+  config.yjit = false
+
   # Please, add to the `ignore` list any other `lib` subdirectories that do
   # not contain `.rb` files, or that should not be reloaded or eager loaded.
   # Common ones are `templates`, `generators`, or `middleware`, for example.
