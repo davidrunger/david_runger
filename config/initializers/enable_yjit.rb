713e2726222b8c1d67663bb14dba570de9f52ba1
David Runger <davidjrunger@gmail.com>
2024-08-13 21:01:03 -0500

    [enable_yjit] Also initialize YJIT in CI (#4907)
    This way, we have the YJIT lines under test coverage. Also, maybe this
    will make CI run a little faster than it will with YJIT enabled? I like
    not enabling YJIT for my tests locally, because I am pretty memory
    constrained on my current machine.

--- config/initializers/enable_yjit.rb
+++ config/initializers/enable_yjit.rb
@@ -1,5 +1,5 @@
 # Enable YJIT on the server (for performance gains), but not on the worker (for memory savings).
-if defined?(RubyVM::YJIT.enable) && defined?(Rails::Server)
+if defined?(RubyVM::YJIT.enable) && (defined?(Rails::Server) || ENV.key?('CI'))
   Rails.application.config.after_initialize do
     RubyVM::YJIT.enable
   end
