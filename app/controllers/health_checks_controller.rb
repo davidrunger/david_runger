ca594fea6d7bb0d2a0afa64bd14acdea45c17620
David Runger <davidjrunger@gmail.com>
2024-08-17 16:45:09 -0500

    [docker-compose] Add a health check for the web service [DEV-93]

---
+++ app/controllers/health_checks_controller.rb
@@ -0,0 +1,36 @@
+class HealthChecksController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  before_action :skip_authorization, only: :index
+
+  def index
+    [
+      Thread.new { check_postgres },
+      Thread.new { check_redis },
+    ].each(&:join)
+
+    head :ok
+  end
+
+  private
+
+  def check_postgres
+    reraising_in_main_thread do
+      ActiveRecord::Base.logger.silence do
+        User.select(:id).first.id
+      end
+    end
+  end
+
+  def check_redis
+    reraising_in_main_thread do
+      $redis_pool.with { _1.call('ping') }
+    end
+  end
+
+  # NOTE: This method avoids a warning about a thread being terminated with an exception.
+  def reraising_in_main_thread
+    yield
+  rescue => error
+    Thread.main.raise(error)
+  end
+end
