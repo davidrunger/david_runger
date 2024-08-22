07ba0fb8eda2fc7c1383be35d5dcc73348073264
David Runger <davidjrunger@gmail.com>
2024-08-22 12:55:33 -0500

    Make RunTypelizer depend on BuildFixtures
    Also, remove debugging code.

--- lib/test/tasks/run_typelizer.rb
+++ lib/test/tasks/run_typelizer.rb
@@ -2,13 +2,7 @@ class Test::Tasks::RunTypelizer < Pallets::Task
   include Test::TaskHelpers
 
   def run
-    pp(Rake::Task.tasks)
-    begin
-      execute_rake_task('typelizer:generate')
-    rescue => error
-      pp(error)
-      puts(error.backtrace)
-    end
+    execute_rake_task('typelizer:generate')
     execute_system_command("! grep --quiet -RP '\\bunknown\\b' app/javascript/types/serializers/")
     execute_system_command('git diff --exit-code')
   end
