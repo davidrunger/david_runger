bd693561f134c89a794c2c9a44f3d32798de7b39
David Runger <davidjrunger@gmail.com>
2024-08-23 15:35:59 -0500

    [ConvertSchemasToTs] Lean on TaskHelpers more
    This might be less performant than loading the Ruby library directly,
    but maybe it's worth it to simplify things and be more consistent. Plus,
    this will provide better failure output, I think.

--- lib/test/tasks/convert_schemas_to_ts.rb
+++ lib/test/tasks/convert_schemas_to_ts.rb
@@ -1,26 +1,13 @@
-require Rails.root.join('lib/json_schemas_to_typescript.rb')
-
 class Test::Tasks::ConvertSchemasToTs < Pallets::Task
   include Test::TaskHelpers
 
   def run
     puts("Running '#{AmazingPrint::Colors.yellow(self.class.name)}' ...")
 
-    time =
-      Benchmark.measure do
-        JsonSchemasToTypescript.write_files
-      end.real
-
-    if execute_system_command('test -z "$(git status --porcelain)"')
-      record_success_and_log_message("'#{self.class.name}' succeeded (took #{time.round(3)}).")
-    else
-      execute_system_command('git diff')
-      # Reset the git state, so it's clean for other test tasks.
-      execute_system_command('git checkout .', log_stdout_only_on_failure: true)
-      record_failed_command('bin/json-schemas-to-typescript')
-      record_failure_and_log_message(<<~LOG.squish)
-        Executing JsonSchemasToTypescript.write_files introduced a diff.
-      LOG
-    end
+    execute_system_command(<<~COMMAND)
+      bin/json-schemas-to-typescript && { test -z "$(git status --porcelain)" || git diff }
+    COMMAND
+    # Reset the git state, so it's clean for other test tasks.
+    execute_system_command('git checkout .', log_stdout_only_on_failure: true)
   end
 end
