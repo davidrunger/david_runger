9ff369e8194cd8cba300456f236213324be5dc00
David Runger <davidjrunger@gmail.com>
2024-08-23 15:49:00 -0500

    [ConvertSchemasToTs] Don't suppress git diff output
    The whole point is to print it to the log.

--- lib/test/tasks/convert_schemas_to_ts.rb
+++ lib/test/tasks/convert_schemas_to_ts.rb
@@ -7,7 +7,7 @@ class Test::Tasks::ConvertSchemasToTs < Pallets::Task
     if !execute_system_command(<<~COMMAND)
       bin/json-schemas-to-typescript && test -z "$(git status --porcelain)"
     COMMAND
-      execute_system_command('git diff', log_stdout_only_on_failure: true)
+      execute_system_command('git diff')
       # Reset the git state, so it's clean for other test tasks.
       execute_system_command('git checkout .', log_stdout_only_on_failure: true)
     end
