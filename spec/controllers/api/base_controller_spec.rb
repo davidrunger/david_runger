0487bf5230444af0d9dc88f002436ada4d81ecfe
David Runger <davidjrunger@gmail.com>
2024-08-24 02:41:52 -0500

    Fix spec broken by removing 'schema' keyword argument

--- spec/controllers/api/base_controller_spec.rb
+++ spec/controllers/api/base_controller_spec.rb
@@ -3,26 +3,34 @@ RSpec.describe Api::BaseController, :without_verifying_authorization do
     skip_before_action :authenticate_user!
 
     def index
-      render_schema_json({ some_really: 'amazing data' }, schema: params[:schema])
+      render_schema_json({ some_really: 'amazing data' })
     end
   end
 
   describe '#render_schema_json' do
-    subject(:get_index) { get(:index, params: { schema: }) }
+    subject(:get_index) { get(:index) }
 
     context 'when there is no JSON schema for the action' do
-      let(:schema) { 'no/such/schema' }
-
       it 'raises a JSON::Schema::ReadFailed error' do
         expect { get(:index) }.to raise_error(JSON::Schema::ReadFailed)
       end
     end
 
-    context 'when the rendered data does not conform to the JSON schema' do
-      let(:schema) { 'items/destroy' }
+    context 'when there is a JSON schema for the action' do
+      before do
+        allow(Rails.root).to receive(:join).and_wrap_original do |original_method, *args|
+          if args == ['spec/support/schemas/base/index.json']
+            Rails.root.join('spec/support/schemas/items/create.json')
+          else
+            original_method.call(*args)
+          end
+        end
+      end
 
-      it 'raises an Api::BaseController::SchemaValidationError' do
-        expect { get_index }.to raise_error(Api::BaseController::SchemaValidationError)
+      context 'when the rendered data does not conform to the JSON schema' do
+        it 'raises an Api::BaseController::SchemaValidationError' do
+          expect { get_index }.to raise_error(Api::BaseController::SchemaValidationError)
+        end
       end
     end
   end
