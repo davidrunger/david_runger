273d19eff7df916bf7e6d6dcd9b92b76c61d7da2
David Runger <davidjrunger@gmail.com>
2024-08-31 21:50:19 -0500

    [crontab] Rebuild the app after pruning docker (#5062)
    Otherwise, I think that what can happen is this:
    1. we prune the Docker cache
    1. we reboot the app
    1. the images are now considered stale because the cache was pruned (?)
    1. Docker tries to build fresh images, but this fails because
    RUBY_VERSION is not provided
    1. the app fails to boot
    
    I think that I ran into this scenario after manually rebooting today (a
    Saturday, where no deploys i.e. builds have gone out yet today).
    
    I'm not sure why this hasn't been happening every Saturday, when our
    crontab performs a scheduled reboot? Is it possible we were rebooting
    before the pruning actually completed? I guess not, because then why
    would I have experienced the issue after manually rebooting today? But,
    due to this possibility (even though it now seems relatively unlikely to
    me), I am moving back the reboot time further after the prune and
    rebuild.
    
    It's actually possible that the reason that I ran into this issue is
    because I had just updated system packages on the server (probably
    including Docker itself)?
    
    Hopefully this will avoid the problematic scenario detailed above.

---
+++ bin/server/prune-docker-and-rebuild.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail # exit on any error, don't allow undefined variables, pipes don't swallow errors
+
+# Move to the application directory.
+cd /root/david_runger
+
+# Prune Docker.
+docker system prune --all --force
+
+# Rebuild Docker image(s).
+bin/build-docker production
