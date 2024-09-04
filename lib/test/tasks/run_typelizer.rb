2f8160e439f0eb2b70f15bb236ea3f84e7bb140a
David Runger <davidjrunger@gmail.com>
2024-09-03 22:52:30 -0500

    [RunAnnotate] Run 'git checkout .' if a diff is introduced [DEV-107]

--- lib/test/tasks/run_typelizer.rb
+++ lib/test/tasks/run_typelizer.rb
@@ -4,6 +4,7 @@ class Test::Tasks::RunTypelizer < Pallets::Task
   def run
     execute_rake_task('typelizer:generate')
     execute_system_command("! grep --quiet -RP '\\bunknown\\b' app/javascript/types/serializers/")
+
     if !execute_system_command('git diff --exit-code')
       # Reset the git state, so it's clean for other test tasks.
       execute_system_command('git checkout .')
