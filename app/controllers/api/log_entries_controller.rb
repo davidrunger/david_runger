8bf266627f29c2a43a95e2f48b05a798076f2925
David Runger <davidjrunger@gmail.com>
2024-09-03 20:59:45 -0500

    Prefix some local variable names with 'datum_'

--- app/controllers/api/log_entries_controller.rb
+++ app/controllers/api/log_entries_controller.rb
@@ -76,8 +76,8 @@ class Api::LogEntriesController < Api::BaseController
 
   def log_entry_json_strings_for_log(log)
     datum_class = log.log_entry_datum_class
-    table_name = datum_class.table_name
-    class_name = datum_class.name
+    datum_table_name = datum_class.table_name
+    datum_class_name = datum_class.name
 
     ApplicationRecord.connection.select_values(<<~SQL.squish)
       SELECT row_to_json(log_entry)
@@ -88,18 +88,18 @@ class Api::LogEntriesController < Api::BaseController
           data,
           log_entries.log_id,
           log_entries.note
-        FROM #{table_name}
+        FROM #{datum_table_name}
         INNER JOIN log_entries
-          ON log_entries.log_entry_datum_id = #{table_name}.id
-          AND log_entries.log_entry_datum_type = '#{class_name}'
+          ON log_entries.log_entry_datum_id = #{datum_table_name}.id
+          AND log_entries.log_entry_datum_type = '#{datum_class_name}'
         WHERE log_entries.log_id = #{log.id}
       ) log_entry;
     SQL
   end
 
   def log_entry_json_strings_for_user_and_datum_class(user:, datum_class:)
-    table_name = datum_class.table_name
-    class_name = datum_class.name
+    datum_table_name = datum_class.table_name
+    datum_class_name = datum_class.name
 
     ApplicationRecord.connection.select_values(<<~SQL.squish)
       SELECT row_to_json(log_entry)
@@ -110,13 +110,13 @@ class Api::LogEntriesController < Api::BaseController
             log_entries.created_at AT TIME ZONE 'UTC',
             'YYYY-MM-DD"T"HH24:MI:SS"Z"'
           ) AS created_at,
-          #{table_name}.data,
+          #{datum_table_name}.data,
           log_entries.log_id,
           log_entries.note
-        FROM #{table_name}
+        FROM #{datum_table_name}
         INNER JOIN log_entries
-          ON log_entries.log_entry_datum_id = #{table_name}.id
-          AND log_entries.log_entry_datum_type = '#{class_name}'
+          ON log_entries.log_entry_datum_id = #{datum_table_name}.id
+          AND log_entries.log_entry_datum_type = '#{datum_class_name}'
         INNER JOIN logs
           ON logs.id = log_entries.log_id
         WHERE logs.user_id = #{user.id}
