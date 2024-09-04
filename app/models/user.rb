f269e85664df2ee806fb430b3a912f055364b01c
David Runger <davidjrunger@gmail.com>
2024-09-03 21:21:42 -0500

    Delete User#text_log_entry_data association
    I don't think we ended up using it?

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,10 +30,6 @@ class User < ApplicationRecord
   has_many :stores, dependent: :destroy
   has_many :items, through: :stores # must come after has_many :stores declaration
   has_many :log_entries, through: :logs
-  has_many :text_log_entry_data,
-    through: :log_entries,
-    source: :log_entry_datum,
-    source_type: 'TextLogEntryDatum'
   has_many :workouts, dependent: :destroy
   has_many :need_satisfaction_ratings, dependent: :destroy
   has_many :check_in_submissions, dependent: :destroy
