d92d41d8b20fc685b2b6bdfc64e2e5aa6678e085
David Runger <davidjrunger@gmail.com>
2024-09-03 22:57:34 -0500

    [RunAnnotate] Run 'git checkout .' if a diff is introduced [DEV-107] (#5087)

--- lib/test/tasks/run_annotate.rb
+++ lib/test/tasks/run_annotate.rb
@@ -3,7 +3,12 @@ class Test::Tasks::RunAnnotate < Pallets::Task
 
   def run
     execute_system_command(<<~COMMAND)
-      bin/annotate --models --show-indexes --sort --exclude fixtures,tests && git diff --exit-code
+      bin/annotate --models --show-indexes --sort --exclude fixtures,tests
     COMMAND
+
+    if !execute_system_command('git diff --exit-code')
+      # Reset the git state, so it's clean for other test tasks.
+      execute_system_command('git checkout .')
+    end
   end
 end
