d03e1999211bc2d4714a990cc712a60e4747428f
David Runger <davidjrunger@gmail.com>
2024-09-03 22:46:36 -0500

    Update 'annotate' comments

--- app/models/number_log_entry_datum.rb
+++ app/models/number_log_entry_datum.rb
@@ -5,13 +5,7 @@
 #  created_at :datetime         not null
 #  data       :float            not null
 #  id         :bigint           not null, primary key
-#  log_id     :bigint
-#  note       :string
 #  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_number_log_entry_data_on_log_id  (log_id)
 #
 class NumberLogEntryDatum < ApplicationRecord
   include LogEntryDatum
--- spec/factories/text_log_entry_data.rb
+++ spec/factories/text_log_entry_data.rb
@@ -5,13 +5,7 @@
 #  created_at :datetime         not null
 #  data       :text             not null
 #  id         :bigint           not null, primary key
-#  log_id     :bigint
-#  note       :string
 #  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_text_log_entry_data_on_log_id  (log_id)
 #
 FactoryBot.define do
   factory :text_log_entry_datum do
