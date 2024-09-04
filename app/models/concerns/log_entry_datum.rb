ff7d8b43f238674b83757f0ebbfe7de112d2ac6a
David Runger <davidjrunger@gmail.com>
2024-09-03 22:38:34 -0500

    Clean up database and code after move to delegated types [LOG-23]

--- app/models/concerns/log_entry_datum.rb
+++ app/models/concerns/log_entry_datum.rb
@@ -2,8 +2,6 @@ module LogEntryDatum
   extend ActiveSupport::Concern
 
   included do
-    # NOTE: This log association is only temporary, during a migration/transition phase.
-    belongs_to :log, optional: true
     has_one :log_entry, as: :log_entry_datum, touch: true, dependent: :destroy
 
     validates :data, presence: true
