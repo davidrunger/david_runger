5ced7fbef6cf72d57d535aa1cbc91b4770d5217c
David Runger <davidjrunger@gmail.com>
2024-09-05 09:50:26 -0500

    [ConvertSchemasToTs] Remove puts printing the class name
    I'm not sure why I added this. It somewhat clutters and confuses the
    test output, and it breaks the pattern we have with other log lines that
    print in yellow which then should have a corresponding green or red
    follow-up log line.

--- lib/test/tasks/convert_schemas_to_ts.rb
+++ lib/test/tasks/convert_schemas_to_ts.rb
@@ -2,8 +2,6 @@ class Test::Tasks::ConvertSchemasToTs < Pallets::Task
   include Test::TaskHelpers
 
   def run
-    puts("Running '#{AmazingPrint::Colors.yellow(self.class.name)}' ...")
-
     if !execute_system_command(<<~COMMAND)
       bin/json-schemas-to-typescript && git diff --exit-code
     COMMAND
