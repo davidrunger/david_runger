442351f3137c4e7d18d43bf8ea9e7aa81ce69f6f
David Runger <davidjrunger@gmail.com>
2024-09-06 03:39:34 -0500

    [specs] Raise error if ApplicationRecord::Base.connection is called
    fixes https://app.rollbar.com/a/davidjrunger/fix/item/davidrunger/672
    
    Unfortunately, I think that this requires monkeypatching ActiveRecord.
    At least, that's the only solution that I can think of. So, that's what
    I'm doing. Fortunately, it's only in test that we need to do it. The
    reason is that something about how tests set up their database
    connection (which I think then gets used by the application code) causes
    the error to not occur in some situations when it would actually occur
    in development or production. In the original ActiveRecord code, the
    `permanent_connection_checkout` config is only checked if
    `pool.permanent_lease?` is truthy. It's falsy in test, though. This
    monkeypatch changes the code to check the
    `permanent_connection_checkout` config whether `pool.permanent_lease?`
    is truthy or falsy.
    
    This also requires forking ActiveAdmin, because it uses
    `ActiveRecord::Base.connection` (via `resource_class.connection`) in one
    place. I have submitted a PR to upstream my fork:
    https://github.com/activeadmin/activeadmin/pull/ 8474 .

---
+++ config/initializers/monkeypatches/active_record_connection_handling.rb
@@ -0,0 +1,36 @@
+if Rails.env.test?
+  # Code being patched:
+  # https://github.com/rails/rails/blob/v7.2.1/activerecord/lib/active_record/connection_handling.rb#L259-L281
+  module Runger::ConnectionHandlingMonkeypatch
+    def connection
+      pool = connection_pool
+      check_calling_connection_allowed
+      if pool.permanent_lease?
+        pool.lease_connection
+      else
+        pool.active_connection
+      end
+    end
+
+    def check_calling_connection_allowed
+      case ActiveRecord.permanent_connection_checkout
+      when :deprecated
+        ActiveRecord.deprecator.warn(<<~MESSAGE)
+          Called deprecated `ActiveRecord::Base.connection` method.
+
+          Either use `with_connection` or `lease_connection`.
+        MESSAGE
+      when :disallowed
+        raise(ActiveRecord::ActiveRecordError, <<~MESSAGE)
+          Called deprecated `ActiveRecord::Base.connection` method.
+
+          Either use `with_connection` or `lease_connection`.
+        MESSAGE
+      end
+    end
+  end
+
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::ConnectionHandling.prepend(Runger::ConnectionHandlingMonkeypatch)
+  end
+end
--- spec/workers/truncate_tables_spec.rb
+++ spec/workers/truncate_tables_spec.rb
@@ -1,19 +1,27 @@
 RSpec.describe TruncateTables do
   subject(:worker) { TruncateTables.new }
 
+  # rubocop:disable RSpec/InstanceVariable
   describe '#perform' do
     subject(:perform) { worker.perform }
+
+    around do |spec|
+      ApplicationRecord.with_connection do |connection|
+        @connection = connection
+        spec.run
+      end
+    end
 
     context 'when there is at least one row in the `requests` table' do
       before { expect(Request.count).to be > 0 }
 
       it 'issues a DELETE command against the `requests` table' do
-        expect(ApplicationRecord.connection).to receive(:execute).
+        expect(@connection).to receive(:execute).
           with(/DELETE FROM requests/).
           and_call_original
 
         # pass other calls through
-        allow(ApplicationRecord.connection).to receive(:execute).and_call_original
+        allow(@connection).to receive(:execute).and_call_original
 
         perform
       end
@@ -23,15 +31,16 @@ RSpec.describe TruncateTables do
       before { Request.delete_all }
 
       it 'does not issue a DELETE command against the `requests` table' do
-        expect(ApplicationRecord.connection).
+        expect(@connection).
           not_to receive(:execute).
           with(/DELETE FROM requests/i)
 
         # pass other calls through
-        allow(ApplicationRecord.connection).to receive(:execute).and_call_original
+        allow(@connection).to receive(:execute).and_call_original
 
         perform
       end
     end
   end
+  # rubocop:enable RSpec/InstanceVariable
 end
