2080465d673b32ac1dc1ce217c2ff0213244ee73
David Runger <davidjrunger@gmail.com>
2024-09-29 11:28:46 -0500

    [RequestRecordable] Log hash when JSON::GeneratorError is raised (#5240)
    fixes rb#666
    
    https://app.rollbar.com/a/davidjrunger/fix/item/davidrunger/666
    
    This doesn't actually fix the above exception, but hopefully it will
    give more detailed insight into what is causing it.

--- spec/controllers/concerns/request_recordable_spec.rb
+++ spec/controllers/concerns/request_recordable_spec.rb
@@ -14,6 +14,8 @@ RSpec.describe RequestRecordable, :without_verifying_authorization do
       end
     end
 
+    let(:params) { {} }
+
     context 'when a user is not logged in' do
       before { controller.sign_out_all_scopes }
 
@@ -23,6 +25,26 @@ RSpec.describe RequestRecordable, :without_verifying_authorization do
 
         it 'includes the auth token id' do
           expect(data_stashed_in_redis_after_request).to include('auth_token_id' => auth_token.id)
+        end
+      end
+
+      context 'when a JSON::GeneratorError is raised' do
+        before do
+          # rubocop:disable RSpec/AnyInstance
+          allow_any_instance_of(Hash).to receive(:to_json).and_raise(JSON::GeneratorError)
+          # rubocop:enable RSpec/AnyInstance
+        end
+
+        it 'logs the hash and re-raises the error' do
+          allow(Rails.logger).to receive(:info).and_call_original
+
+          expect { data_stashed_in_redis_after_request }.to raise_error(JSON::GeneratorError)
+
+          expect(Rails.logger).
+            to have_received(:info).
+            with(
+              /\[RequestRecordable\]\[JSON::GeneratorError\].*:format=>:html/,
+            )
         end
       end
     end
