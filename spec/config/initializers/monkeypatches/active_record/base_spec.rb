11fc8dda40c0076e1488237df0271878a56875e4
David Runger <davidjrunger@gmail.com>
2024-11-08 12:24:03 -0600

    Reallow ApplicationRecord::Base.connection (#5471)
    https://github.com/activeadmin/activeadmin/pull/ 8474 is not going to be
    merged (at least, not in the near future), and so this won't be fixed
    upstream in activeadmin anytime soon. Therefore, let's give up and get
    back onto upstream activeadmin (and ransack) now by reallowing
    `connection`.

--- spec/config/initializers/monkeypatches/active_record/base_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe(ActiveRecord::Base) do
-  describe '::connection' do
-    subject(:connection) { ActiveRecord::Base.connection }
-
-    it 'raises an error' do
-      expect { connection }.to raise_error(ActiveRecord::ActiveRecordError)
-    end
-  end
-end
