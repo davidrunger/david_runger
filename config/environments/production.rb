9175a3b701930c94ecf5dba872ee7ba1b4c0527d
David Runger <davidjrunger@gmail.com>
2024-11-08 18:12:12 -0600

    Fix comment about attributes used for 'inspect' in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Only use :id for inspections in production.
+  # Use all attributes for inspections in production.
   config.active_record.attributes_for_inspect = :all
 
   # Enable DNS rebinding protection and other `Host` header attacks.
