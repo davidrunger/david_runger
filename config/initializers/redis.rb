81cfcbf8ebb52cc1d475b918099375b405ff0f53
David Runger <davidjrunger@gmail.com>
2025-01-13 23:50:43 -0600

    Remove RAILS_MAX_THREADS fallback and change its value from 3 to 2 (#5796)
    Motivation for removing the fallback: It essentially DRYs things up.
    Previously, we had the same fallback value in three or four different
    files and would need to update it in each of those places whenever we
    wanted to change it.
    
    Motivation for reducing from 3 to 2: Hopefully this will save us a
    little bit of memory, which I think that we might want/need, partially
    because I am thinking about adding yet more services to our Docker
    Compose setup (specifically Loki and Grafana), in addition to having
    recently added Vector.
    
    Sidekiq will use RAILS_MAX_THREADS as its default concurrency ("Starting
    in Rails 5, `RAILS_MAX_THREADS` can be used to configure Rails and
    Sidekiq concurrency", [source][1]), so I'm also removing the concurrency
    specifications from `config/sidekiq.yml`, since they are basically WET
    with the specifications of RAILS_MAX_THREADS in `.env.production` (added
    in this change) and `.env.development`.
    
    [1]:
    https://github.com/sidekiq/sidekiq/wiki/Advanced-Options#concurrency

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,5 @@ unless IS_DOCKER_BUILD
   redis_options = RedisOptions.new
 
   redis_config = RedisClient.config(url: redis_options.url)
-  $redis_pool = redis_config.new_pool(size: Integer(ENV.fetch('RAILS_MAX_THREADS', 3)))
+  $redis_pool = redis_config.new_pool(size: Integer(ENV.fetch('RAILS_MAX_THREADS')))
 end
