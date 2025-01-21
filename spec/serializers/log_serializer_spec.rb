66443fafe1ab27a1dea8c9ea2c8f3264521b57a4
David Runger <davidjrunger@gmail.com>
2025-01-21 01:25:25 -0600

    Annotate LogSerializer spec

--- spec/serializers/log_serializer_spec.rb
+++ spec/serializers/log_serializer_spec.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: logs
+#
+#  created_at               :datetime         not null
+#  data_label               :string           not null
+#  data_type                :string           not null
+#  description              :string
+#  id                       :bigint           not null, primary key
+#  name                     :string           not null
+#  publicly_viewable        :boolean          default(FALSE), not null
+#  reminder_last_sent_at    :datetime
+#  reminder_time_in_seconds :integer
+#  slug                     :string           not null
+#  updated_at               :datetime         not null
+#  user_id                  :bigint           not null
+#
+# Indexes
+#
+#  index_logs_on_user_id_and_name  (user_id,name) UNIQUE
+#  index_logs_on_user_id_and_slug  (user_id,slug) UNIQUE
+#
 RSpec.describe(LogSerializer) do
   subject(:log_serializer) do
     LogSerializer.new(logs, params: { current_user: })
