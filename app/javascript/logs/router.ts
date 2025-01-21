cd469773a4adddac37e93e4b19f18a2c54c4cdc5
David Runger <davidjrunger@gmail.com>
2025-01-20 23:22:23 -0600

    [logs] Don't show log selector tip when in shared-log view
    (This applies whenever the path is a shared log path, regardless of
    whether a user is viewing their own shared log at that path (i.e.
    previewing the share view) or viewing someone else's shared log.)
    
    When viewing someone else's shared log, there are no other logs
    available to select, so we shouldn't suggest the log selector.
    
    When viewing one's own log at a shared path, we want that "preview" view
    to accurately reflect what other user will see (which, as mentioned
    just above, won't include a tip about the log selector).

--- app/javascript/logs/router.ts
+++ app/javascript/logs/router.ts
@@ -8,7 +8,12 @@ import LogsIndex from './components/LogsIndex.vue';
 const routes = [
   { path: '/logs', name: 'logs-index', component: LogsIndex },
   { path: '/logs/:slug', name: 'log', component: Log },
-  { path: '/users/:user_id/logs/:slug', name: 'shared_log', component: Log },
+  {
+    path: '/users/:user_id/logs/:slug',
+    name: 'shared_log',
+    component: Log,
+    meta: { sharedView: true },
+  },
 ];
 
 const router = createRouter({
