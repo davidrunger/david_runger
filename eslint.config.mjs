d19cde97248ed46243a42592d4f817b1a44dfc8e
David Runger <davidjrunger@gmail.com>
2025-01-23 11:59:33 -0600

    Run eslint 9 config migration tool
    `pnpm dlx  @eslint/migrate-config .eslintrc.js`
    
    It prints this warning:
    
    > WARNING: This tool does not yet work great for .eslintrc.(js|cjs|mjs)
    > files. It will convert the evaluated output of our config file, not
    > the source code. Please review the output carefully to ensure it is
    > correct.

---
+++ eslint.config.mjs
@@ -0,0 +1,130 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import _import from "eslint-plugin-import";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import vue from "eslint-plugin-vue";
+import globals from "globals";
+import parser from "vue-eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: [
+        "app/javascript/rails_assets/",
+        "app/javascript/types/bootstrap/",
+        "app/javascript/types/responses/",
+        "app/javascript/types/serializers/",
+        "node_modules/",
+        "public/vite-admin/",
+        "public/vite/",
+    ],
+}, ...fixupConfigRules(compat.extends(
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "@vue/eslint-config-typescript",
+    "prettier",
+)), {
+    plugins: {
+        import: fixupPluginRules(_import),
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        vue: fixupPluginRules(vue),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            Routes: false,
+        },
+
+        parser: parser,
+        ecmaVersion: 5,
+        sourceType: "module",
+
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+        },
+    },
+
+    settings: {
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts"],
+        },
+
+        "import/resolver": {
+            typescript: {
+                alwaysTryTypes: true,
+            },
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "arrow-parens": "off",
+        "function-paren-newline": "off",
+
+        "import/extensions": ["error", "ignorePackages", {
+            js: "never",
+            ts: "never",
+        }],
+
+        "import/no-extraneous-dependencies": "off",
+
+        "import/no-unresolved": ["error", {
+            ignore: ["chartjs-adapter-luxon", "vue-chartjs"],
+        }],
+
+        "import/prefer-default-export": "off",
+
+        "max-len": ["warn", {
+            code: 100,
+            ignoreUrls: true,
+        }],
+
+        "newline-per-chained-call": "off",
+        "no-alert": "off",
+        "no-console": "warn",
+        "no-debugger": "warn",
+        "no-else-return": "off",
+        "no-new": "off",
+        "no-param-reassign": "off",
+        "no-plusplus": "off",
+
+        "no-restricted-imports": ["error", {
+            paths: [{
+                name: "lodash",
+                message: "Use lodash-es.",
+            }],
+        }],
+
+        "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"],
+        "no-underscore-dangle": "off",
+        "no-unreachable": "warn",
+
+        "no-use-before-define": ["error", {
+            functions: false,
+        }],
+
+        "no-unused-vars": "off",
+        "object-shorthand": ["error", "always"],
+        "operator-linebreak": "off",
+
+        quotes: ["warn", "single", {
+            avoidEscape: true,
+        }],
+
+        "require-await": "error",
+        "vue/multi-word-component-names": "off",
+    },
+}];
