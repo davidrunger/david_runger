dd6162f079f77977e81a0bfbe18da5714950eb9d
David Runger <davidjrunger@gmail.com>
2025-01-31 00:49:24 -0600

    Rename app/javascript/{admin_packs => admin_entrypoints}/
    This parallels #5971 .

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -2,7 +2,7 @@ module AssetsHelper
   def admin_ts_tag(pack_name)
     js_file_path =
       JSON.parse(File.read('public/vite-admin/.vite/manifest.json')).
-        dig("admin_packs/#{pack_name}.ts", 'file')
+        dig("admin_entrypoints/#{pack_name}.ts", 'file')
     javascript_include_tag("/vite-admin/#{js_file_path}")
   end
 
