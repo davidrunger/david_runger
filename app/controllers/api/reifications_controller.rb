1a96980042a1bbe4d4e4425a736b0c5eec3a94e6
David Runger <davidjrunger@gmail.com>
2025-02-02 16:45:35 -0600

    Rename ReificationsPolicy => ReificationPolicy (singular) [DEV-152]
    All of our other policy classes are named with a singular noun. This
    change renames ReificationsPolicy (plural) to ReificationPolicy
    (singular) in order to be consistent with that pattern.

--- app/controllers/api/reifications_controller.rb
+++ app/controllers/api/reifications_controller.rb
@@ -2,7 +2,7 @@ class Api::ReificationsController < Api::BaseController
   def create
     @version = PaperTrail::Version.find(params[:paper_trail_version_id])
 
-    authorize(@version, :create?, policy_class: ReificationsPolicy)
+    authorize(@version, :create?, policy_class: ReificationPolicy)
 
     @version.reify.save!
 
