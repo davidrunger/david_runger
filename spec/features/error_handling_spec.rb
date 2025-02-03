6f4a5af9905c50d51cba36a3f521eb7ec29c5662
David Runger <davidjrunger@gmail.com>
2025-02-02 18:58:19 -0600

    [specs] Consolidate error-handling examples and suppress actual logs
    With Cuprite-using feature specs, they are costly enough that it makes
    sense to consolidate multiple different expectations into a single
    example.
    
    The spec(s) in question here are run with the `rack_test` driver, and so
    are much faster. However, I still don't know that the cost of splitting
    them up is justified. I don't think splitting these increases the
    clarity of the spec(s) very much, so I don't think there is much upside.
    
    Also, this change suppresses logs from actually being written to
    `log/test.log` by stubbing some methods on `Rails.logger` (without
    `and_call_original`). Motivation: when looking at logs, these log lines
    seem concerning and like something that needs to be investigated, which
    is sort of a waste of time, because these log lines are expected. So,
    instead of actually writing the log lines, let's stub them, instead.

--- spec/features/error_handling_spec.rb
+++ spec/features/error_handling_spec.rb
@@ -16,19 +16,27 @@ RSpec.describe 'Handling exceptions', :rack_test_driver do
       context 'when a non-RoutingError is raised' do
         before do
           expect(BrowserSupportChecker).to receive(:new).and_raise(StandardError.new(error_message))
+
+          # Don't actually write log messages for the error.
+          allow(Rails.logger).to receive(:error)
+          allow(Rails.logger).to receive(:add)
         end
 
         let(:error_message) { 'A BrowserSupportChecker instance could not be initialized!' }
 
-        it 'renders the 500 error page and responds with 500' do
+        it 'renders the 500 error page, responds with 500, does not expose the error message, and logs an error' do
           visit(upgrade_browser_path)
+
           expect(page).to have_css('h1', text: 'Sorry, something went wrong.')
           expect(page.status_code).to be(500)
-        end
-
-        it 'does not expose the error message' do
-          visit(upgrade_browser_path)
           expect(page).not_to have_text(error_message)
+          expect(Rails.logger).to have_received(:error).with(<<~ERROR_LOG.squish)
+            [error-report:error] StandardError : A BrowserSupportChecker
+            instance could not be initialized! |
+            handled=false source=application.action_dispatch
+            controller_action="home#upgrade_browser"
+          ERROR_LOG
+          expect(Rails.logger).to have_received(:add).with(Logger::ERROR, /#{error_message}/)
         end
       end
     end
