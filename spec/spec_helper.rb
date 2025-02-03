44dac453135d937e053ceea4989cdf76a888f2ec
David Runger <davidjrunger@gmail.com>
2025-02-02 20:49:20 -0600

    Focus spec that is failing in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -403,9 +403,9 @@ RSpec.configure do |config|
   # is tagged with `:focus`, all examples get run. RSpec also provides
   # aliases for `it`, `describe`, and `context` that include `:focus`
   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
-  if !SpecHelper.is_ci?
+  # if !SpecHelper.is_ci?
     config.filter_run_when_matching(:focus)
-  end
+  # end
 
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
