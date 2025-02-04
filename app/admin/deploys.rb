075037d07033c772e7fce967820a27cd7787a130
David Runger <davidjrunger@gmail.com>
2025-02-04 03:14:31 -0600

    [admin/deploys] Make Git SHAs GitHub commit links [ADM-1]
    This will make it more convenient / faster to see what a particular
    deploy was deploying.
    
    A single deploy could include multiple new commits, and so a compare
    view from the previously deployed SHA to the SHA of each deploy in
    question would probably be a better approach. However, that would be
    somewhat difficult to do without an N + 1 query; at least, I don't think
    it would be straightforward to do while simply leveraging  ActiveAdmin's
    capacity to automatically construct most of the view. So, for now, for
    simplicity, I am going to just ship this change. I think that hopefully
    this 20% of the work will still deliver 80% of the possible benefits.
    
    Also, while looking at the show page and creating a decorator, make the
    main title prettier by defining a `to_s` method on the decorator.

--- app/admin/deploys.rb
+++ app/admin/deploys.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register(Deploy) do
+  decorate_with DeployDecorator
   menu parent: 'Admin'
+
+  index do
+    id_column
+    column :github_commit_link
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :github_commit_link
+      row :created_at
+      row :updated_at
+    end
+
+    active_admin_comments
+  end
 end
--- spec/controllers/admin/deploys_controller_spec.rb
+++ spec/controllers/admin/deploys_controller_spec.rb
@@ -5,11 +5,16 @@ RSpec.describe Admin::DeploysController do
     describe '#index' do
       subject(:get_index) { get(:index) }
 
-      it 'lists deploys' do
+      it 'lists deploys with the commit SHA as a GitHub link' do
         get_index
 
+        expect(response).to have_http_status(200)
+
         Deploy.all.presence!.each do |deploy|
-          expect(response.body).to have_text(deploy.git_sha)
+          expect(response.body).to have_link(
+            deploy.git_sha,
+            href: "https://github.com/davidrunger/david_runger/commit/#{deploy.git_sha}",
+          )
         end
       end
     end
@@ -19,11 +24,15 @@ RSpec.describe Admin::DeploysController do
 
       let(:deploy) { deploys(:deploy) }
 
-      it 'responds with 200' do
+      it 'responds with 200 and links to the commit on GitHub' do
         get_show
 
         expect(response).to have_http_status(200)
-        expect(response.body).to have_text(deploy.git_sha)
+
+        expect(response.body).to have_link(
+            deploy.git_sha,
+            href: "https://github.com/davidrunger/david_runger/commit/#{deploy.git_sha}",
+          )
       end
     end
   end
