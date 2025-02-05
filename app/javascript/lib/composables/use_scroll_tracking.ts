c4772eda0ae8017f8c49bef6725fa15ecf2fb0c9
David Runger <davidjrunger@gmail.com>
2025-02-04 18:37:00 -0600

    [home] Save scroll Events [HOME-13] (#6011)
    This will probably be a decent indicator of a real human user, which is
    something that I am interested in.
    
    This change only creates one scroll Event per page load. A user might
    trigger very many scroll events, but there's only really value in
    storing one.

---
+++ app/javascript/lib/composables/use_scroll_tracking.ts
@@ -0,0 +1,25 @@
+import { onMounted, onUnmounted } from 'vue';
+
+import { trackEvent } from '@/lib/events';
+
+let trackedScrollEvent = false;
+
+function trackScrollEvent() {
+  if (!trackedScrollEvent) {
+    trackEvent('scroll', {
+      page_url: window.location.href,
+    });
+
+    trackedScrollEvent = true;
+  }
+}
+
+export function useScrollTracking() {
+  onMounted(() => {
+    window.addEventListener('scroll', trackScrollEvent);
+
+    onUnmounted(() => {
+      window.removeEventListener('scroll', trackScrollEvent);
+    });
+  });
+}
