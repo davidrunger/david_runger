09ea63f39790ad431b276facbe084b492dbdd0b3
David Runger <davidjrunger@gmail.com>
2025-02-05 10:11:43 -0600

    [admin/deploys] Add GitHub compare view link since previous [ADM-2] (#6018)
    Shoutout to ChatGPT for giving me this `LAG` query! (Private link:
    https://chatgpt.com/c/67a36fb6-74c0-8007-99c8-67f662464c79)

--- app/admin/deploys.rb
+++ app/admin/deploys.rb
@@ -2,9 +2,19 @@ ActiveAdmin.register(Deploy) do
   decorate_with DeployDecorator
   menu parent: 'Admin'
 
+  controller do
+    def scoped_collection
+      Deploy.select(<<~SQL.squish)
+        deploys.*,
+        LAG(deploys.git_sha) OVER (ORDER BY deploys.created_at) AS previous_git_sha
+      SQL
+    end
+  end
+
   index do
     id_column
     column('Git SHA') { it.github_commit_link }
+    column('Diff') { it.github_diff_link }
     column :created_at
     column :updated_at
     actions
@@ -14,6 +24,7 @@ ActiveAdmin.register(Deploy) do
     attributes_table do
       row :id
       row('Git SHA') { it.github_commit_link }
+      row('Diff') { it.github_diff_link }
       row :created_at
       row :updated_at
     end
