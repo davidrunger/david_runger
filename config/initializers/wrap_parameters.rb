c15ddaa41109383c1f6aaaef4113c98f63cb1ff5
David Runger <davidjrunger@gmail.com>
2025-02-07 14:54:29 -0600

    Track scrolling with http/ky/fetch (not sendBeacon) [HOME-15] (#6045)
    At least one ad blocker (uBlock Origin) blocks `sendBeacon`, but I doubt
    that any block `fetch`. We use `sendBeacon` for external link clicks
    because those need to be sent asynchronously (because otherwise the
    request might be cancelled as the user navigates away), but we don't
    really need that for scrolls. So, this change uses `fetch` (via ky, via
    our http wrapper library) to track scrolls, which should be more
    reliable.

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -5,7 +5,7 @@
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
 ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json]
+  wrap_parameters format: []
   ActionController::Parameters.action_on_unpermitted_parameters = false
 end
 
