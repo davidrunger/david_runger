db67b9c2cc6e33822b183c8a4d8177d959b2fdef
David Runger <davidjrunger@gmail.com>
2025-02-07 19:23:10 -0600

    Remove isArrayOfStrings type predicate
    I considered leaving it, since it seems potentially valuable, but I
    guess I'll just add it back if I ever really need it, rather than leave
    it lying about. YAGNI and all that.

--- app/javascript/lib/type_predicates.ts
+++ app/javascript/lib/type_predicates.ts
@@ -1,13 +1,6 @@
 import { TextLogEntry } from '@/logs/types';
 import { LogEntry } from '@/types';
 
-export function isArrayOfStrings(object: unknown): object is Array<string> {
-  return (
-    Array.isArray(object) &&
-    object.every((element) => typeof element === 'string')
-  );
-}
-
 export function isArrayOfTextLogEntries(
   logEntries: Array<LogEntry>,
 ): logEntries is Array<TextLogEntry> {
