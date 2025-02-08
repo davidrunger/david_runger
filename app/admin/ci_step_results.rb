38893dfd143d111bb2309a3b00e17bebccdf8db6
David Runger <davidjrunger@gmail.com>
2025-02-08 15:20:57 -0600

    Add CiStepResult admin pages [DEV-164] (#6054)

---
+++ app/admin/ci_step_results.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register(CiStepResult) do
+  decorate_with CiStepResultDecorator
+  includes :user
+
+  show title: :name_with_identifiers do
+    attributes_table do
+      row :id
+      row :user
+
+      row :name
+      row :seconds
+      row :branch
+      row :passed
+      row :github_run_id
+      row :github_run_attempt
+      row :started_at
+      row :stopped_at
+      row :sha
+
+      row :created_at
+      row :updated_at
+    end
+
+    active_admin_comments
+  end
+end
