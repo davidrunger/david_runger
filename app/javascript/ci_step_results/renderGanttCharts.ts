1075ea2cfe40f5f04a2e0c99e7ab23ac771d9489
David Runger <davidjrunger@gmail.com>
2025-02-12 15:59:06 -0600

    Show duration of steps on hover in Gantt charts [CSR-7]

--- app/javascript/ci_step_results/renderGanttCharts.ts
+++ app/javascript/ci_step_results/renderGanttCharts.ts
@@ -36,7 +36,10 @@ export function renderGanttCharts(ciStepResultsSet: Array<CiStepResultsSet>) {
           type: 'nominal',
           legend: null,
         },
-        tooltip: [{ field: 'name', type: 'nominal', title: 'Step' }],
+        tooltip: [
+          { field: 'name', type: 'nominal', title: 'Step' },
+          { field: 'seconds', type: 'quantitative', title: 'Seconds' },
+        ],
       },
     };
 
