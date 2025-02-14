872bbdd7c7fafed222bb9735546a227b9867e9bc
David Runger <davidjrunger@gmail.com>
2025-02-14 11:18:38 -0600

    [WaitForPercyStart] Log number of attempts before achieving success
    This will be useful to have some rough idea about and record of how long
    it usually takes.

--- lib/test/tasks/wait_for_percy_start.rb
+++ lib/test/tasks/wait_for_percy_start.rb
@@ -7,15 +7,17 @@ class Test::Tasks::WaitForPercyStart < Pallets::Task
       # take particularly long if there is a new Chromium version to download.)
       num_attempts = 20
 
-      num_attempts.times do
+      num_attempts.times do |index|
         if system('./node_modules/.bin/percy exec:ping')
-          record_success_and_log_message('Percy is running.')
+          record_success_and_log_message("Percy is running after #{index + 1} checks.")
 
-          return
+          break
+        elsif index == num_attempts - 1
+          record_failure_and_log_message(<<~LOG)
+            Percy is still not running after #{index + 1} attempts.
+          LOG
         end
       end
-
-      record_failure_and_log_message("Percy is still not running after #{num_attempts} attempts.")
     else
       record_success_and_log_message('Percy token was not present; skipping percy exec:ping.')
     end
