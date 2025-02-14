0eb4b675fb707fd2191c7bab68c8226f42f329cd
David Runger <davidjrunger@gmail.com>
2025-02-14 04:59:12 -0600

    [ci] Stop creating dedicated feature specs database
    With this change, instead of using a dedicated DB, `RunFeatureTestsA`
    will now use the `_unit` DB (similar to how `RunFeatureTestsB` uses the
    `_api` DB).
    
    The `CreateDbCopies` step takes a nontrivial amount of time (~10+
    seconds). I think that reducing the number of DBs created by 25%, which
    this change does (by no longer creating a dedicated DB for feature
    specs), will probably also decrease the time that `CreateDbCopies` takes
    by ~25%. That will allow us to start running the unit tests and API
    controller tests more quickly. Since it seems that the unit tests pretty
    much always finish running before our JavaScript has finished compiling,
    we aren't able to start feature specs in parallel with the unit tests,
    anyway.
    
    If unit tests ever start taking longer and/or if compiling JavaScript
    starts taking less time, then it might be worth undoing this change.
    But, for now, I think it's the optimal flow.

--- lib/test/tasks/run_feature_tests_a.rb
+++ lib/test/tasks/run_feature_tests_a.rb
@@ -3,7 +3,7 @@ class Test::Tasks::RunFeatureTestsA < Pallets::Task
 
   def run
     execute_rspec_command(<<~COMMAND)
-      DB_SUFFIX=_feature CAPYBARA_SERVER_PORT=3001
+      DB_SUFFIX=_unit CAPYBARA_SERVER_PORT=3001
       bin/rspec $(cat tmp/feature_specs_a.txt)
       --format RSpec::Instafail --format progress --force-color
     COMMAND
