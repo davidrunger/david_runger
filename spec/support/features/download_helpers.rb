6c2d46da5d35ddd5bd892ebc521b872c3f9e3a29
David Runger <davidjrunger@gmail.com>
2025-02-15 15:09:20 -0600

    Add debugging print statements and increase sleep from 0.05 to 0.1 s
    It failed in CI:
    https://github.com/davidrunger/david_runger/actions/runs/13348590540/job/37282183996?pr=6155#step:11:306

--- spec/support/features/download_helpers.rb
+++ spec/support/features/download_helpers.rb
@@ -11,7 +11,7 @@ RSpec.configure do |config|
 end
 
 module Features::DownloadHelpers
-  def downloaded_file_path(relative_glob_pattern, max_attempts: 100, sleep_seconds: 0.05)
+  def downloaded_file_path(relative_glob_pattern, max_attempts: 100, sleep_seconds: 0.1)
     if @capybara_downloads_tmp_dir.nil?
       raise('Apply the :with_tmp_download_dir RSpec metadata to the spec.')
     end
@@ -22,8 +22,16 @@ module Features::DownloadHelpers
       matching_paths = Dir.glob(absolute_glob_pattern)
 
       if (matching_path = matching_paths.first)
+        puts("Success on attempt #{index + 1}.")
+
         break matching_path
       elsif index == max_attempts - 1
+        puts(%(Capybara.save_path: #{Capybara.save_path}))
+        puts(%(@capybara_downloads_tmp_dir: #{@capybara_downloads_tmp_dir}))
+        puts(%(relative_glob_pattern: #{relative_glob_pattern}))
+        puts(%(absolute_glob_pattern: #{absolute_glob_pattern}))
+        puts(%(`ls #{absolute_glob_pattern}`: #{`ls #{absolute_glob_pattern}`}))
+
         raise(<<~ERROR)
           Could not find a file matching '#{relative_glob_pattern}' after
           #{max_attempts} attempt(s) with a sleep of #{sleep_seconds} seconds.
