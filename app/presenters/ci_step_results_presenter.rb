1273c0d5ba9b9f52c68a80a650a2cf09666a6acc
David Runger <davidjrunger@gmail.com>
2025-02-16 19:40:26 -0600

    [ci-step-results] Make 'name' input a typeahead (datalist) [CSR-14] (#6171)
    ![image](https://github.com/user-attachments/assets/829097f3-d7bd-4dfb-a2d8-ce5a10df2679)

--- app/presenters/ci_step_results_presenter.rb
+++ app/presenters/ci_step_results_presenter.rb
@@ -11,9 +11,7 @@ class CiStepResultsPresenter
 
   memoize \
   def branch_names
-    @user.
-      ci_step_results.
-      where(created_at: @search_params.fetch('created_at_gt')..).
+    user_ci_step_results_in_time_range.
       distinct.
       pluck(:branch).
       sort_by do |branch_name|
@@ -23,6 +21,21 @@ class CiStepResultsPresenter
           branch_name.downcase
         end
       end
+  end
+
+  memoize \
+  def ci_step_names
+    user_ci_step_results_in_time_range.
+      distinct.
+      pluck(:name).
+      sort_by(&:downcase)
+  end
+
+  memoize \
+  def user_ci_step_results_in_time_range
+    @user.
+      ci_step_results.
+      where(created_at: @search_params.fetch('created_at_gt')..)
   end
 
   # rubocop:disable Metrics/PerceivedComplexity
