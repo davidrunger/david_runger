84e36728f942afbe1de9fcb5a35a193b53f58bf6
David Runger <davidjrunger@gmail.com>
2025-02-17 10:37:15 -0600

    Split CiStepResultsFormPresenter from CiStepResultsPresenter [CSR-16] (#6177)
    It seems suspicious that my first two presenters both come in the same
    domain area, but I feel that this is right.

--- app/presenters/ci_step_results_presenter.rb
+++ app/presenters/ci_step_results_presenter.rb
@@ -3,39 +3,8 @@ class CiStepResultsPresenter
 
   FIELDS_TO_PLUCK = %i[name github_run_id github_run_attempt created_at seconds].freeze
 
-  def initialize(ci_step_results:, user:, search_params:)
+  def initialize(ci_step_results:)
     @ci_step_results = ci_step_results
-    @user = user
-    @search_params = search_params
-  end
-
-  memoize \
-  def branch_names
-    user_ci_step_results_in_time_range.
-      distinct.
-      pluck(:branch).
-      sort_by do |branch_name|
-        if branch_name == 'main'
-          ''
-        else
-          branch_name.downcase
-        end
-      end
-  end
-
-  memoize \
-  def ci_step_names
-    user_ci_step_results_in_time_range.
-      distinct.
-      pluck(:name).
-      sort_by(&:downcase)
-  end
-
-  memoize \
-  def user_ci_step_results_in_time_range
-    @user.
-      ci_step_results.
-      where(created_at: @search_params.fetch('created_at_gt')..)
   end
 
   # rubocop:disable Metrics/PerceivedComplexity
