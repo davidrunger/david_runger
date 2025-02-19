49cd2f40d25daa516b17969178bfb2f1db327ad7
David Runger <davidjrunger@gmail.com>
2025-02-19 10:15:11 -0600

    [groceries] Auto-linkify URLs in grocery items [GROC-25]
    Sometimes I include URLs in my grocery items. It will be convenient for
    these URLs to be hyperlinked. This change implements that.
    
    I tried to find a micro-library that would do this for me, but I
    couldn't find something with the characteristics that I want, so I am
    implementing it myself. Hopefully this regex will cover the relevant
    (edge) cases as I want it to.
    
    Also, I am adding `"moduleDetection": "force"` to `tsconfig.json` to
    avoid the error ("Cannot redeclare block-scoped variable") that I was
    getting when editing a file (`app/javascript/typescript-scratchpad.ts`)
    that did not have any imports or exports and which had a constant name
    (`regex`) that was also in another file
    (`app/javascript/lib/linkify.ts`). Source:
    https://stackoverflow.com/a/77005521/4009384 .

---
+++ app/javascript/lib/linkify.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line max-len
+const httpUrlRegex = /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*[a-zA-Z0-9/])(?:#[-a-zA-Z0-9()@:%_+.~#?&/=]*)?)/gi;
+
+export function linkify(text: string): string {
+  return text.replace(httpUrlRegex, '<a href="$1">$1</a>');
+}
