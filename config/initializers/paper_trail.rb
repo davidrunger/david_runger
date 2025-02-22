e534592baec7c4e64e7910d94c1193009acd2c78
David Runger <davidjrunger@gmail.com>
2025-02-22 05:42:55 -0600

    Turn off PaperTrail for specs (except where needed) [DEV-188]
    This might make specs faster, especially since I recently added
    PaperTrail tracking for LogEntries (#6199).
    
    I hate to diverge from what happens in production, but I think that this
    is probably worth it.

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,4 +1,4 @@
-PaperTrail.config.enabled = true
+PaperTrail.config.enabled = !Rails.env.test?
 PaperTrail.config.has_paper_trail_defaults = {
   # Omit `create` events.
   on: %i[destroy touch update],
