de2825f23844553792ddeeb7be5ce862f08e7e54
David Runger <davidjrunger@gmail.com>
2025-02-22 00:44:12 -0600

    Add :nocov: comments

--- config/initializers/0w_secret_key_base.rb
+++ config/initializers/0w_secret_key_base.rb
@@ -6,6 +6,8 @@ secret_key_base =
 
 if secret_key_base.present?
   DavidRunger::Application.config.secret_key_base = secret_key_base
+  # :nocov:
 elsif ENV.fetch('SECRET_KEY_BASE_DUMMY', nil) != '1'
   fail('Could not find a secret_key_base in ENV or credentials.')
+  # :nocov:
 end
