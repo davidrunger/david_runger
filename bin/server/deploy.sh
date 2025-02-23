e799959577a2d3dc42ef9af802e4e52d6be31eb5
David Runger <davidjrunger@gmail.com>
2025-02-23 08:09:54 -0600

    Disable SC1091 in problematic places

--- bin/server/deploy.sh
+++ bin/server/deploy.sh
@@ -32,7 +32,7 @@ if [ ! -v LOCAL_TEST ] ; then
 fi
 
 # Export necessary env vars.
-# shellcheck source=bin/server/export-env-vars-needed-by-docker
+# shellcheck disable=SC1091
 . bin/server/export-env-vars-needed-by-docker
 
 # Rebuild the app (for development if testing locally, otherwise for production).
