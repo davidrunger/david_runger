davidrunger/david_runger

commit 63650ebd82302d7f08edea07753e92455ca6b494

Add endpoint to forward Grafana alert webhooks as emails [DEV-186] (#6235)
I want to make it so that Grafana can send a webhook to my app when a
monitoring alert is triggered, and my app will convert that webhook
content into an email and send it to me. (There's something unideal
about using the system that is being monitored as part of the
alert-sending process, but I'll take that risk.)

I'm doing this instead of just having Grafana send the email because it
seems that configuring Grafana to send emails itself is a decently big
pain and/or somewhat of a security risk (e.g. having to put a GMail
password for my personal account onto the server).

To develop and test the Grafana -> `web` webhook, I wanted to be able to
iterate quickly on changes to the Rails web app while running the Rails
web app within the Docker Compose context (where it could be pinged by
Grafana's alert test requests). I didn't want to have to rebuild the
Docker image each time that I made a change to the Rails app source
code, though; that would have been very slow and annoying!

Fortunately, Docker Compose has something that seems to be designed for
this use case, [Compose Watch][1].

[1]: https://docs.docker.com/compose/how-tos/file-watch/

We can use Docker Compose Watch via a command like this...

```
COMPOSE_BAKE=true GIT_REV=$(git rev-parse origin/main) RUBY_VERSION=$(cat .ruby-version) RAILS_ENV=development docker compose watch cadvisor clock grafana loki nginx node_exporter postgres prometheus rails_metrics redis-app redis-cache vector web worker
```

... which will boot the specified services (which is all that run in
production, except for `certbot`, which there's not really a point in
running locally) and which will sync file changes to the running Docker
images that were built with a `develop.watch` configuration in
`docker-compose.yml`, which is currently the services that use the
`default-rails-config`.

We cannot use `--build-arg` with Docker Compose Watch, though, so this

config/environments/production.rb
@@ -103,10 +103,11 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = :all
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    'davidrunger.com', # Allow requests from davidrunger.com.
+    /.*\.davidrunger\.com/, # Allow requests from subdomains like `www.davidrunger.com`.
+    'web:3000', # Allow access from other services in the Docker Compose network.
+  ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }

app/controllers/api/webhook_email_forwards_controller.rb
@@ -1,0 +1,15 @@
+class Api::WebhookEmailForwardsController < Api::BaseController
+  def create
+    authorize(:create?, policy_class: WebhookEmailForwardPolicy)
+
+    subject = params[:title]
+    html_body = params[:message]
+    GenericMailer.generic_html(
+      current_or_auth_token_user.email,
+      subject,
+      html_body,
+    ).deliver_later
+
+    head :created
+  end
+end

app/controllers/application_controller.rb
@@ -123,7 +123,7 @@ class ApplicationController < ActionController::Base
 
   # override Rails's built-in #verify_authenticity_token method to allow for `auth_token` use
   def verify_authenticity_token
-    if auth_token_param.present?
+    if auth_token_secret.present?
       verify_valid_auth_token!
     else
       super

app/controllers/concerns/token_authenticatable.rb
@@ -8,17 +8,27 @@ module TokenAuthenticatable
 
   memoize \
   def auth_token
-    return nil if auth_token_param.blank?
+    return nil if auth_token_secret.blank?
 
-    AuthToken.find_by(secret: auth_token_param)
+    AuthToken.find_by(secret: auth_token_secret)
   end
 
   memoize \
-  def auth_token_param
-    auth_token_param = params[:auth_token]
+  def auth_token_secret
+    auth_token_secret = params[:auth_token] || auth_token_in_header
     # For AuthTokensController requests, the top-level `auth_token` param might be a hash, which we
     # don't want here.
-    auth_token_param.is_a?(String) ? auth_token_param : nil
+    auth_token_secret.is_a?(String) ? auth_token_secret : nil
+  end
+
+  memoize \
+  def auth_token_in_header
+    authorization_header = request.headers['Authorization']
+
+    if authorization_header.present?
+      match = authorization_header.match(/\ABearer\s+(?<token>.+)\z/)
+      match&.named_captures&.[]('token')
+    end
   end
 
   memoize \

app/mailers/generic_mailer.rb
@@ -1,0 +1,5 @@
+class GenericMailer < ApplicationMailer
+  def generic_html(to, subject, html_body)
+    mail(to:, subject:, body: html_body, content_type: 'text/html')
+  end
+end

app/policies/webhook_email_forward_policy.rb
@@ -1,0 +1,2 @@
+class WebhookEmailForwardPolicy < ApplicationPolicy
+end

bin/build-docker
@@ -16,15 +16,17 @@ build() {
   services=("$@")
 
   if [ "${#services[@]}" -eq 0 ]; then
-    COMPOSE_BAKE=true docker compose --progress=plain build \
-      --build-arg GIT_REV="$(git rev-parse origin/main)" \
-      --build-arg RUBY_VERSION="$(cat .ruby-version)" \
-      --build-arg RAILS_ENV="$rails_env"
+    COMPOSE_BAKE=true \
+    GIT_REV="$(git rev-parse origin/main)" \
+    RAILS_ENV="$rails_env" \
+    RUBY_VERSION="$(cat .ruby-version)" \
+      docker compose --progress=plain build
   else
-    COMPOSE_BAKE=true docker compose --progress=plain build "${services[@]}" \
-      --build-arg GIT_REV="$(git rev-parse origin/main)" \
-      --build-arg RUBY_VERSION="$(cat .ruby-version)" \
-      --build-arg RAILS_ENV="$rails_env"
+    COMPOSE_BAKE=true \
+    GIT_REV="$(git rev-parse origin/main)" \
+    RAILS_ENV="$rails_env" \
+    RUBY_VERSION="$(cat .ruby-version)" \
+      docker compose --progress=plain build "${services[@]}"
   fi
 }
 

config/environments/development.rb
@@ -98,6 +98,9 @@ Rails.application.configure do
       :letter_opener
     end
 
+  # Allow access from other services in the Docker Compose network.
+  config.hosts << 'web:3000'
+
   local_hostname = ENV.fetch('LOCAL_HOSTNAME', nil)
   if local_hostname && !local_hostname.empty? # rubocop:disable Rails/Present
     config.hosts << local_hostname

commit 22b0135efc4150031b067a11894e68331cbe5c68

[production.rb] Skip host validation for '/up'
I think that this will fix our deploy failure
https://github.com/davidrunger/david_runger/actions/runs/13483444237/job/37671590476#step:5:395
caused by
https://github.com/davidrunger/david_runger/pull/6235/files#diff-da60b4e96eff2b132991226d308949e23f4ef3aad45ad59edd09cbc32cc6251eR106-R110
.

config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == '/up' } }
 
   # Email
   config.action_mailer.perform_deliveries = true