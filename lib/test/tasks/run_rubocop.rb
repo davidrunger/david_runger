8d712613b553b1f3b5a4febc86f99665a13b71d0
David Runger <davidjrunger@gmail.com>
2025-02-23 11:28:25 -0600

    Try 'bundle exec rubocop'

--- lib/test/tasks/run_rubocop.rb
+++ lib/test/tasks/run_rubocop.rb
@@ -3,7 +3,7 @@ class Test::Tasks::RunRubocop < Pallets::Task
 
   def run
     execute_system_command(<<~COMMAND)
-      bin/rubocop --color --format clang
+      bundle exec rubocop --color --format clang
     COMMAND
   end
 end
