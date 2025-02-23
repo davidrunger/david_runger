a8e595274ae9ec3d80086417699f880aee8f76cf
David Runger <davidjrunger@gmail.com>
2025-02-23 10:29:30 -0600

    Remove 'English' require

--- config/initializers/std_lib.rb
+++ config/initializers/std_lib.rb
@@ -1,4 +1,3 @@
-require 'English' # $CHILD_STATUS and other named global variables
 require 'csv' # for LogsController, Logs::UploadsController
 require 'digest/sha1' # for ApplicationWorker uniqueness enforcement
 require 'fileutils' # assets:precompile
