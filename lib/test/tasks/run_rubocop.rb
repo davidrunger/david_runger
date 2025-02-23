2370164f98068a0c7693bd51c6281d3e4376661d
David Runger <davidjrunger@gmail.com>
2025-02-23 11:47:21 -0600

    Use bin/rubocop in comment rather than bundle exec rubocop (#6243)

--- lib/test/tasks/run_rubocop.rb
+++ lib/test/tasks/run_rubocop.rb
@@ -2,7 +2,7 @@ class Test::Tasks::RunRubocop < Pallets::Task
   include Test::TaskHelpers
 
   def run
-    # bundle exec rubocop --color --format clang
+    # bin/rubocop --color --format clang
     execute_system_command(<<~COMMAND)
       echo 'Temporarily disabled due to bugs (?) in RuboCop.'
     COMMAND
