5636226a705d06a7d7ccc3fa904a3280f85ea5da
David Runger <davidjrunger@gmail.com>
2025-02-23 22:30:50 -0600

    [ci] Remove double backslash escapes by using single quoted heredocs (#6244)
    This makes it easier to copy the commands into a terminal when I
    want/need to replicate one of the CI steps on my local machine.

--- lib/test/tasks/run_html_controller_tests.rb
+++ lib/test/tasks/run_html_controller_tests.rb
@@ -3,10 +3,10 @@ class Test::Tasks::RunHtmlControllerTests < Pallets::Task
 
   def run
     # run all tests in `spec/controllers/` _except_ those in `spec/controllers/api/`
-    execute_rspec_command(<<~COMMAND)
+    execute_rspec_command(<<~'COMMAND')
       DB_SUFFIX=_html
       bin/rspec
-      $(ls -d spec/controllers/*/ | grep -v 'spec/controllers/api/' | tr '\\n' ' ')
+      $(ls -d spec/controllers/*/ | grep -v 'spec/controllers/api/' | tr '\n' ' ')
       $(ls spec/controllers/*.rb)
       $(ls spec/helpers/*.rb)
       --format RSpec::Instafail --format progress --force-color
