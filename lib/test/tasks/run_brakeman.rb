615bb568ef5aa186056530a5efe9cd2e085bd617
David Runger <davidjrunger@gmail.com>
2025-02-24 05:02:36 -0600

    [RunBrakeman] Skip any gitignored initializers
    This way, the check will pass for me locally, even if I have brakeman
    violations in my `config/initializers/z.rb` file.

--- lib/test/tasks/run_brakeman.rb
+++ lib/test/tasks/run_brakeman.rb
@@ -2,12 +2,21 @@ class Test::Tasks::RunBrakeman < Pallets::Task
   include Test::TaskHelpers
 
   def run
+    files_to_skip =
+      ['app/workers/application_worker.rb'] +
+      `git status --ignored --porcelain | cut -d' ' -f 2 | grep 'config/initializers/'`.
+        split("\n")
+    skip_files_arg =
+      files_to_skip.map do |file_to_skip|
+        "--skip-files #{file_to_skip}"
+      end.join(' ')
+
     # skip `application_worker.rb` because it has `...`, which the brakeman parser cannot understand
     execute_system_command(<<~COMMAND.squish, log_stdout_only_on_failure: true)
       bin/brakeman
         --quiet
         --no-pager
-        --skip-files app/workers/application_worker.rb
+        #{skip_files_arg}
     COMMAND
   end
 end
