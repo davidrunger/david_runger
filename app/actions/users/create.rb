88afa03df75ba02ef6d25c7a7d640ef7fc843c4a
David Runger <davidjrunger@gmail.com>
2025-02-27 11:32:17 -0600

    Save Google 'sub' immediately for newly created users [DEV-194]
    The Google `sub` value should be saved immediately for new users when
    they sign up, but it's currently not being saved for them. This change
    fixes that (by saving the Google `sub` value immediately when a new user
    signs up).

--- app/actions/users/create.rb
+++ app/actions/users/create.rb
@@ -1,10 +1,11 @@
 class Users::Create < ApplicationAction
   requires :email, String
+  requires :google_sub, String
 
   returns :user, User
 
   def execute
-    result.user = User.create!(email:)
+    result.user = User.create!(email:, google_sub:)
     AdminMailer.user_created(result.user.id).deliver_later
   end
 end
