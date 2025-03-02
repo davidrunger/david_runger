6e127b47966f11b55f76688a18825a72315c16c1
David Runger <davidjrunger@gmail.com>
2025-03-01 21:04:52 -0600

    [spec/features/blog] Ensure existing user is 1+ minute old [COM-4] (#6303)
    Otherwise, the spec can flake (because of this condition:
    https://github.com/davidrunger/david_runger/blob/a5da7009cd61c093d909852d611c197bdbfde83e/app/controllers/application_controller.rb/#L209).

--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -49,78 +49,82 @@ RSpec.describe 'Blog' do
       let(:existing_user) { users(:user) }
       let(:existing_user_email) { existing_user.email }
 
-      it 'shows the blog article content and allows making comments, replying to comments, editing comments, and deleting comments' do
-        visit blog_url_path
+      context 'when the replying user is at least 1 minute old' do
+        before { existing_user.update!(created_at: 1.minute.ago) }
 
-        expect(page).to have_text(h1_text)
-
-        click_on('Sign in / sign up')
-        click_on('Log in with Google')
-        fill_in('Public display name', with: 'Sue Salamander')
-        click_on('Submit')
-
-        comment_text = 'I have something to say!'
-        fill_in('Write a comment...', with: comment_text)
-        click_on('Post')
-
-        expect(page).to have_css('.comment', text: comment_text)
-
-        Capybara.using_session('replier') do
-          MockOmniAuth.google_oauth2(email: existing_user_email)
-
+        it 'shows the blog article content and allows making comments, replying to comments, editing comments, and deleting comments' do
           visit blog_url_path
+
+          expect(page).to have_text(h1_text)
 
           click_on('Sign in / sign up')
           click_on('Log in with Google')
+          fill_in('Public display name', with: 'Sue Salamander')
+          click_on('Submit')
 
-          click_on('Reply')
-          reply_text = 'I have something to say in reply!'
-          find_all(:fillable_field, 'Write a comment...')[1].send_keys(reply_text)
+          comment_text = 'I have something to say!'
+          fill_in('Write a comment...', with: comment_text)
+          click_on('Post')
 
-          with_inline_sidekiq do
-            num_emails_before = ActionMailer::Base.deliveries.size
+          expect(page).to have_css('.comment', text: comment_text)
+
+          Capybara.using_session('replier') do
+            MockOmniAuth.google_oauth2(email: existing_user_email)
+
+            visit blog_url_path
+
+            click_on('Sign in / sign up')
+            click_on('Log in with Google')
+
             click_on('Reply')
-            # Wait for two emails: one to an admin and one to the user being replied to.
-            wait_for { ActionMailer::Base.deliveries.size }.to eq(num_emails_before + 2)
+            reply_text = 'I have something to say in reply!'
+            find_all(:fillable_field, 'Write a comment...')[1].send_keys(reply_text)
+
+            with_inline_sidekiq do
+              num_emails_before = ActionMailer::Base.deliveries.size
+              click_on('Reply')
+              # Wait for two emails: one to an admin and one to the user being replied to.
+              wait_for { ActionMailer::Base.deliveries.size }.to eq(num_emails_before + 2)
+            end
+
+            expect(page).to have_css('.comment', text: reply_text)
+
+            open_email(new_user_email.downcase)
+
+            expect(current_email).to be_present
+            expect(current_email).to have_text(<<~EMAIL.squish)
+              User #{existing_user.id} has replied to a comment that you made at
+              https://davidrunger.com/blog/something-i-learned .
+              Comment content
+              I have something to say in reply!
+            EMAIL
+
+            click_on('Edit')
+            additional_text = ' Additionally!'
+            find_all(:fillable_field, 'Write a comment...')[1].send_keys(additional_text)
+            click_on('Update')
+
+            expect(page).to have_css('.comment', text: "#{reply_text}#{additional_text}")
           end
 
-          expect(page).to have_css('.comment', text: reply_text)
-
-          open_email(new_user_email.downcase)
-
-          expect(current_email).to be_present
-          expect(current_email).to have_text(<<~EMAIL.squish)
-            User #{existing_user.id} has replied to a comment that you made at
-            https://davidrunger.com/blog/something-i-learned .
-            Comment content
-            I have something to say in reply!
-          EMAIL
-
-          click_on('Edit')
-          additional_text = ' Additionally!'
-          find_all(:fillable_field, 'Write a comment...')[1].send_keys(additional_text)
-          click_on('Update')
-
-          expect(page).to have_css('.comment', text: "#{reply_text}#{additional_text}")
-        end
-
-        # Reload the page to load the reply comment.
-        visit blog_url_path
-
-        expect(comment_count).to eq(2)
-
-        click_on('Delete')
-
-        # Because the comment has at least one reply, it is anonymized, rather than fully deleted.
-        expect(comment_count).to eq(2)
-        expect(page).to have_text('[deleted]')
-
-        Capybara.using_session('replier') do
+          # Reload the page to load the reply comment.
           visit blog_url_path
+
+          expect(comment_count).to eq(2)
+
           click_on('Delete')
 
-          # Because the reply has no replies of its own, clicking "Delete" fully deletes it.
-          expect(comment_count).to eq(1)
+          # Because the comment has at least one reply, it is anonymized, rather than fully deleted.
+          expect(comment_count).to eq(2)
+          expect(page).to have_text('[deleted]')
+
+          Capybara.using_session('replier') do
+            visit blog_url_path
+            click_on('Delete')
+
+            # Because the reply has no replies of its own, clicking "Delete" fully deletes it.
+            expect(comment_count).to eq(1)
+          end
         end
       end
     end
