c6e14e025e0bf82cecf45cdda0c368b44afa8f3f
David Runger <davidjrunger@gmail.com>
2025-03-02 07:22:17 -0600

    [CreateDbCopies] Add debug query to see problematic PG connections
    I want more insight into these intermittent errors:
    
    https://github.com/davidrunger/david_runger/actions/runs/13615687843/job/38058422747?pr=6305#step:12:163

--- lib/test/tasks/create_db_copies.rb
+++ lib/test/tasks/create_db_copies.rb
@@ -31,6 +31,13 @@ class Test::Tasks::CreateDbCopies < Pallets::Task
         execute_system_command("dropdb --if-exists #{db_name}")
       end
 
+      execute_system_command(<<~COMMAND.squish)
+        psql -U postgres -h 127.0.0.1 -d david_runger_test -c "
+        SELECT pid, usename, application_name, client_addr, state, query
+        FROM pg_stat_activity
+        WHERE datname = 'david_runger_test' AND pid != pg_backend_pid();"
+      COMMAND
+
       execute_system_command(<<~COMMAND)
         createdb
           -T david_runger_test #{db_name}
