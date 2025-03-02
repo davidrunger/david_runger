96c0a5d1faa7788fbb4d9f84194c3b6a3f0dcd9f
David Runger <davidjrunger@gmail.com>
2025-03-02 06:48:03 -0600

    Remove max-len disablement comment (#6304)
    This should have been done in #6302 .

--- app/javascript/lib/linkify.ts
+++ app/javascript/lib/linkify.ts
@@ -1,5 +1,4 @@
 const httpUrlRegex =
-  // eslint-disable-next-line max-len
   /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*[a-zA-Z0-9/])(?:#[-a-zA-Z0-9()@:%_+.~#?&/=]*)?)/gi;
 
 export function linkify(text: string): string {
