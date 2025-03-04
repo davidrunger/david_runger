cdb168d66433c6b18e5b405df9f533cd3f148a21
David Runger <davidjrunger@gmail.com>
2025-03-03 22:12:11 -0600

    [comments] Add syntax highlighting for code blocks [COM-3] (#6315)
    We are now loading CSS for two separate syntax highlighting libraries,
    but whatever.
    
    
    ![image](https://github.com/user-attachments/assets/4315e7ed-b216-437a-bb58-36b678725949)

--- app/javascript/comments/components/Comment.vue
+++ app/javascript/comments/components/Comment.vue
@@ -51,14 +51,35 @@
 
 <script setup lang="ts">
 import DOMPurify from 'dompurify';
+import hljs from 'highlight.js/lib/core';
+import bash from 'highlight.js/lib/languages/bash';
+import crystal from 'highlight.js/lib/languages/crystal';
+import diff from 'highlight.js/lib/languages/diff';
+import dockerfile from 'highlight.js/lib/languages/dockerfile';
+import erb from 'highlight.js/lib/languages/erb';
+import haml from 'highlight.js/lib/languages/haml';
+import javascript from 'highlight.js/lib/languages/javascript';
+import json from 'highlight.js/lib/languages/json';
+import lua from 'highlight.js/lib/languages/lua';
+import nginx from 'highlight.js/lib/languages/nginx';
+import plaintext from 'highlight.js/lib/languages/plaintext';
+import ruby from 'highlight.js/lib/languages/ruby';
+import scss from 'highlight.js/lib/languages/scss';
+import shell from 'highlight.js/lib/languages/shell';
+import sql from 'highlight.js/lib/languages/sql';
+import typescript from 'highlight.js/lib/languages/typescript';
+import yaml from 'highlight.js/lib/languages/yaml';
 import { DateTime } from 'luxon';
-import { marked } from 'marked';
+import { Marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
 import { computed, ref } from 'vue';
 
 import CommentForm from '@/comments/components/CommentForm.vue';
 import { useCommentsStore } from '@/comments/stores/commentsStore';
 import { type Comment } from '@/comments/types/comment';
 import { edit_public_name_my_account_path } from '@/rails_assets/routes';
+
+import 'highlight.js/styles/github-dark.css';
 
 const props = defineProps<{
   comment: Comment;
@@ -72,8 +93,37 @@ const editNamePath = edit_public_name_my_account_path({
   redirect_chain: window.location.toString(),
 });
 
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('crystal', crystal);
+hljs.registerLanguage('diff', diff);
+hljs.registerLanguage('dockerfile', dockerfile);
+hljs.registerLanguage('erb', erb);
+hljs.registerLanguage('haml', haml);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('json', json);
+hljs.registerLanguage('lua', lua);
+hljs.registerLanguage('nginx', nginx);
+hljs.registerLanguage('plaintext', plaintext);
+hljs.registerLanguage('ruby', ruby);
+hljs.registerLanguage('scss', scss);
+hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('sql', sql);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('yaml', yaml);
+
+const marked = new Marked(
+  markedHighlight({
+    emptyLangClass: 'hljs',
+    langPrefix: 'hljs language-',
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    },
+  }),
+);
+
 const formattedAndSanitizedContent = computed(() => {
-  const rawHtml = marked(props.comment.content || '', { async: false });
+  const rawHtml = marked.parse(props.comment.content || '', { async: false });
   return DOMPurify.sanitize(rawHtml);
 });
 const timeSinceCreation = computed(() => {
@@ -100,6 +150,10 @@ function handleReplyCreate(content: string) {
 </script>
 
 <style scoped>
+.comment :deep(pre) {
+  padding: 0;
+}
+
 .content-and-reply-button {
   margin-bottom: 1rem;
 }
