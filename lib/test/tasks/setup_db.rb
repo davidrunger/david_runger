0e9969b25499e9d967be4481470d00c2d01e9907
David Runger <davidjrunger@gmail.com>
2025-03-04 19:53:57 -0600

    Set DISABLE_TYPELIZER=false in .env.test
    Because we eager load the app in CI, we need `DISABLE_TYPELIZER=false`
    when the app initially boots up; we can't just enable it for steps like
    RunTypelizer that need it. If we do so, there will be at least one
    `unknown` in the generated types.
    https://github.com/davidrunger/david_runger/actions/runs/13666704463/job/38209308590?pr=6322#step:13:289

--- lib/test/tasks/setup_db.rb
+++ lib/test/tasks/setup_db.rb
@@ -2,7 +2,7 @@ class Test::Tasks::SetupDb < Pallets::Task
   include Test::TaskHelpers
 
   def run
-    execute_rake_task('db:create')
+    execute_rake_task('db:create', 'DISABLE_TYPELIZER=1')
     execute_rake_task('db:environment:set', 'RAILS_ENV=test')
     execute_rake_task('db:schema:load')
   end
