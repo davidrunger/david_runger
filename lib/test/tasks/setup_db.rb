bb360a94f41adc5b41a896636d294cdfca506049
David Runger <davidjrunger@gmail.com>
2025-03-04 22:20:54 -0600

    [SetupDb] Remove 'RAILS_ENV=test' arg that does nothing
    (... as far as I can tell.)

--- lib/test/tasks/setup_db.rb
+++ lib/test/tasks/setup_db.rb
@@ -3,7 +3,7 @@ class Test::Tasks::SetupDb < Pallets::Task
 
   def run
     execute_rake_task('db:create')
-    execute_rake_task('db:environment:set', 'RAILS_ENV=test')
+    execute_rake_task('db:environment:set')
     execute_rake_task('db:schema:load')
   end
 end
