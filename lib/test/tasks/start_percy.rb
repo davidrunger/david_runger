b89bd501bf03fecef36ff9a62edca804f9038d40
David Runger <davidjrunger@gmail.com>
2025-03-05 17:34:46 -0600

    [StartPercy] Do a long initial 20 second sleep, then 4 second sleeps (#6340)
    This should waste less CPU time while still completing before JS
    compilation finishes.

--- lib/test/tasks/start_percy.rb
+++ lib/test/tasks/start_percy.rb
@@ -8,17 +8,29 @@ class Test::Tasks::StartPercy < Pallets::Task
       # Make up to 20 attempts to verify that Percy is running. (It tends to
       # take particularly long if there is a new Chromium version to download.)
       num_attempts = 20
+      total_sleep_time = 0
 
       num_attempts.times do |index|
-        sleep(5)
+        sleep_time =
+          if total_sleep_time < 20
+            20
+          else
+            4
+          end
+        total_sleep_time += sleep_time
+        sleep(sleep_time)
 
         if system('./node_modules/.bin/percy exec:ping')
-          record_success_and_log_message("Percy is running after #{index + 1} check(s).")
+          record_success_and_log_message(<<~LOG.squish)
+            Percy is running after #{index + 1} check(s)
+            and #{total_sleep_time} seconds of sleep time.
+          LOG
 
           break
         elsif index == num_attempts - 1
-          record_failure_and_log_message(<<~LOG)
-            Percy is still not running after #{index + 1} attempt(s).
+          record_failure_and_log_message(<<~LOG.squish)
+            Percy is still not running after #{index + 1} attempt(s)
+            and #{total_sleep_time} seconds of sleep time.
           LOG
         end
       end
