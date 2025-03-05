8a3aa27155bf641c9dc5fc333af76e53527488ce
David Runger <davidjrunger@gmail.com>
2025-03-05 17:07:04 -0600

    [StartPercy] Increase sleep from 4 to 5 seconds
    I think that this will put a tiny bit less CPU load while not risking
    taking longer than JavaScript compilation.

--- lib/test/tasks/start_percy.rb
+++ lib/test/tasks/start_percy.rb
@@ -10,7 +10,7 @@ class Test::Tasks::StartPercy < Pallets::Task
       num_attempts = 20
 
       num_attempts.times do |index|
-        sleep(4)
+        sleep(5)
 
         if system('./node_modules/.bin/percy exec:ping')
           record_success_and_log_message("Percy is running after #{index + 1} check(s).")
