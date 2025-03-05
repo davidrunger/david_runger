aed9abe204b5c9c7a53244cedcbc1ed7e0be9082
David Runger <davidjrunger@gmail.com>
2025-03-05 10:52:38 -0600

    [RunRubocop] Print paths that we are excluding
    I'm slightly suspicious that sometimes this takes a pathological form of
    some sort?

--- lib/test/tasks/run_rubocop.rb
+++ lib/test/tasks/run_rubocop.rb
@@ -2,6 +2,8 @@ class Test::Tasks::RunRubocop < Pallets::Task
   include Test::TaskHelpers
 
   def run
+    `git status --ignored --porcelain`.lines.grep(/^!! /)
+
     execute_system_command(<<~COMMAND)
       bin/rubocop --color --format tap
     COMMAND
