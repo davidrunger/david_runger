c54410a595b972c252649d234a1b1280f50a9113
David Runger <davidjrunger@gmail.com>
2025-03-06 13:54:04 -0600

    Make test datamigration less dangerous (just downcase the email)

--- db/datamigrate/20250306182535_downcase_user1_email.rb
+++ db/datamigrate/20250306182535_downcase_user1_email.rb
@@ -1,15 +1,15 @@
-class SetUser1Email < Datamigration::Base
+class DowncaseUser1Email < Datamigration::Base
   def run
     within_transaction do
       user = User.find(1)
 
       log("Email before: #{user.email} .")
 
-      user.update!(email: 'davidjrunger@gmail.com')
+      user.update!(email: user.email.downcase)
 
       log("Email after: #{user.email} .")
     end
   end
 end
 
-Datamigration::Runner.new(SetUser1Email).run
+Datamigration::Runner.new(DowncaseUser1Email).run
