c9fd3e5f5dcc6dee89ce66332f35491c35b19294
David Runger <davidjrunger@gmail.com>
2025-03-13 00:42:29 -0500

    Monkeypatch 'rspec-wait' to call 'Prosopite.pause'
    This makes it so that the repeated calls (if the expectation is not
    initially satisfied) in a `wait_for` block don't get flagged as an N + 1
    query by Prosopite.

---
+++ config/initializers/monkeypatches/rspec_wait_handler.rb
@@ -0,0 +1,47 @@
+if defined?(RSpec::Wait::Handler) && defined?(Prosopite)
+  module RSpecWaitHandlerPatches
+    # rubocop:disable Metrics
+    def handle_matcher(target, initial_matcher, message, &block)
+      start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+      answer = nil
+
+      Prosopite.pause do
+        matcher = RSpec.configuration.clone_wait_matcher ? initial_matcher.clone : initial_matcher
+
+        answer =
+          if (
+            matcher.respond_to?(:supports_block_expectations?) &&
+            matcher.supports_block_expectations?
+          )
+            method(:handle_matcher).super_method.super_method.call(
+              target,
+              matcher,
+              message,
+              &block
+            )
+          else
+            method(:handle_matcher).super_method.super_method.call(
+              target.call,
+              matcher,
+              message,
+              &block
+            )
+          end
+      rescue RSpec::Expectations::ExpectationNotMetError
+        raise if RSpec.world.wants_to_quit
+
+        elapsed_time = Process.clock_gettime(Process::CLOCK_MONOTONIC) - start_time
+        raise if elapsed_time > RSpec.configuration.wait_timeout
+
+        sleep(RSpec.configuration.wait_delay)
+        retry
+      end
+
+      answer
+    end
+  end
+  # rubocop:enable Metrics
+
+  RSpec::Wait::Handler.prepend(RSpecWaitHandlerPatches)
+end
