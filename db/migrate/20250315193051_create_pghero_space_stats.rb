9b08dd077c2aefd4b8c3ae38fcbe1a4f2be01b47
David Runger <davidjrunger@gmail.com>
2025-03-15 17:08:07 -0500

    Fix RuboCop complaint(s)

--- db/migrate/20250315193051_create_pghero_space_stats.rb
+++ db/migrate/20250315193051_create_pghero_space_stats.rb
@@ -1,5 +1,6 @@
 class CreatePgheroSpaceStats < ActiveRecord::Migration[8.0]
   def change
+    # rubocop:disable Rails/CreateTableWithTimestamps
     create_table :pghero_space_stats do |t|
       t.text :database
       t.text :schema
@@ -7,6 +8,7 @@ class CreatePgheroSpaceStats < ActiveRecord::Migration[8.0]
       t.integer :size, limit: 8
       t.timestamp :captured_at
     end
+    # rubocop:enable Rails/CreateTableWithTimestamps
 
     add_index :pghero_space_stats, %i[database captured_at]
   end
