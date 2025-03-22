59e42be9f69f834e389a983adf387c84de23aced
David Runger <davidjrunger@gmail.com>
2025-03-22 10:38:12 -0500

    [JsonPrioritizable] Move comment to a better location

--- app/controllers/concerns/json_prioritizable.rb
+++ app/controllers/concerns/json_prioritizable.rb
@@ -17,11 +17,11 @@ module JsonPrioritizable
         [type, q]
       end.sort_by { |_, q| -q } # Sort by q value descending
 
-      # If application/json has highest priority (tied or better),
-      # force request format to JSON
       json_priority = accepts.find { |type, _| type == 'application/json' }&.last || 0
       highest_priority = accepts.first&.last || 0
 
+      # If `application/json` has highest priority (tied or better),
+      # force request format to JSON.
       if json_priority >= highest_priority && json_priority > 0
         request.format = :json
       end
