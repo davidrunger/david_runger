6cc42d6ac3cd1678c07b698ae5f67d00e62c7f7c
David Runger <davidjrunger@gmail.com>
2025-03-23 19:16:05 -0500

    Sanitize HTML returned by (herein renamed) 'linkifyAndSanitize' (#6502)
    Since the HTML returned by the function formerly known as `linkify` and
    now known as `linkifyAndSanitize` will often be dangerously inserted
    into the DOM using `v-html`, sanitizing the linkified HTML by default
    will provide a layer of protection against XSS attacks.
    
    In fact, if it weren't for our Content Security Policy (CSP), we would
    have had an XSS vulnerability here! Thank goodness that we do have a
    pretty strong CSP that provides broad protection against XSS attacks.
    But it's good to have multiple, redundant layers of protection, rather
    than relying on any single layer of protection, which is why I think
    that this change is very much worth making, regardless of the fact that
    it's not patching an actual XSS vulnerability.

--- app/javascript/lib/linkifyAndSanitize.ts
+++ app/javascript/lib/linkifyAndSanitize.ts
@@ -1,6 +1,8 @@
+import DOMPurify from 'dompurify';
+
 const httpUrlRegex =
   /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*[a-zA-Z0-9/])(?:#[-a-zA-Z0-9()@:%_+.~#?&/=]*)?)/gi;
 
-export function linkify(text: string): string {
-  return text.replace(httpUrlRegex, '<a href="$1">$1</a>');
+export function linkifyAndSanitize(text: string): string {
+  return DOMPurify.sanitize(text.replace(httpUrlRegex, '<a href="$1">$1</a>'));
 }
