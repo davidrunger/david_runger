1b359b1afe28a319a12a107451417a6fc3548599
David Runger <davidjrunger@gmail.com>
2025-03-23 19:00:22 -0500

    Sanitize HTML returned by (herein renamed) 'linkifyAndSanitize'
    Since the HTML returned by the function formerly known as `linkify` and
    now known as `linkifyAndSanitize` will often be dangerously inserted
    into the DOM using `v-html`, sanitizing all of the linkified HTML by
    default provides a layer of protection against XSS attacks.
    
    In fact, if it weren't for our Content Security Policy (CSP), we would
    have had an XSS vulnerability here! Thank goodness that we do have a
    pretty strong CSP that provides broad protection against XSS attacks.
    But it's good to have multiple, redundant layers of protection, rather
    than relying on any single layer of protection, which is why I think
    that this change is very much worth making, regardless of the fact that
    it's not patching an actual XSS vulnerability.

--- app/javascript/groceries/components/Item.vue
+++ app/javascript/groceries/components/Item.vue
@@ -23,7 +23,7 @@
       )
     template(v-else)
       span.item-name
-        span(v-html="linkify(item.name)")
+        span(v-html="linkifyAndSanitize(item.name)")
         |
         |
         a.js-link.text-neutral-400(@click="editItemName" class="hover:text-black")
@@ -47,7 +47,7 @@ import { EditIcon, MinusIcon, PlusIcon, XIcon } from 'vue-tabler-icons';
 import { useGroceriesStore } from '@/groceries/store';
 import type { Item } from '@/groceries/types';
 import { useCancellableInput } from '@/lib/composables/useCancellableInput';
-import { linkify } from '@/lib/linkify';
+import { linkifyAndSanitize } from '@/lib/linkifyAndSanitize';
 
 const ICON_SIZE = 17;
 
