0f6fdc64dcf36a72b2b9402308b084b4c7a37917
David Runger <davidjrunger@gmail.com>
2025-03-25 14:21:24 -0500

    [TokenAuthenticatable] Add some controller specs
    Part of my motivation is to lay the groundwork for the planned changes
    of https://davidrunger.atlassian.net/browse/DEV- 222 .
    
    Also, I think that these unit specs will help to ensure the correctness
    of the tested code, now and in the future.

---
+++ spec/controllers/concerns/token_authenticatable_spec.rb
@@ -0,0 +1,45 @@
+RSpec.describe TokenAuthenticatable, :without_verifying_authorization do
+  controller(ApplicationController) do
+    def index
+      render(plain: '#index action')
+    end
+  end
+
+  let(:auth_token) { AuthToken.first! }
+
+  describe '#current_or_auth_token_user' do
+    before do
+      request.headers.merge!(headers)
+      get(:index, params:)
+    end
+
+    let(:headers) { {} }
+    let(:params) { {} }
+
+    context 'when no user is logged in' do
+      before { sign_out(:user) }
+
+      context 'when no auth_token param or header is provided' do
+        it 'is nil' do
+          expect(controller.send(:current_or_auth_token_user)).to eq(nil)
+        end
+      end
+
+      context 'when an auth_token param corresponding to an AuthToken secret is provided' do
+        let(:params) { { auth_token: auth_token.secret } }
+
+        it "is the AuthToken's user" do
+          expect(controller.send(:current_or_auth_token_user)).to eq(auth_token.user)
+        end
+      end
+
+      context 'when an Authorization header corresponding to an AuthToken secret is provided' do
+        let(:headers) { { 'Authorization' => "Bearer #{auth_token.secret}" } }
+
+        it "is the AuthToken's user" do
+          expect(controller.send(:current_or_auth_token_user)).to eq(auth_token.user)
+        end
+      end
+    end
+  end
+end
