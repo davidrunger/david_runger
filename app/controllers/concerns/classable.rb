404b78712367ab7e3d0b921056c28db5f4fbd980
David Runger <davidjrunger@gmail.com>
2025-03-25 02:43:47 -0500

    Extract Sessions::BaseController and give login pages bg-blue-50

--- app/controllers/concerns/classable.rb
+++ app/controllers/concerns/classable.rb
@@ -2,26 +2,11 @@ module Classable
   extend ActiveSupport::Concern
 
   included do
+    class_attribute :container_classes
+    class_attribute :html_classes
+
     helper_method :container_classes
     helper_method :html_classes
-  end
-
-  class_methods do
-    def container_classes=(classes)
-      @container_classes = classes
-    end
-
-    def container_classes
-      @container_classes || []
-    end
-
-    def html_classes=(classes)
-      @html_classes = classes
-    end
-
-    def html_classes
-      @html_classes || []
-    end
   end
 
   def container_classes
