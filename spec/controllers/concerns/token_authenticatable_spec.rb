63cfd2c128a8e4eda7059f58cd6bcc54488f3d3f
David Runger <davidjrunger@gmail.com>
2025-03-25 14:36:53 -0500

    [TokenAuthenticatable] Add some controller specs (#6536)
    Part of my motivation is to lay the groundwork for the planned changes
    of https://davidrunger.atlassian.net/browse/DEV- 222 .
    
    Also, I think that these unit specs will help to ensure the correctness
    of the tested code, now and in the future.

---
+++ spec/controllers/concerns/token_authenticatable_spec.rb
@@ -0,0 +1,52 @@
+RSpec.describe TokenAuthenticatable, :without_verifying_authorization do
+  controller(ApplicationController) do
+    def index
+      render(plain: 'action:index')
+    end
+  end
+
+  let(:auth_token) { AuthToken.first! }
+
+  describe '#current_or_auth_token_user' do
+    before do
+      request.headers.merge!(headers)
+      get(:index, params:)
+    end
+
+    let(:headers) { {} }
+    let(:params) { {} }
+    let(:authorization_header_name) { 'Authorization' }
+    let(:auth_token_param_name) { 'auth_token' }
+
+    context 'when no user is logged in' do
+      before { sign_out(:user) }
+
+      context 'when no auth_token header or param is provided' do
+        before do
+          expect(headers.transform_keys(&:to_s)[authorization_header_name]).to be_blank
+          expect(params.transform_keys(&:to_s)[auth_token_param_name]).to be_blank
+        end
+
+        it 'is nil' do
+          expect(controller.send(:current_or_auth_token_user)).to eq(nil)
+        end
+      end
+
+      context 'when an auth_token param corresponding to an AuthToken secret is provided' do
+        let(:params) { { auth_token_param_name => auth_token.secret } }
+
+        it "is the AuthToken's user" do
+          expect(controller.send(:current_or_auth_token_user)).to eq(auth_token.user)
+        end
+      end
+
+      context 'when an Authorization header corresponding to an AuthToken secret is provided' do
+        let(:headers) { { authorization_header_name => "Bearer #{auth_token.secret}" } }
+
+        it "is the AuthToken's user" do
+          expect(controller.send(:current_or_auth_token_user)).to eq(auth_token.user)
+        end
+      end
+    end
+  end
+end
