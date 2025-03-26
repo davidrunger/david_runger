ce866b1c457bede2b236a5b9c2026fc2c12b0530
David Runger <davidjrunger@gmail.com>
2025-03-26 17:06:03 -0500

    Remove all remaining usage of Rails credentials [DEV-223] (#6555)
    The only place where we were still using Rails credentials was in the
    test environment, to upload screenshots from failed feature specs and to
    upload compiled assets. This change removes that usage and replaces it
    with ENV vars stored in GitHub Actions secrets.
    
    I don't have the credentials any longer to decrypt the encrypted test
    credentials in order to transfer them to GitHub Actions secrets, so
    instead I have generated new ones (and deleted the old ones), which is
    probably a good idea, anyway (to keep the secrets somewhat fresher, in
    case the previous secrets might have been leaked at some point).

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,6 @@
 Aws.config.update(
   credentials: Aws::Credentials.new(
-    ENV['AWS_ACCESS_KEY_ID'].presence ||
-      Rails.application.credentials.aws&.dig(:access_key_id),
-    ENV['AWS_SECRET_ACCESS_KEY'].presence ||
-      Rails.application.credentials.aws&.dig(:secret_access_key),
+    ENV['AWS_ACCESS_KEY_ID'].presence,
+    ENV['AWS_SECRET_ACCESS_KEY'].presence,
   ),
 )
