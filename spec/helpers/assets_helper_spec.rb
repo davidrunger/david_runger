cc21694b38572ab7e578bf65705833c8a52af50f
David Runger <davidjrunger@gmail.com>
2025-03-29 00:00:53 -0500

    Add test coverage for uncovered lines
    I feel bad stubbing vite_typescript_tag, but I think it might be sort of
    necessary.

--- spec/helpers/assets_helper_spec.rb
+++ spec/helpers/assets_helper_spec.rb
@@ -1,4 +1,47 @@
 RSpec.describe AssetsHelper do
+  describe '#admin_ts_tag' do
+    subject(:admin_ts_tag) { helper.admin_ts_tag(entrypoint_name) }
+
+    let(:entrypoint_name) { 'active_admin' }
+
+    context 'when Rails.env is "development"', rails_env: :development do
+      context 'when VITE_RUBY_ENTRYPOINTS_DIR and VITE_RUBY_PUBLIC_OUTPUT_DIR are set to admin values' do
+        around do |spec|
+          ClimateControl.modify(
+            VITE_RUBY_ENTRYPOINTS_DIR: 'admin_entrypoints',
+            VITE_RUBY_PUBLIC_OUTPUT_DIR: 'vite-admin',
+          ) do
+            spec.run
+          end
+        end
+
+        context 'when a request to "http://localhost:3036/vite-admin/admin_entrypoints/#{entrypoint_name}.ts" returns 200' do
+          before do
+            stub_request(
+              :get,
+              "http://localhost:3036/vite-admin/admin_entrypoints/#{entrypoint_name}.ts",
+            ).to_return(status: 200, headers: {}, body: '')
+          end
+
+          context 'when #vite_typescript_tag returns a value for the entrypoint' do
+            before do
+              allow(helper).
+                to receive(:vite_typescript_tag).
+                with(entrypoint_name, defer: 'defer', crossorigin: nil).
+                and_return(stubbed_vite_typescript_tag)
+            end
+
+            let(:stubbed_vite_typescript_tag) { '<script></script><style></style>' }
+
+            it 'returns a vite_typescript_tag (which will also include a style tag, if appropriate)' do
+              expect(admin_ts_tag).to eq(stubbed_vite_typescript_tag)
+            end
+          end
+        end
+      end
+    end
+  end
+
   describe '#ts_tag' do
     subject(:ts_tag) { helper.ts_tag(pack_name) }
 
