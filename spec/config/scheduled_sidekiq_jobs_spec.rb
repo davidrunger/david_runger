c02112ee43c20e1651c35bf0801d0d216bd1343f
David Runger <davidjrunger@gmail.com>
2025-03-30 03:43:13 -0500

    Bump skedjewel from 1.1.0 to 2.0.0 (#6607)

--- spec/config/scheduled_sidekiq_jobs_spec.rb
+++ spec/config/scheduled_sidekiq_jobs_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe 'Sidekiq scheduled jobs' do
   subject(:schedule) { YAML.load_file('config/skedjewel.yml') }
 
   specify 'all scheduled Sidekiq job classes are indeed defined' do
-    expect { schedule['jobs'].keys.map(&:constantize) }.not_to raise_error
+    expect do
+      schedule['jobs_by_rails_env'].values.map do |jobs_for_env|
+        jobs_for_env.keys.map(&:constantize)
+      end
+    end.not_to raise_error
   end
 end
