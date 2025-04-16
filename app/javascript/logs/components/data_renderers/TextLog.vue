davidrunger/david_runger

commit 7fcf4edbc980903c8054159a0ea7a63a39173317

[logs/css] Tweak 'ol' and 'ul' styling (#6093)
## Before


![image](https://github.com/user-attachments/assets/4dfb9f0a-ae26-41b9-bb21-9c63912f9cc7)

## After


![image](https://github.com/user-attachments/assets/b6ab9fa4-5f96-445b-b1c2-e86cc4899a1a)

app/javascript/logs/components/data_renderers/TextLog.vue
@@ -51,21 +51,44 @@ const sortedLogEntries = computed((): Array<TextLogEntry> => {
 </script>
 
 <style lang="scss">
-ol {
-  counter-reset: item;
-
+ol,
+ul {
   $li-indent: 17px;
 
+  &:not(:first-child) {
+    margin-top: 16px;
+  }
+
   li {
+    &:not(:first-of-type) {
+      margin-top: 0.5em;
+    }
+
     display: block;
-    margin-top: 0.5em;
     margin-left: $li-indent;
 
     &::before {
       margin-left: -1 * $li-indent;
+      position: absolute;
+    }
+  }
+}
+
+ol {
+  counter-reset: item;
+
+  li {
+    &::before {
       content: counter(item) '. ';
       counter-increment: item;
-      position: absolute;
+    }
+  }
+}
+
+ul {
+  li {
+    &::before {
+      content: '– ';
     }
   }
 }

commit b35d388f94477278147fd6f9abae356971b63595

[logs] Scope CSS selector to not add dashes in timepicker [LOG-49] (#6720)
This fixes a visual bug that was introduced by #6093 . There are still
some remaining issues with the timepicker, but for now let's ship this
improvement, at least.

| Before | After |
| - | - |
|
![image](https://github.com/user-attachments/assets/9d8fadaf-511d-4aeb-8413-4908cae80ee2)
|
![image](https://github.com/user-attachments/assets/f79b88dc-5886-4b40-8402-9d973ab0530b)
|

app/javascript/logs/components/data_renderers/TextLog.vue
@@ -89,11 +89,9 @@ ol {
   }
 }
 
-ul {
-  li {
-    &::before {
-      content: '– ';
-    }
+.text-log-table td ul li {
+  &::before {
+    content: '– ';
   }
 }
 