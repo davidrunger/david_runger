653afa1e1c51a849b34205d806be9d64383c49e4
David Runger <davidjrunger@gmail.com>
2025-06-04 09:07:17 -0500

    Fix RuboCop complaint(s)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -16,7 +16,7 @@ class ApplicationRecord < ActiveRecord::Base
       "#{name}Serializer".constantize
     end
 
-    def has_paper_trail_for_all_events # rubocop:disable Naming/PredicateName
+    def has_paper_trail_for_all_events
       # Log all events, including `create` (which we omit by default).
       has_paper_trail(on: %i[create destroy touch update])
     end
