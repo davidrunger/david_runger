dc85cff68fc54b9b9c152f005408d893306ccd3e
David Runger <davidjrunger@gmail.com>
2025-06-12 19:38:51 -0500

    Abort prune and rebuild if expected services aren't running [DEV-245] (#6948)

--- bin/server/prune-docker-and-rebuild.sh
+++ bin/server/prune-docker-and-rebuild.sh
@@ -2,6 +2,16 @@
 
 set -euo pipefail # exit on any error, don't allow undefined variables, pipes don't swallow errors
 
+# To minimize risk of accidental DB deletion, continue only if all expected
+# services are running.
+expected_num_services=15
+actual_num_services=$(docker ps --filter status=running --quiet | wc -l)
+
+if [[ "$actual_num_services" -ne "$expected_num_services" ]] ; then
+  echo "Expected $expected_num_services running Docker services, but found $actual_num_services. Exiting."
+  exit 1
+fi
+
 # Move to the application directory.
 cd /root/david_runger
 
