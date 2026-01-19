4c13c86c8ca4991dd774ecf73997df8ce63f84bc
David Runger <davidjrunger@gmail.com>
2026-01-19 15:37:13 -0600

    [specs/groceries] Ensure expected/required viewed_at store ordering
    This change will fix flakiness introduced in #8099 .

--- spec/support/fixture_builder.rb
+++ spec/support/fixture_builder.rb
@@ -20,8 +20,16 @@ FixtureBuilder.configure do |fbuilder|
     admin_user = name(:admin_user, create(:admin_user, email: 'davidjrunger@gmail.com')).first
 
     # groceries
-    create(:store, user:, name: 'Another Store')
-    store = name(:store, create(:store, user:, name: 'A Long Store Name So It Wraps')).first
+    other_store = create(:store, user:, name: 'Another Store')
+    store = name(
+      :store,
+      create(
+        :store,
+        user:,
+        name: 'A Long Store Name So It Wraps',
+        viewed_at: other_store.viewed_at + 10.seconds,
+      ),
+    ).first
     name(:item, create(:item, :needed, store:, name: 'olive oil', needed: 2))
     create(:item, :unneeded, store:, name: 'apples')
 
