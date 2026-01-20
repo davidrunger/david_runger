154e8c431100040587d1836415f74406847e14a8
David Runger <davidjrunger@gmail.com>
2026-01-20 14:46:39 -0600

    Fix RedisOptions specs and add new ones

--- spec/poros/redis_options_spec.rb
+++ spec/poros/redis_options_spec.rb
@@ -25,15 +25,13 @@ RSpec.describe RedisOptions do
 
       let(:db_number) { 2 }
 
-      context 'when RAILS_ENV is "development"' do
-        around { |spec| ClimateControl.modify(RAILS_ENV: 'development') { spec.run } }
-
+      context 'when Rails.env is "development"', rails_env: 'development' do
         it 'returns a localhost URL with a database path' do
           expect(url).to eq('redis://localhost:6379/2')
         end
       end
 
-      context 'when RAILS_ENV is "production"' do
+      context 'when Rails.env is "production"', rails_env: 'production' do
         around { |spec| ClimateControl.modify(RAILS_ENV: 'production') { spec.run } }
 
         context 'when a REDIS_URL env var is present' do
@@ -49,57 +47,117 @@ RSpec.describe RedisOptions do
     end
   end
 
-  describe '#default_db_number' do
-    subject(:default_db_number) { redis_options.send(:default_db_number) }
+  describe '#test_db_number' do
+    subject(:test_db_number) { redis_options.send(:test_db_number, sidekiq:) }
 
-    context 'when not providing a database number' do
-      subject(:redis_options) { RedisOptions.new }
+    context 'when getting a DB number for Sidekiq' do
+      let(:sidekiq) { true }
 
-      context 'when DB_SUFFIX is _unit' do
-        around { |spec| ClimateControl.modify(DB_SUFFIX: '_unit') { spec.run } }
+      context 'when not providing a database number' do
+        subject(:redis_options) { RedisOptions.new }
 
-        it 'returns 4' do
-          expect(default_db_number).to eq(4)
+        context 'when DB_SUFFIX is _unit' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_unit') { spec.run } }
+
+          it 'returns 9' do
+            expect(test_db_number).to eq(9)
+          end
+        end
+
+        context 'when DB_SUFFIX is _api' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_api') { spec.run } }
+
+          it 'returns 10' do
+            expect(test_db_number).to eq(10)
+          end
+        end
+
+        context 'when DB_SUFFIX is _html' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_html') { spec.run } }
+
+          it 'returns 11' do
+            expect(test_db_number).to eq(11)
+          end
+        end
+
+        context 'when DB_SUFFIX is _feature_a' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_feature_a') { spec.run } }
+
+          it 'returns 12' do
+            expect(test_db_number).to eq(12)
+          end
+        end
+
+        context 'when DB_SUFFIX is _feature_c' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_feature_c') { spec.run } }
+
+          it 'returns 13' do
+            expect(test_db_number).to eq(13)
+          end
+        end
+
+        context 'when DB_SUFFIX is _unexpected' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_unexpected') { spec.run } }
+
+          it 'raises an error' do
+            expect { test_db_number }.to raise_error('Unexpected DB_SUFFIX!')
+          end
         end
       end
+    end
 
-      context 'when DB_SUFFIX is _api' do
-        around { |spec| ClimateControl.modify(DB_SUFFIX: '_api') { spec.run } }
+    context 'when not getting a DB number for Sidekiq' do
+      let(:sidekiq) { false }
 
-        it 'returns 5' do
-          expect(default_db_number).to eq(5)
+      context 'when not providing a database number' do
+        subject(:redis_options) { RedisOptions.new }
+
+        context 'when DB_SUFFIX is _unit' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_unit') { spec.run } }
+
+          it 'returns 4' do
+            expect(test_db_number).to eq(4)
+          end
         end
-      end
 
-      context 'when DB_SUFFIX is _html' do
-        around { |spec| ClimateControl.modify(DB_SUFFIX: '_html') { spec.run } }
+        context 'when DB_SUFFIX is _api' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_api') { spec.run } }
 
-        it 'returns 6' do
-          expect(default_db_number).to eq(6)
+          it 'returns 5' do
+            expect(test_db_number).to eq(5)
+          end
         end
-      end
 
-      context 'when DB_SUFFIX is _feature_a' do
-        around { |spec| ClimateControl.modify(DB_SUFFIX: '_feature_a') { spec.run } }
+        context 'when DB_SUFFIX is _html' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_html') { spec.run } }
 
-        it 'returns 7' do
-          expect(default_db_number).to eq(7)
+          it 'returns 6' do
+            expect(test_db_number).to eq(6)
+          end
         end
-      end
 
-      context 'when DB_SUFFIX is _feature_c' do
-        around { |spec| ClimateControl.modify(DB_SUFFIX: '_feature_c') { spec.run } }
+        context 'when DB_SUFFIX is _feature_a' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_feature_a') { spec.run } }
 
-        it 'returns 9' do
-          expect(default_db_number).to eq(9)
+          it 'returns 7' do
+            expect(test_db_number).to eq(7)
+          end
         end
-      end
 
-      context 'when DB_SUFFIX is _unexpected' do
-        around { |spec| ClimateControl.modify(DB_SUFFIX: '_unexpected') { spec.run } }
+        context 'when DB_SUFFIX is _feature_c' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_feature_c') { spec.run } }
 
-        it 'raises an error' do
-          expect { default_db_number }.to raise_error('Unexpected DB_SUFFIX!')
+          it 'returns 8' do
+            expect(test_db_number).to eq(8)
+          end
+        end
+
+        context 'when DB_SUFFIX is _unexpected' do
+          around { |spec| ClimateControl.modify(DB_SUFFIX: '_unexpected') { spec.run } }
+
+          it 'raises an error' do
+            expect { test_db_number }.to raise_error('Unexpected DB_SUFFIX!')
+          end
         end
       end
     end
