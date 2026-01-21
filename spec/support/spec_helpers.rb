8fc0df89fd4ad01a7c4b9a8d2b466db732321df8
David Runger <davidjrunger@gmail.com>
2026-01-20 23:57:43 -0600

    [specs] Add and use new 'wait_until' helper (rather than sleeping) (#8122)
    This will make the spec both faster (since often we will not sleep at
    all) and also more reliable (since we can sleep up to 5 seconds, if
    needed). When testing this, a few times I did see `NameError:
    uninitialized constant Event`. Hopefully that is not something that we
    see again and it was just a result of some weirdness in how I was
    testing this.

--- spec/support/spec_helpers.rb
+++ spec/support/spec_helpers.rb
@@ -7,4 +7,16 @@ module SpecHelpers
     allow(Flipper).to receive(:enabled?).and_call_original
     allow(Flipper).to receive(:enabled?).with(feature_name).and_return(true)
   end
+
+  def wait_until
+    Prosopite.pause do
+      50.times do
+        if yield
+          return
+        else
+          sleep(0.1)
+        end
+      end
+    end
+  end
 end
