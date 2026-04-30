e59a1e10a2b2eb95918adf5f1b76fe22e9daa8ab
David Runger <davidjrunger@gmail.com>
2026-04-30 09:18:48 -0500

    [home/contact] Remove trailing slash from LinkedIn URL
    The trailing slash seems to cause a redirect in the link checker
    request.

--- app/javascript/home/components/Contact.vue
+++ app/javascript/home/components/Contact.vue
@@ -4,7 +4,7 @@ HomeSection(
   title="Contact me / Links"
 )
   p #[b Email:] #[a(href="mailto:davidjrunger@gmail.com") davidjrunger@gmail.com]
-  p #[b LinkedIn:] #[a(href="https://www.linkedin.com/in/davidrunger/") /in/davidrunger]
+  p #[b LinkedIn:] #[a(href="https://www.linkedin.com/in/davidrunger") /in/davidrunger]
   p #[b GitHub:] #[a(href="https://github.com/davidrunger") @davidrunger]
   p #[b Blog:] #[a(href="/blog") my tech blog]
 </template>
