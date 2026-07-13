64129f9e04a84923392234253dcb02bbd01d0b34
David Runger <davidjrunger@gmail.com>
2026-07-13 09:47:00 -0500

    Fix RuboCop complaint(s)

--- config/initializers/monkeypatches/warning.rb
+++ config/initializers/monkeypatches/warning.rb
@@ -14,7 +14,7 @@ if Rails.env.test?
 
     def warn(message, *_args, **_kwargs)
       # :nocov:
-      unless IGNORED_WARNINGS.any? { message.include?(it) }
+      unless IGNORED_WARNINGS.intersect?(message)
         StoredWarnings.warnings << message
       end
 
